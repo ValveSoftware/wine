50f3e62484005b800353e391879a77e962e815e3
Sebastian Lackner <sebastian@fds-team.de>
2015-03-30 12:32:34 +0200

    server: Add a helper function set_sd_from_token_internal to merge two security descriptors.
    CW-Bug-Id: #22436

--- server/object.h
+++ server/object.h
@@ -175,6 +175,9 @@ extern struct fd *no_get_fd( struct object *obj );
 extern unsigned int default_map_access( struct object *obj, unsigned int access );
 extern struct security_descriptor *default_get_sd( struct object *obj );
 extern int default_set_sd( struct object *obj, const struct security_descriptor *sd, unsigned int set_info );
+extern struct security_descriptor *set_sd_from_token_internal( const struct security_descriptor *sd,
+                                                               const struct security_descriptor *old_sd,
+                                                               unsigned int set_info, struct token *token );
 extern int set_sd_defaults_from_token( struct object *obj, const struct security_descriptor *sd,
                                        unsigned int set_info, struct token *token );
 extern WCHAR *no_get_full_name( struct object *obj, data_size_t *ret_len );
