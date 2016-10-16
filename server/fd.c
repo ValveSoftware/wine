c7ba2f6e652a2181314239b208b689c8d20287b3
Sebastian Lackner <sebastian@fds-team.de>
2016-10-16 03:21:42 +0200

    server: Improve STATUS_CANNOT_DELETE checks for directory case.

--- server/fd.c
+++ server/fd.c
@@ -1924,6 +1924,7 @@ struct fd *open_fd( struct fd *root, const char *name, int flags, mode_t *mode, 
     int root_fd = -1;
     int rw_mode;
     char *path;
+    int created = (flags & O_CREAT);
 
     if (((options & FILE_DELETE_ON_CLOSE) && !(access & DELETE)) ||
         ((options & FILE_DIRECTORY_FILE) && (flags & O_TRUNC)))
@@ -1962,6 +1963,7 @@ struct fd *open_fd( struct fd *root, const char *name, int flags, mode_t *mode, 
                 file_set_error();
                 goto error;
             }
+            created = 0;
         }
         flags &= ~(O_CREAT | O_EXCL | O_TRUNC);
     }
@@ -2056,7 +2058,7 @@ struct fd *open_fd( struct fd *root, const char *name, int flags, mode_t *mode, 
         }
 
         /* can't unlink files if we don't have permission to access */
-        if ((options & FILE_DELETE_ON_CLOSE) && !(flags & O_CREAT) &&
+        if ((options & FILE_DELETE_ON_CLOSE) && !created &&
             !(st.st_mode & (S_IWUSR | S_IWGRP | S_IWOTH)))
         {
             set_error( STATUS_CANNOT_DELETE );
