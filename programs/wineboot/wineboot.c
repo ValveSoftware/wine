285d64e7617ae31e3e03a6554404054cb5e9341f
Andrew Eikum <aeikum@codeweavers.com>
2018-01-22 14:35:51 -0600

    HACK: wineboot: Don't show "updating prefix" window

--- programs/wineboot/wineboot.c
+++ programs/wineboot/wineboot.c
@@ -1217,6 +1217,7 @@ static INT_PTR CALLBACK wait_dlgproc( HWND hwnd, UINT msg, WPARAM wp, LPARAM lp 
     return 0;
 }
 
+/*
 static HWND show_wait_window(void)
 {
     HWND hwnd = CreateDialogParamW( GetModuleHandleW(0), MAKEINTRESOURCEW(IDD_WAITDLG), 0,
@@ -1224,6 +1225,7 @@ static HWND show_wait_window(void)
     ShowWindow( hwnd, SW_SHOWNORMAL );
     return hwnd;
 }
+*/
 
 static HANDLE start_rundll32( const WCHAR *inf_path, BOOL wow64 )
 {
@@ -1380,7 +1382,7 @@ static void update_wineprefix( BOOL force )
 
         if ((process = start_rundll32( inf_path, FALSE )))
         {
-            HWND hwnd = show_wait_window();
+/*            HWND hwnd = show_wait_window();*/
             for (;;)
             {
                 MSG msg;
@@ -1392,7 +1394,7 @@ static void update_wineprefix( BOOL force )
                 }
                 else while (PeekMessageW( &msg, 0, 0, 0, PM_REMOVE )) DispatchMessageW( &msg );
             }
-            DestroyWindow( hwnd );
+/*            DestroyWindow( hwnd );*/
         }
         install_root_pnp_devices();
         update_user_profile();
