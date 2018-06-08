0674e5b375fa8bbb7f9699bf30fd07342dbf14e7
Zebediah Figura <z.figura12@gmail.com>
2018-06-08 15:47:16 -0500

    ntdll: Implement NtPulseEvent().

--- dlls/ntdll/sync.c
+++ dlls/ntdll/sync.c
@@ -477,6 +477,9 @@ NTSTATUS WINAPI NtPulseEvent( HANDLE handle, PULONG PulseCount )
 {
     NTSTATUS ret;
 
+    if (do_esync())
+        return esync_pulse_event( handle );
+
     if (PulseCount)
       FIXME("(%p,%d)\n", handle, *PulseCount);
 
