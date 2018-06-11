cd089d7865118f1d89531a58c582153e7594b633
Zebediah Figura <z.figura12@gmail.com>
2018-06-10 23:12:16 -0500

    server: Allocate shared memory segments for semaphores and mutexes.
    As has been described in the README, these two objects have state that can't
    be expressed (or read from) the eventfd descriptor. Namely, for semaphores
    we need to know the current lock count, and for mutexes we need to know the
     owner tid and the recursion count. To make these work across processes, we
    store them in a global shared memory section.
    
    This patch has no effect by itself; it's for separation.
    
    We use the WINEPREFIX dir to discriminate shm sections for simultaneously
    running servers; this is based off of code in libwine (specifically
    init_server_dir()).

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5812,6 +5812,8 @@ struct create_esync_reply
     struct reply_header __header;
     obj_handle_t handle;
     int          type;
+    unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -5830,6 +5832,8 @@ struct open_esync_reply
     struct reply_header __header;
     obj_handle_t handle;
     int          type;
+    unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -6773,6 +6777,6 @@ union generic_reply
     struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 603
+#define SERVER_PROTOCOL_VERSION 604
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
--- server/main.c
+++ server/main.c
@@ -36,6 +36,7 @@
 #include "file.h"
 #include "thread.h"
 #include "request.h"
+#include "esync.h"
 
 /* command-line options */
 int debug_level = 0;
@@ -139,6 +140,9 @@ int main( int argc, char *argv[] )
 
     sock_init();
     open_master_socket();
+
+    if (do_esync())
+        esync_init();
 
     if (debug_level) fprintf( stderr, "wineserver: starting (pid=%ld)\n", (long) getpid() );
     init_signals();
