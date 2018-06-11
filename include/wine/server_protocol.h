f51ccff27c328e7fa3697fccb1ee506469f2dbea
Zebediah Figura <z.figura12@gmail.com>
2018-06-10 23:12:16 -0500

    server: Allocate shared memory segments for semaphores and mutexes.
    As has been described in the README, these two objects have state that can't
    be expressed (or read from) the eventfd descriptor. Namely, for semaphores
    we need to know the current lock count, and for mutexes we need to know the
     owner tid and the recursion count. To make these work across processes, we
    store them in a global shared memory section.
    
    This patch has no effect by itself; it's for separation.
    
    We use the WINEPREFIX dir to discriminate shm sections for simultaneously
    running servers; this is based off of code in libwine (specifically
    init_server_dir()).

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5680,6 +5680,8 @@ struct create_esync_reply
     struct reply_header __header;
     obj_handle_t handle;
     int          type;
+    unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -5698,6 +5700,8 @@ struct open_esync_reply
     struct reply_header __header;
     obj_handle_t handle;
     int          type;
+    unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -6623,6 +6627,6 @@ union generic_reply
     struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 578
+#define SERVER_PROTOCOL_VERSION 579
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
--- server/trace.c
+++ server/trace.c
@@ -4554,6 +4554,7 @@ static void dump_create_esync_reply( const struct create_esync_reply *req )
 {
     fprintf( stderr, " handle=%04x", req->handle );
     fprintf( stderr, ", type=%d", req->type );
+    fprintf( stderr, ", shm_idx=%08x", req->shm_idx );
 }
 
 static void dump_open_esync_request( const struct open_esync_request *req )
@@ -4569,6 +4570,7 @@ static void dump_open_esync_reply( const struct open_esync_reply *req )
 {
     fprintf( stderr, " handle=%04x", req->handle );
     fprintf( stderr, ", type=%d", req->type );
+    fprintf( stderr, ", shm_idx=%08x", req->shm_idx );
 }
 
 static void dump_get_esync_fd_request( const struct get_esync_fd_request *req )
