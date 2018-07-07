80aa648c876c6c09fb2a55e002f85e78852e2de3
Zebediah Figura <z.figura12@gmail.com>
2018-07-07 12:57:47 +0200

    server: Create eventfd descriptors for pseudo-fd objects and use them for named pipes.

--- server/file.h
+++ server/file.h
@@ -104,6 +104,7 @@ extern char *dup_fd_name( struct fd *root, const char *name );
 extern void get_nt_name( struct fd *fd, struct unicode_str *name );
 
 extern int default_fd_signaled( struct object *obj, struct wait_queue_entry *entry );
+extern int default_fd_get_esync_fd( struct object *obj, enum esync_type *type );
 extern int default_fd_get_poll_events( struct fd *fd );
 extern void default_poll_event( struct fd *fd, int event );
 extern void fd_queue_async( struct fd *fd, struct async *async, int type );
