00ac8025069c7f3f74bd2967b6520826ae2fd2f4
Zebediah Figura <z.figura12@gmail.com>
2018-08-04 15:15:12 -0500

    ntdll, server: Abort if esync is enabled for the server but not the client, and vice versa.

--- dlls/ntdll/thread.c
+++ dlls/ntdll/thread.c
@@ -267,8 +267,7 @@ void thread_init(void)
 
     fill_cpu_info();
 
-    if (do_esync())
-        esync_init();
+    esync_init();
 
     NtCreateKeyedEvent( &keyed_event, GENERIC_READ | GENERIC_WRITE, NULL, 0 );
 }
