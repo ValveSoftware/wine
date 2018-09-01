99a76860f516f2b7edd7c03f1231d092e638a41d
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 15:47:39 -0500

    ntdll: Implement NtReleaseSemaphore().
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>

--- dlls/ntdll/sync.c
+++ dlls/ntdll/sync.c
@@ -345,6 +345,9 @@ NTSTATUS WINAPI NtReleaseSemaphore( HANDLE handle, ULONG count, PULONG previous 
 {
     NTSTATUS ret;
 
+    if (do_fsync())
+        return fsync_release_semaphore( handle, count, previous );
+
     if (do_esync())
         return esync_release_semaphore( handle, count, previous );
 
