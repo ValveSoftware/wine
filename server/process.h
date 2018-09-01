d506158fa870e71d65e5cf74d977e0a727a5bcb9
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 17:20:36 -0500

    server: Create futex sections for process objects.
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>

--- server/process.h
+++ server/process.h
@@ -99,6 +99,7 @@ struct process
     const struct rawinput_device *rawinput_kbd;   /* rawinput keyboard device, if any */
     struct list          kernel_object;   /* list of kernel object pointers */
     int                  esync_fd;        /* esync file descriptor (signaled on exit) */
+    unsigned int         fsync_idx;
 };
 
 #define CPU_FLAG(cpu) (1 << (cpu))
