bc2f28009fb362d7423f87994a78f572ef7fe69a
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 16:36:16 -0500

    ntdll: Implement NtSetEvent().
    (cherry picked from commit de5f9485e1b6ae7c1883159cb57b0bab4fb115e6)

--- dlls/ntdll/unix/fsync.h
+++ dlls/ntdll/unix/fsync.h
@@ -27,6 +27,7 @@ extern NTSTATUS fsync_create_semaphore(HANDLE *handle, ACCESS_MASK access,
 extern NTSTATUS fsync_release_semaphore( HANDLE handle, ULONG count, ULONG *prev ) DECLSPEC_HIDDEN;
 extern NTSTATUS fsync_create_event( HANDLE *handle, ACCESS_MASK access,
     const OBJECT_ATTRIBUTES *attr, EVENT_TYPE type, BOOLEAN initial ) DECLSPEC_HIDDEN;
+extern NTSTATUS fsync_set_event( HANDLE handle, LONG *prev ) DECLSPEC_HIDDEN;
 
 extern NTSTATUS fsync_wait_objects( DWORD count, const HANDLE *handles, BOOLEAN wait_any,
                                     BOOLEAN alertable, const LARGE_INTEGER *timeout ) DECLSPEC_HIDDEN;
