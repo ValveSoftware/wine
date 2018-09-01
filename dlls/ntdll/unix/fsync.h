bcba4f81e31d4118608bda66cbbde7a6f1959679
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 16:39:38 -0500

    ntdll: Implement NtResetEvent().

--- dlls/ntdll/unix/fsync.h
+++ dlls/ntdll/unix/fsync.h
@@ -28,6 +28,7 @@ extern NTSTATUS fsync_release_semaphore( HANDLE handle, ULONG count, ULONG *prev
 extern NTSTATUS fsync_create_event( HANDLE *handle, ACCESS_MASK access,
     const OBJECT_ATTRIBUTES *attr, EVENT_TYPE type, BOOLEAN initial );
 extern NTSTATUS fsync_set_event( HANDLE handle, LONG *prev );
+extern NTSTATUS fsync_reset_event( HANDLE handle, LONG *prev );
 
 extern NTSTATUS fsync_wait_objects( DWORD count, const HANDLE *handles, BOOLEAN wait_any,
                                     BOOLEAN alertable, const LARGE_INTEGER *timeout );
