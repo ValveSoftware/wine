9378ef210274222482b1818b34ca6b45c958adbe
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 17:47:50 -0500

    server: Create futexes for thread objects.

--- server/thread.h
+++ server/thread.h
@@ -92,6 +92,7 @@ struct thread
     WCHAR                 *desc;          /* thread description string */
     int                    esync_fd;      /* esync file descriptor (signalled on exit) */
     int                    esync_apc_fd;  /* esync apc fd (signalled when APCs are present) */
+    unsigned int           fsync_idx;
 };
 
 struct thread_snapshot
