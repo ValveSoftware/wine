f16c3304ba07681ac9cbe7205e143456be9a24c2
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 16:25:59 -0500

    ntdll: Implement waiting on fsync objects.

--- dlls/ntdll/sync.c
+++ dlls/ntdll/sync.c
@@ -1135,6 +1135,13 @@ static NTSTATUS wait_objects( DWORD count, const HANDLE *handles,
 
     if (!count || count > MAXIMUM_WAIT_OBJECTS) return STATUS_INVALID_PARAMETER_1;
 
+    if (do_fsync())
+    {
+        NTSTATUS ret = fsync_wait_objects( count, handles, wait_any, alertable, timeout );
+        if (ret != STATUS_NOT_IMPLEMENTED)
+            return ret;
+    }
+
     if (do_esync())
     {
         NTSTATUS ret = esync_wait_objects( count, handles, wait_any, alertable, timeout );
