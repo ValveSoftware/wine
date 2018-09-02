9d7002ff4ea2961d833ea4845cc2e8e595c09cef
Zebediah Figura <z.figura12@gmail.com>
2018-09-02 11:48:26 -0500

    server, ntdll: Also store the fsync type in the server.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5899,13 +5899,17 @@ struct create_fsync_request
     unsigned int access;
     int low;
     int high;
+    int type;
     /* VARARG(objattr,object_attributes); */
+    char __pad_28[4];
 };
 struct create_fsync_reply
 {
     struct reply_header __header;
     obj_handle_t handle;
+    int type;
     unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -6863,6 +6867,6 @@ union generic_reply
     struct fsync_msgwait_reply fsync_msgwait_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 613
+#define SERVER_PROTOCOL_VERSION 614
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
