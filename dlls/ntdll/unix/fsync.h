395a704bd29f7875bdc0b27d4886c6edb6813d68
Zebediah Figura <z.figura12@gmail.com>
2018-09-02 12:05:37 -0500

    ntdll: Implement NtOpenEvent().

--- dlls/ntdll/unix/fsync.h
+++ dlls/ntdll/unix/fsync.h
@@ -29,6 +29,8 @@ extern NTSTATUS fsync_open_semaphore( HANDLE *handle, ACCESS_MASK access,
     const OBJECT_ATTRIBUTES *attr );
 extern NTSTATUS fsync_create_event( HANDLE *handle, ACCESS_MASK access,
     const OBJECT_ATTRIBUTES *attr, EVENT_TYPE type, BOOLEAN initial );
+extern NTSTATUS fsync_open_event( HANDLE *handle, ACCESS_MASK access,
+    const OBJECT_ATTRIBUTES *attr );
 extern NTSTATUS fsync_set_event( HANDLE handle, LONG *prev );
 extern NTSTATUS fsync_reset_event( HANDLE handle, LONG *prev );
 extern NTSTATUS fsync_create_mutex( HANDLE *handle, ACCESS_MASK access,
