c1310b7ecd2059f74d8a3814e59a4cad94c29302
Matteo Bruni <mbruni@codeweavers.com>
2018-09-06 22:04:18 +0200

    winex11: Fullscreen hack, don't ever blit to the frontbuffer after a wglSwapBuffers().
    Fixes DOOM.

--- dlls/winex11.drv/opengl.c
+++ dlls/winex11.drv/opengl.c
@@ -2402,7 +2402,6 @@ static void wglFinish(void)
                 fs_hack_setup_context( ctx, gl );
             if(!gl->fs_hack_did_swapbuf)
                 fs_hack_blit_framebuffer( gl, GL_FRONT );
-            gl->fs_hack_did_swapbuf = FALSE;
         }else if(gl->fs_hack_context_set_up){
             ctx->fs_hack = FALSE;
             fs_hack_setup_context(ctx, gl);
@@ -2443,7 +2442,6 @@ static void wglFlush(void)
                 fs_hack_setup_context( ctx, gl );
             if(!gl->fs_hack_did_swapbuf)
                 fs_hack_blit_framebuffer( gl, GL_FRONT );
-            gl->fs_hack_did_swapbuf = FALSE;
         }else if(gl->fs_hack_context_set_up){
             ctx->fs_hack = FALSE;
             fs_hack_setup_context(ctx, gl);
