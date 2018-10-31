f570f03396e6274a97a862fec4104169439db5df
Henri Verbeet <hverbeet@codeweavers.com>
2018-10-31 13:01:48 +0330

    wined3d: Pass correct bind flags to swapchain creation functions.
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 543605ca9d43f9aa394a5c2f859805d0a61ea74f)

--- dlls/wined3d/swapchain.c
+++ dlls/wined3d/swapchain.c
@@ -915,12 +915,12 @@ static HRESULT swapchain_init(struct wined3d_swapchain *swapchain, struct wined3
             goto err;
         }
 
-        texture_desc.usage = swapchain->desc.backbuffer_usage;
-        texture_desc.bind_flags = 0;
-        if (texture_desc.usage & WINED3DUSAGE_RENDERTARGET)
-            texture_desc.bind_flags |= WINED3D_BIND_RENDER_TARGET;
-        if (texture_desc.usage & WINED3DUSAGE_TEXTURE)
-            texture_desc.bind_flags |= WINED3D_BIND_SHADER_RESOURCE;
+        texture_desc.bind_flags = swapchain->desc.backbuffer_bind_flags;
+        texture_desc.usage = 0;
+        if (texture_desc.bind_flags & WINED3D_BIND_RENDER_TARGET)
+            texture_desc.usage |= WINED3DUSAGE_RENDERTARGET;
+        if (texture_desc.bind_flags & WINED3D_BIND_SHADER_RESOURCE)
+            texture_desc.usage |= WINED3DUSAGE_TEXTURE;
         for (i = 0; i < swapchain->desc.backbuffer_count; ++i)
         {
             TRACE("Creating back buffer %u.\n", i);
