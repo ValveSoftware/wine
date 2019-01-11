ca9c386591aa28848a4817718d30eb7c69e2440d
Andrew Eikum <aeikum@codeweavers.com>
2019-01-11 11:34:03 -0600

    winex11: Ignore ConfigureNotify messages if there is a FULLSCREEN WM state pending
    Into the Breach goes fullscreen by first maximizing, then setting the
    window to fullscreen in a separate call. Mutter processes the maximize
    request _after_ Wine has sent the fullscreen request. As a result, we
    get a ConfigureNotify for the size of the workspace when we expect the
    window to be fullscreened. We then notify ITB that it is no longer
    fullscreen, which begins the process over again, causing an infinite
    loop.
    
    This fixes that by setting a flag if we have a fullscreen request
    pending and ignoring ConfigureNotify requests if it is set. We unset it
    when we receive a _NET_WM_STATE PropertyNotify event that contains the
    FULLSCREEN flag.

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -1010,6 +1010,12 @@ void update_net_wm_states( struct x11drv_win_data *data )
                    i, data->hwnd, data->whole_window,
                    (new_state & (1 << i)) != 0, (data->net_wm_state & (1 << i)) != 0 );
 
+            if(i == NET_WM_STATE_FULLSCREEN)
+            {
+                data->pending_fullscreen = (new_state & (1 << i)) != 0;
+                TRACE("set pending_fullscreen to: %u\n", data->pending_fullscreen);
+            }
+
             xev.xclient.data.l[0] = (new_state & (1 << i)) ? _NET_WM_STATE_ADD : _NET_WM_STATE_REMOVE;
             xev.xclient.data.l[1] = X11DRV_Atoms[net_wm_state_atoms[i] - FIRST_XATOM];
             xev.xclient.data.l[2] = ((net_wm_state_atoms[i] == XATOM__NET_WM_STATE_MAXIMIZED_VERT) ?
@@ -1140,6 +1146,7 @@ static void unmap_window( HWND hwnd )
 
         data->mapped = FALSE;
         data->net_wm_state = 0;
+        data->pending_fullscreen = FALSE;
     }
     release_win_data( data );
 }
@@ -1156,6 +1163,7 @@ void make_window_embedded( struct x11drv_win_data *data )
         if (!data->managed) XUnmapWindow( data->display, data->whole_window );
         else XWithdrawWindow( data->display, data->whole_window, data->vis.screen );
         data->net_wm_state = 0;
+        data->pending_fullscreen = FALSE;
     }
     data->embedded = TRUE;
     data->managed = TRUE;
@@ -1652,6 +1660,7 @@ static void destroy_whole_window( struct x11drv_win_data *data, BOOL already_des
     data->whole_colormap = 0;
     data->wm_state = WithdrawnState;
     data->net_wm_state = 0;
+    data->pending_fullscreen = FALSE;
     data->mapped = FALSE;
     if (data->xic)
     {
