f80e4109a9ab24e921738f2206dfaa8ab786b9bd
Zebediah Figura <z.figura12@gmail.com>
2019-03-03 11:23:23 -0600

    ntdll: Implement NtQuerySemaphore().

--- dlls/ntdll/unix/fsync.h
+++ dlls/ntdll/unix/fsync.h
@@ -27,6 +27,7 @@ extern NTSTATUS fsync_create_semaphore(HANDLE *handle, ACCESS_MASK access,
 extern NTSTATUS fsync_release_semaphore( HANDLE handle, ULONG count, ULONG *prev );
 extern NTSTATUS fsync_open_semaphore( HANDLE *handle, ACCESS_MASK access,
     const OBJECT_ATTRIBUTES *attr );
+extern NTSTATUS fsync_query_semaphore( HANDLE handle, void *info, ULONG *ret_len );
 extern NTSTATUS fsync_create_event( HANDLE *handle, ACCESS_MASK access,
     const OBJECT_ATTRIBUTES *attr, EVENT_TYPE type, BOOLEAN initial );
 extern NTSTATUS fsync_open_event( HANDLE *handle, ACCESS_MASK access,
