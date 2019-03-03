4b185e6851dfc9b7e5c4757d36a5fe7d6859b4ea
Zebediah Figura <z.figura12@gmail.com>
2019-03-03 11:44:00 -0600

    server: Create futex sections for true file objects and use them for directory change notifications.

--- server/fd.c
+++ server/fd.c
@@ -1728,6 +1728,9 @@ static struct fd *alloc_fd_object(void)
     if (do_esync())
         fd->esync_fd = esync_create_fd( 1, 0 );
 
+    if (do_fsync())
+        fd->fsync_idx = fsync_alloc_shm( 1, 0 );
+
     if ((fd->poll_index = add_poll_user( fd )) == -1)
     {
         release_object( fd );
