f8174055da03439660e44d9e1d18c3478221ac88
Zebediah Figura <zfigura@codeweavers.com>
2019-06-13 12:59:53 -0500

    ntdll, server: Make extra sure that esync is never used if fsync is used.
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>

--- dlls/ntdll/unix/esync.c
+++ dlls/ntdll/unix/esync.c
@@ -58,6 +58,7 @@
 
 #include "unix_private.h"
 #include "esync.h"
+#include "fsync.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(esync);
 
@@ -67,7 +68,7 @@ int do_esync(void)
     static int do_esync_cached = -1;
 
     if (do_esync_cached == -1)
-        do_esync_cached = getenv("WINEESYNC") && atoi(getenv("WINEESYNC"));
+        do_esync_cached = getenv("WINEESYNC") && atoi(getenv("WINEESYNC")) && !do_fsync();
 
     return do_esync_cached;
 #else
