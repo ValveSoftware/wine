3427118692a5c894e3d517205c3f02602ba5f0ab
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2019-06-24 00:22:51 +0000

    wininet: Always set last error in HttpQueryInfoW.
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 3522b127a3a16949df16d023cc91d7dbbea1a8c5)

--- dlls/wininet/http.c
+++ dlls/wininet/http.c
@@ -3911,8 +3911,8 @@ lend:
          WININET_Release( &request->hdr );
 
     TRACE("%u <--\n", res);
-    if(res != ERROR_SUCCESS)
-        SetLastError(res);
+
+    SetLastError(res);
     return res == ERROR_SUCCESS;
 }
 
