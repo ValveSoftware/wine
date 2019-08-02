b48592d4f13b29d90a02ce1eabe718638a390470
Rémi Bernon <rbernon@codeweavers.com>
2019-08-02 02:24:32 -0400

    winex11.drv: Advertise XInput2 version 2.1 support.
    Under XInput2 protocol version < 2.1, RawEvents are not supposed to be
    sent if a pointer grab is active. However slave device events are still
    received regardless of this specification and Wine implemented a
    workaround to receive RawEvents during pointer grabs by listening to
    these slave device events. Then, as soon as a mouse button is pressed
    only the grabbing client will receive the raw motion events.
    
    By advertising the support of XInput2 version >= 2.1, where RawEvents
    are sent even during pointer grabs, we ensure to receive the RawMotion
    events from the desktop window thread, even if a mouse grab is active.
    
    It is now also possible to simplify the code by listening to master
    device events only and get rid of slave device id tracking.

--- dlls/winex11.drv/x11drv.h
+++ dlls/winex11.drv/x11drv.h
@@ -340,12 +340,9 @@ struct x11drv_thread_data
     DWORD    clip_reset;           /* time when clipping was last reset */
     HKL      kbd_layout;           /* active keyboard layout */
     enum { xi_unavailable = -1, xi_unknown, xi_disabled, xi_enabled } xi2_state; /* XInput2 state */
-    void    *xi2_devices;          /* list of XInput2 devices (valid when state is enabled) */
-    int      xi2_device_count;
     struct x11drv_valuator_data x_rel_valuator;
     struct x11drv_valuator_data y_rel_valuator;
     int      xi2_core_pointer;     /* XInput2 core pointer id */
-    int      xi2_current_slave;    /* Current slave driving the Core pointer */
 };
 
 extern struct x11drv_thread_data *x11drv_init_thread_data(void) DECLSPEC_HIDDEN;
