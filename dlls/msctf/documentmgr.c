def8df2a0ad8b15325a166b8d262b40bb13535da
Zhiyi Zhang <zzhang@codeweavers.com>
2019-08-16 13:25:32 +0000

    msctf: Check TF_GetThreadMgr error in DocumentMgr_Destructor.
    Thread managers could be already destroyed when calling DocumentMgr_Destructor.
    
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

--- dlls/msctf/documentmgr.c
+++ dlls/msctf/documentmgr.c
@@ -80,8 +80,11 @@ static void DocumentMgr_Destructor(DocumentMgr *This)
     ITfThreadMgr *tm;
     TRACE("destroying %p\n", This);
 
-    TF_GetThreadMgr(&tm);
-    ThreadMgr_OnDocumentMgrDestruction(tm, &This->ITfDocumentMgr_iface);
+    if (SUCCEEDED(TF_GetThreadMgr(&tm)))
+    {
+        ThreadMgr_OnDocumentMgrDestruction(tm, &This->ITfDocumentMgr_iface);
+        ITfThreadMgr_Release(tm);
+    }
 
     if (This->contextStack[0])
         ITfContext_Release(This->contextStack[0]);
