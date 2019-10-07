cde1dec08778856f1e1ef1ff9b31561677f6de67
Rémi Bernon <rbernon@codeweavers.com>
2019-10-07 13:03:43 +0200

    winex11.drv: Release pointer grab on FocusOut events.
    When a window receives FocusOut event, whether it is because the WM is
    grabbing the keyboard or because of an actual input focus change, we
    should release the pointer grab.
    
    We will re-apply it on FocusIn event if it is necessary from Wine's
    perspective.

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -955,6 +955,8 @@ static BOOL X11DRV_FocusOut( HWND hwnd, XEvent *xev )
         break;
     }
 
+    if (hwnd == GetForegroundWindow()) ungrab_clipping_window();
+
     /* ignore wm specific NotifyUngrab / NotifyGrab events w.r.t focus */
     if (event->mode == NotifyGrab || event->mode == NotifyUngrab) return FALSE;
 
