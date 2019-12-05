36c61c095cf1b32c8177a4fc724073ab3eed4ef8
Derek Lesho <dlesho@codeweavers.com>
2019-12-05 12:41:57 -0600

    bcrypt: Fallback to _gnutls_decode_ber_rs_raw if gnutls_decode_rs_value is unavailable.
    This can be removed when the runtime ships gnutls >= 3.6

--- dlls/bcrypt/gnutls.c
+++ dlls/bcrypt/gnutls.c
@@ -267,8 +267,11 @@ BOOL gnutls_initialize(void)
     }
     if (!(pgnutls_decode_rs_value = wine_dlsym( libgnutls_handle, "gnutls_decode_rs_value", NULL, 0 )))
     {
-        WARN("gnutls_decode_rs_value not found\n");
-        pgnutls_decode_rs_value = compat_gnutls_decode_rs_value;
+        if (!(pgnutls_decode_rs_value = wine_dlsym( libgnutls_handle, "_gnutls_decode_ber_rs_raw", NULL, 0)))
+        {
+            WARN("gnutls_decode_rs_value and legacy alternative _gnutls_decode_ber_rs_raw not found\n");
+            pgnutls_decode_rs_value = compat_gnutls_decode_rs_value;
+        }
     }
 
     if (TRACE_ON( bcrypt ))
