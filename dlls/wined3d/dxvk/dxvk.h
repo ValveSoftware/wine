154d2f3e258875f339b368c6c707eeda6be8ef7c
Andrew Eikum <aeikum@codeweavers.com>
2019-12-10 09:17:13 -0600

    wined3d: Use dxvk_config library to load DXVK options

--- dlls/wined3d/dxvk/dxvk.h
+++
@@ -1,9 +0,0 @@
-/* C-lang interface to DXVK's C++ implementation */
-
-struct DXVKOptions {
-    int32_t customVendorId;
-    int32_t customDeviceId;
-    int nvapiHack;
-};
-
-void dxvk_get_options(struct DXVKOptions *);
