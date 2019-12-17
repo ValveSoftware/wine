2327ef974507485642de1331beeed850d92f1445
Rémi Bernon <rbernon@codeweavers.com>
2019-12-17 16:58:07 +0100

    Revert "winex11.drv: Only call XWarpPointer if we can get exclusive pointer grab."
    This reverts commit 74efb3e872aebf57a42d62b52e149ae26f320c9a.
    
    We are now only activating windows only once the window manager has
    released its grab, it should be safer to let them move the pointer
    around and this should not be needed anymore.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47771
    CW-Bug-Id: #21879

--- dlls/winex11.drv/mouse.c
+++ dlls/winex11.drv/mouse.c
@@ -1461,23 +1461,10 @@ BOOL X11DRV_SetCursorPos( INT x, INT y )
         return FALSE;
     }
 
-    if (!clipping_cursor &&
-        XGrabPointer( data->display, root_window, False,
-                      PointerMotionMask | ButtonPressMask | ButtonReleaseMask,
-                      GrabModeAsync, GrabModeAsync, None, None, CurrentTime ) != GrabSuccess)
-    {
-        WARN( "refusing to warp pointer to %u, %u without exclusive grab\n", (int)pos.x, (int)pos.y );
-        return FALSE;
-    }
-
     TRACE( "real setting to %s\n", wine_dbgstr_point( &pos ) );
 
     XWarpPointer( data->display, root_window, root_window, 0, 0, 0, 0, pos.x, pos.y );
     data->warp_serial = NextRequest( data->display );
-
-    if (!clipping_cursor)
-        XUngrabPointer( data->display, CurrentTime );
-
     XNoOp( data->display );
     XFlush( data->display ); /* avoids bad mouse lag in games that do their own mouse warping */
     TRACE( "warped to (fake) %d,%d serial %lu\n", x, y, data->warp_serial );
