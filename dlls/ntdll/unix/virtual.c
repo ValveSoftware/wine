87cb261f2ef0c322107ee44a44391d72f999cf33
Paul Gofman <pgofman@codeweavers.com>
2020-01-14 21:39:23 +0300

    ntdll: Increase step after failed map attempt in try_map_free_area().

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -1075,6 +1075,7 @@ static void* try_map_free_area( void *base, void *end, ptrdiff_t step,
             step == 0)
             break;
         start = (char *)start + step;
+        step *= 2;
     }
 
     return NULL;
