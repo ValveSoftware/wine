8e7f11892858dbd06786bec6520668390a972ea6
Andrew Eikum <aeikum@codeweavers.com>
2020-01-30 10:16:19 -0600

    winegstreamer: HACK: Use a different gst registry file per architecture

--- dlls/winegstreamer/wg_parser.c
+++ dlls/winegstreamer/wg_parser.c
@@ -1949,6 +1949,22 @@ static void init_gstreamer_once(void)
     int argc = ARRAY_SIZE(args) - 1;
     char **argv = args;
     GError *err;
+    const char *e;
+
+    if ((e = getenv("WINE_GST_REGISTRY_DIR")))
+    {
+        char gst_reg[PATH_MAX];
+#if defined(__x86_64__)
+        const char *arch = "/registry.x86_64.bin";
+#elif defined(__i386__)
+        const char *arch = "/registry.i386.bin";
+#else
+#error Bad arch
+#endif
+        strcpy(gst_reg, e);
+        strcat(gst_reg, arch);
+        setenv("GST_REGISTRY_1_0", gst_reg, 1);
+    }
 
     if (!gst_init_check(&argc, &argv, &err))
     {
