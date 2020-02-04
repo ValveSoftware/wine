0c810c898d8c071a59dcf8274c3e496f95581f8f
Zhiyi Zhang <zzhang@codeweavers.com>
2020-02-04 15:42:20 +0800

    user32: Send a WM_ACTIVATE message after restoring a minimized window.
    The WmShowRestoreMinimizedOverlappedSeq message sequence in tests
    clearly show that there is a WM_ACTIVATE message at the end of
    ShowWindow() calls after restoring a minimized window, and it's
    not from SetFocus().
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47507
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

--- dlls/user32/winpos.c
+++ dlls/user32/winpos.c
@@ -1182,7 +1182,14 @@ static BOOL show_window( HWND hwnd, INT cmd )
     else WIN_ReleasePtr( wndPtr );
 
     /* if previous state was minimized Windows sets focus to the window */
-    if (style & WS_MINIMIZE) SetFocus( hwnd );
+    if (style & WS_MINIMIZE)
+    {
+        SetFocus( hwnd );
+        /* Send a WM_ACTIVATE message for a top level window, even if the window is already active */
+        style = GetWindowLongW( hwnd, GWL_STYLE );
+        if (!(style & WS_CHILD) && !(swp & SWP_NOACTIVATE))
+            SendMessageW( hwnd, WM_ACTIVATE, WA_ACTIVE, 0 );
+    }
 
 done:
     SetThreadDpiAwarenessContext( context );
