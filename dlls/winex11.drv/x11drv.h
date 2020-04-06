6ddffafc2ccf14dc4ad41a682f3fd9b3362140ab
Zhiyi Zhang <zzhang@codeweavers.com>
2020-04-06 15:30:40 +0800

    winex11.drv: Bypass compositor only when a window is full virtual screen.
    Bypass compositor only when a window is full virtual screen. Otherwise, it might cause flicking on
    other monitors.
    
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

--- dlls/winex11.drv/x11drv.h
+++ dlls/winex11.drv/x11drv.h
@@ -726,6 +726,7 @@ extern void X11DRV_expect_error( Display *display, x11drv_error_callback callbac
 extern int X11DRV_check_error(void) DECLSPEC_HIDDEN;
 extern void X11DRV_X_to_window_rect( struct x11drv_win_data *data, RECT *rect, int x, int y, int cx, int cy ) DECLSPEC_HIDDEN;
 extern BOOL is_window_rect_full_screen( const RECT *rect ) DECLSPEC_HIDDEN;
+extern BOOL is_window_rect_full_virtual_screen( const RECT *rect ) DECLSPEC_HIDDEN;
 extern POINT virtual_screen_to_root( INT x, INT y ) DECLSPEC_HIDDEN;
 extern POINT root_to_virtual_screen( INT x, INT y ) DECLSPEC_HIDDEN;
 extern RECT get_virtual_screen_rect(void) DECLSPEC_HIDDEN;
