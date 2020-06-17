e00dd154f1ef9e452910a80bde19f3cf7183a79d
Brendan Shanks <bshanks@codeweavers.com>
2020-06-17 14:44:25 -0700

    amd_ags_x64: Update to 5.3.0.

--- dlls/amd_ags_x64/amd_ags_x64_main.c
+++ dlls/amd_ags_x64/amd_ags_x64_main.c
@@ -24,6 +24,7 @@ enum amd_ags_version
     AMD_AGS_VERSION_5_1_1,
     AMD_AGS_VERSION_5_2_0,
     AMD_AGS_VERSION_5_2_1,
+    AMD_AGS_VERSION_5_3_0,
 
     AMD_AGS_VERSION_COUNT
 };
@@ -39,6 +40,7 @@ static const amd_ags_versions[AMD_AGS_VERSION_COUNT] =
     {5, 1, 1},
     {5, 2, 0},
     {5, 2, 1},
+    {5, 3, 0},
 };
 
 struct AGSContext
@@ -152,6 +154,7 @@ static AGSReturnCode init_ags_context(AGSContext *context)
             break;
         case AMD_AGS_VERSION_5_2_0:
         case AMD_AGS_VERSION_5_2_1:
+        case AMD_AGS_VERSION_5_3_0:
         default:
             device->agsDeviceInfo520.adapterString = vk_properties->deviceName;
             device->agsDeviceInfo520.vendorId = vk_properties->vendorID;
