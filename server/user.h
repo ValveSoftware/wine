d8bac9f82e8b9c862de5ec535796e7d554aa7648
Huw Davies <huw@codeweavers.com>
2020-06-30 15:42:25 +0100

    server: Use a separate variable to determine the message on Alt release.
    Signed-off-by: Huw Davies <huw@codeweavers.com>

--- server/user.h
+++ server/user.h
@@ -71,6 +71,7 @@ struct desktop
     user_handle_t                          cursor_win;       /* window that contains the cursor */
     struct object                         *shared_mapping;   /* desktop shared memory mapping */
     volatile struct desktop_shared_memory *shared;           /* desktop shared memory ptr */
+    unsigned int                           last_press_alt:1; /* last key press was Alt (used to determine msg on Alt release) */
 };
 
 /* user handles functions */
