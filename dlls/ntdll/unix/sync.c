9240daa73dd020cee0ab2571a24948d3e35256e7
Zebediah Figura <zfigura@codeweavers.com>
2020-07-06 12:09:22 -0500

    ntdll: Create eventfd-based objects for semaphores.

--- dlls/ntdll/unix/sync.c
+++ dlls/ntdll/unix/sync.c
@@ -64,6 +64,7 @@
 #include "wine/server.h"
 #include "wine/debug.h"
 #include "unix_private.h"
+#include "esync.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(sync);
 
@@ -261,6 +262,9 @@ NTSTATUS WINAPI NtCreateSemaphore( HANDLE *handle, ACCESS_MASK access, const OBJ
     *handle = 0;
     if (max <= 0 || initial < 0 || initial > max) return STATUS_INVALID_PARAMETER;
     if ((ret = alloc_object_attributes( attr, &objattr, &len ))) return ret;
+
+    if (do_esync())
+        return esync_create_semaphore( handle, access, attr, initial, max );
 
     SERVER_START_REQ( create_semaphore )
     {
