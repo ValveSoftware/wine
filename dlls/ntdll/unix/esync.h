2efbebccaf4811f0be28e11e7a9bef6222a9230b
Zebediah Figura <zfigura@codeweavers.com>
2020-07-06 16:34:56 -0500

    ntdll, server: Implement NtCreateMutant().
    Wine-Staging: eventfd_synchronization

--- dlls/ntdll/unix/esync.h
+++ dlls/ntdll/unix/esync.h
@@ -31,6 +31,9 @@ extern NTSTATUS esync_create_event( HANDLE *handle, ACCESS_MASK access,
 extern NTSTATUS esync_reset_event( HANDLE handle );
 extern NTSTATUS esync_set_event( HANDLE handle );
 
+extern NTSTATUS esync_create_mutex( HANDLE *handle, ACCESS_MASK access,
+    const OBJECT_ATTRIBUTES *attr, BOOLEAN initial );
+
 extern NTSTATUS esync_wait_objects( DWORD count, const HANDLE *handles, BOOLEAN wait_any,
                                     BOOLEAN alertable, const LARGE_INTEGER *timeout );
 
