8b6b07ac867b53b19a217a094bfc9550325120a0
Zebediah Figura <zfigura@codeweavers.com>
2020-07-06 12:09:22 -0500

    ntdll: Create eventfd-based objects for semaphores.

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -89,6 +89,7 @@
 #include "winioctl.h"
 #include "winternl.h"
 #include "unix_private.h"
+#include "esync.h"
 #include "wine/list.h"
 #include "wine/debug.h"
 
@@ -2084,6 +2085,7 @@ static void start_main_thread(void)
     signal_init_thread( teb );
     dbg_init();
     startup_info_size = server_init_process();
+    esync_init();
     virtual_map_user_shared_data();
     init_cpu_info();
     init_files();
