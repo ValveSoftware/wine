74d8909637ad9b67615c5f957dcdb6f8097b7720
Huw Davies <huw@codeweavers.com>
2020-07-08 16:07:08 +0100

    server: Don't return the desktop keystate from the send_hardware_message request.
    Signed-off-by: Huw Davies <huw@codeweavers.com>

--- server/queue.c
+++ server/queue.c
@@ -2691,7 +2691,6 @@ DECL_HANDLER(send_hardware_message)
     struct desktop *desktop;
     unsigned int origin = (req->flags & SEND_HWMSG_INJECTED ? IMO_INJECTED : IMO_HARDWARE);
     struct msg_queue *sender = get_current_queue();
-    data_size_t size = min( 256, get_reply_max_size() );
 
     if (!(desktop = get_thread_desktop( current, 0 ))) return;
 
@@ -2727,7 +2726,6 @@ DECL_HANDLER(send_hardware_message)
 
     reply->new_x = desktop->shared->cursor.x;
     reply->new_y = desktop->shared->cursor.y;
-    set_reply_data( (void *)desktop->shared->keystate, size );
     release_object( desktop );
 }
 
