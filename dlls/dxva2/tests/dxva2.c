d6646652a2f5f85d51fab5df3383f04bef713a18
Nikolay Sivov <nsivov@codeweavers.com>
2020-10-21 09:46:28 +0300

    dxva2: Fix valid handle range test.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 2b04143e6dfb4969b28b76a4cdd00ee85ebb9be2)

--- dlls/dxva2/tests/dxva2.c
+++ dlls/dxva2/tests/dxva2.c
@@ -92,6 +92,9 @@ static void test_device_manager(void)
     hr = IDirect3DDeviceManager9_LockDevice(manager, 0, &device2, FALSE);
     ok(hr == DXVA2_E_NOT_INITIALIZED, "Unexpected hr %#x.\n", hr);
 
+    hr = IDirect3DDeviceManager9_CloseDeviceHandle(manager, 0);
+    ok(hr == E_HANDLE, "Unexpected hr %#x.\n", hr);
+
     /* Invalid token. */
     hr = IDirect3DDeviceManager9_ResetDevice(manager, device, token + 1);
     ok(hr == E_INVALIDARG, "Unexpected hr %#x.\n", hr);
@@ -100,9 +103,13 @@ static void test_device_manager(void)
     ok(hr == S_OK, "Unexpected hr %#x.\n", hr);
     refcount = get_refcount((IUnknown *)device);
 
+    hr = IDirect3DDeviceManager9_CloseDeviceHandle(manager, 0);
+    ok(hr == E_HANDLE, "Unexpected hr %#x.\n", hr);
+
     handle1 = NULL;
     hr = IDirect3DDeviceManager9_OpenDeviceHandle(manager, &handle1);
     ok(hr == S_OK, "Unexpected hr %#x.\n", hr);
+    ok(!!handle1, "Unexpected handle value.\n");
 
     refcount2 = get_refcount((IUnknown *)device);
     ok(refcount2 == refcount, "Unexpected refcount %d.\n", refcount);
@@ -127,6 +134,9 @@ static void test_device_manager(void)
     ok(hr == S_OK, "Unexpected hr %#x.\n", hr);
 
     hr = IDirect3DDeviceManager9_TestDevice(manager, handle1);
+    ok(hr == E_HANDLE, "Unexpected hr %#x.\n", hr);
+
+    hr = IDirect3DDeviceManager9_TestDevice(manager, 0);
     ok(hr == E_HANDLE, "Unexpected hr %#x.\n", hr);
 
     handle = NULL;
