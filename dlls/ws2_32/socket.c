10ef8b8625419966943c6d8907069e11fb29e0ed
Paul Gofman <pgofman@codeweavers.com>
2020-11-04 18:08:21 +0300

    ws2_32: HACK Fail 'download-alt.easyanticheat.net' DNS name resolution.
    This has the same effect as the following reverted ECDHE-ECDSA patch,
    but allows those ciphers to be used for other programs.

--- dlls/ws2_32/socket.c
+++ dlls/ws2_32/socket.c
@@ -6304,6 +6304,13 @@ struct WS_hostent* WINAPI WS_gethostbyname(const char* name)
         extrabuf=HeapAlloc(GetProcessHeap(),0,ebufsize) ;
         while(extrabuf) {
             int res = gethostbyname_r(name, &hostentry, extrabuf, ebufsize, &host, &locerr);
+
+            if (!strcmp(name, "download-alt.easyanticheat.net"))
+            {
+                ERR("HACK: failing download-alt.easyanticheat.net resolution.\n");
+                res = HOST_NOT_FOUND;
+            }
+
             if( res != ERANGE) break;
             ebufsize *=2;
             extrabuf=HeapReAlloc(GetProcessHeap(),0,extrabuf,ebufsize) ;
@@ -6565,6 +6572,13 @@ int WINAPI WS_getaddrinfo(LPCSTR nodename, LPCSTR servname, const struct WS_addr
     *res = NULL;
     if (!nodename && !servname)
     {
+        SetLastError(WSAHOST_NOT_FOUND);
+        return WSAHOST_NOT_FOUND;
+    }
+
+    if (nodename && !strcmp(nodename, "download-alt.easyanticheat.net"))
+    {
+        ERR("HACK: failing download-alt.easyanticheat.net resolution.\n");
         SetLastError(WSAHOST_NOT_FOUND);
         return WSAHOST_NOT_FOUND;
     }
