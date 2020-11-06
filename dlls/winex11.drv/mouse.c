849434cf8abac5114c8cae4b62620f16858a169e
Andrew Eikum <aeikum@codeweavers.com>
2020-11-06 12:20:16 -0600

    Revert "winex11.drv: Interpret mouse 6/7 as horiz scroll."
    This reverts commit 893080e4df5a45929320ebb88b8668eea316476c.
    
    We have a trade-off here between supporting horizontal scroll, or
    mapping those actions to the XBUTTON{1,2} messages. Some users were
    using those buttons in their games, which broke with this change. It
    seems more likely that the extra button mapping will be useful than
    horizontal scrolling, so let's revert this commit.

--- dlls/winex11.drv/mouse.c
+++ dlls/winex11.drv/mouse.c
@@ -78,8 +78,8 @@ static const UINT button_down_flags[NB_BUTTONS] =
     MOUSEEVENTF_RIGHTDOWN,
     MOUSEEVENTF_WHEEL,
     MOUSEEVENTF_WHEEL,
-    MOUSEEVENTF_HWHEEL,
-    MOUSEEVENTF_HWHEEL,
+    MOUSEEVENTF_XDOWN,  /* FIXME: horizontal wheel */
+    MOUSEEVENTF_XDOWN,
     MOUSEEVENTF_XDOWN,
     MOUSEEVENTF_XDOWN
 };
@@ -91,8 +91,8 @@ static const UINT button_up_flags[NB_BUTTONS] =
     MOUSEEVENTF_RIGHTUP,
     0,
     0,
-    0,
-    0,
+    MOUSEEVENTF_XUP,
+    MOUSEEVENTF_XUP,
     MOUSEEVENTF_XUP,
     MOUSEEVENTF_XUP
 };
@@ -104,8 +104,8 @@ static const UINT button_down_data[NB_BUTTONS] =
     0,
     WHEEL_DELTA,
     -WHEEL_DELTA,
-    -WHEEL_DELTA,
-    WHEEL_DELTA,
+    XBUTTON1,
+    XBUTTON2,
     XBUTTON1,
     XBUTTON2
 };
@@ -117,8 +117,8 @@ static const UINT button_up_data[NB_BUTTONS] =
     0,
     0,
     0,
-    0,
-    0,
+    XBUTTON1,
+    XBUTTON2,
     XBUTTON1,
     XBUTTON2
 };
