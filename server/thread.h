f7a7955b17e6ca31c95f2ea16abc0a2997455c45
Rémi Bernon <rbernon@codeweavers.com>
2020-11-21 00:20:27 +0100

    server: Create a thread queue shared mapping.

--- server/thread.h
+++ server/thread.h
@@ -95,6 +95,8 @@ struct thread
     struct list            kernel_object; /* list of kernel object pointers */
     data_size_t            desc_len;      /* thread description length in bytes */
     WCHAR                 *desc;          /* thread description string */
+    struct object         *queue_shared_mapping; /* thread queue shared memory mapping */
+    volatile struct queue_shared_memory *queue_shared;  /* thread queue shared memory ptr */
 };
 
 extern struct thread *current;
