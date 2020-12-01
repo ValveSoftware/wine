ValveSoftware/wine

commit fe51369b94c4766450fb45e1510e7ca8b435c601

winex11.drv: Implement multi-monitor fullscreen hack.
Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

dlls/winex11.drv/xrender.c
@@ -1462,65 +1462,75 @@ static void multiply_alpha( Picture pict, XRenderPictFormat *format, int alpha,
 }
 
 /* if we are letterboxing, draw black bars */
-static void fs_hack_draw_black_bars( Picture dst_pict )
+static void fs_hack_draw_black_bars( HMONITOR monitor, Picture dst_pict )
 {
     static const XRenderColor black = { 0, 0, 0, 0xffff };
     POINT tl, br;   /* top-left / bottom-right */
-    POINT real_mode = fs_hack_real_mode();
-    POINT size = fs_hack_get_scaled_screen_size();
+    RECT user_rect = fs_hack_current_mode(monitor);
+    RECT real_rect = fs_hack_real_mode(monitor);
+    SIZE scaled_screen = fs_hack_get_scaled_screen_size(monitor);
     XRenderPictureAttributes pa;
 
     /* first unclip the picture, so that we can actually draw them */
     pa.clip_mask = None;
     pXRenderChangePicture( gdi_display, dst_pict, CPClipMask, &pa );
 
-    tl.x = tl.y = 0;
-    fs_hack_user_to_real(&tl);
-    br.x = tl.x + size.x;
-    br.y = tl.y + size.y;
+    tl.x = user_rect.left;
+    tl.y = user_rect.top;
+    fs_hack_point_user_to_real(&tl);
+    tl.x = tl.x - real_rect.left;
+    tl.y = tl.y - real_rect.top;
+    br.x = tl.x + scaled_screen.cx;
+    br.y = tl.y + scaled_screen.cy;
 
     if (tl.x > 0)
     {
         /* black bars left & right */
         pXRenderFillRectangle(gdi_display, PictOpSrc, dst_pict, &black,
                 0, 0, /* x, y */
-                tl.x, real_mode.y);    /* w, h */
+                tl.x, real_rect.bottom - real_rect.top);    /* w, h */
         pXRenderFillRectangle(gdi_display, PictOpSrc, dst_pict, &black,
                 br.x, 0,
-                real_mode.x - br.x, real_mode.y);
+                real_rect.right - real_rect.left - br.x, real_rect.bottom - real_rect.top);
     }
     else if (tl.y > 0)
     {
         /* black bars top & bottom */
         pXRenderFillRectangle(gdi_display, PictOpSrc, dst_pict, &black,
                 0, 0,
-                real_mode.x, tl.y);
+                real_rect.right - real_rect.left, tl.y);
         pXRenderFillRectangle(gdi_display, PictOpSrc, dst_pict, &black,
                 0, br.y,
-                real_mode.x, real_mode.y - br.y);
+                real_rect.right - real_rect.left, real_rect.bottom - real_rect.top - br.y);
     }
 }
 
 /* Helper function for (stretched) blitting using xrender */
-static void xrender_blit( int op, Picture src_pict, Picture mask_pict, Picture dst_pict,
+static void xrender_blit( struct xrender_physdev *physdev,
+                          int op, Picture src_pict, Picture mask_pict, Picture dst_pict,
                           int x_src, int y_src, int width_src, int height_src,
                           int x_dst, int y_dst, int width_dst, int height_dst,
                           double xscale, double yscale )
 {
     int x_offset, y_offset;
+    HMONITOR monitor;
 
-    if (fs_hack_mapping_required())
+    monitor = fs_hack_monitor_from_hwnd(WindowFromDC(physdev->dev.hdc));
+    if (fs_hack_mapping_required(monitor))
     {
+        double user_to_real_scale;
         POINT p;
         p.x = x_dst;
         p.y = y_dst;
-        fs_hack_user_to_real(&p);
+        fs_hack_point_user_to_real(&p);
         x_dst = p.x;
         y_dst = p.y;
-        width_dst *= fs_hack_user_to_real_w;
-        height_dst *= fs_hack_user_to_real_h;
-        xscale /= fs_hack_user_to_real_w;
-        yscale /= fs_hack_user_to_real_h;
+
+        user_to_real_scale = fs_hack_get_user_to_real_scale(monitor);
+        width_dst *= user_to_real_scale;
+        height_dst *= user_to_real_scale;
+        xscale /= user_to_real_scale;
+        yscale /= user_to_real_scale;
     }
 
     if (width_src < 0)
@@ -1563,8 +1573,8 @@ static void xrender_blit( int op, Picture src_pict, Picture mask_pict, Picture d
     pXRenderComposite( gdi_display, op, src_pict, mask_pict, dst_pict,
                        x_offset, y_offset, 0, 0, x_dst, y_dst, width_dst, height_dst );
 
-    if (fs_hack_mapping_required())
-        fs_hack_draw_black_bars( dst_pict );
+    if (fs_hack_mapping_required( monitor ))
+        fs_hack_draw_black_bars( monitor, dst_pict );
 }
 
 /* Helper function for (stretched) mono->color blitting using xrender */
@@ -1721,7 +1731,7 @@ static void xrender_stretch_blit( struct xrender_physdev *physdev_src, struct xr
         if (physdev_dst->pict_format->depth == 32 && physdev_src->pict_format->depth < 32)
             mask_pict = get_no_alpha_mask();
 
-        xrender_blit( PictOpSrc, src_pict, mask_pict, dst_pict,
+        xrender_blit( physdev_dst, PictOpSrc, src_pict, mask_pict, dst_pict,
                       physdev_src->x11dev->dc_rect.left + src->x,
                       physdev_src->x11dev->dc_rect.top + src->y,
                       src->width, src->height, x_dst, y_dst, dst->width, dst->height, xscale, yscale );
@@ -1768,7 +1778,7 @@ static void xrender_put_image( Pixmap src_pixmap, Picture src_pict, Picture mask
     }
     else xscale = yscale = 1;  /* no scaling needed with a repeating source */
 
-    xrender_blit( PictOpSrc, src_pict, mask_pict, dst_pict, src->x, src->y, src->width, src->height,
+    xrender_blit( physdev, PictOpSrc, src_pict, mask_pict, dst_pict, src->x, src->y, src->width, src->height,
                   x_dst, y_dst, dst->width, dst->height, xscale, yscale );
 
     if (drawable) pXRenderFreePicture( gdi_display, dst_pict );
@@ -1784,6 +1794,7 @@ static BOOL CDECL xrenderdrv_StretchBlt( PHYSDEV dst_dev, struct bitblt_coords *
     struct xrender_physdev *physdev_dst = get_xrender_dev( dst_dev );
     struct xrender_physdev *physdev_src = get_xrender_dev( src_dev );
     BOOL stretch = (src->width != dst->width) || (src->height != dst->height);
+    HMONITOR monitor;
 
     TRACE("src %d,%d %dx%d vis=%s  dst %d,%d %dx%d vis=%s  rop=%06x\n",
           src->x, src->y, src->width, src->height, wine_dbgstr_rect(&src->visrect),
@@ -1799,7 +1810,8 @@ static BOOL CDECL xrenderdrv_StretchBlt( PHYSDEV dst_dev, struct bitblt_coords *
     if (physdev_dst->format == WXR_FORMAT_MONO && physdev_src->format != WXR_FORMAT_MONO)
         goto x11drv_fallback;
 
-    if (fs_hack_mapping_required())
+    monitor = fs_hack_monitor_from_hwnd(WindowFromDC(dst_dev->hdc));
+    if (fs_hack_mapping_required(monitor))
         stretch = TRUE;
 
     /* if not stretching, we only need to handle format conversion */
@@ -1821,12 +1833,15 @@ static BOOL CDECL xrenderdrv_StretchBlt( PHYSDEV dst_dev, struct bitblt_coords *
         XSetSubwindowMode( gdi_display, tmpGC, IncludeInferiors );
         XSetGraphicsExposures( gdi_display, tmpGC, False );
 
-        if (fs_hack_mapping_required())
+        if (fs_hack_mapping_required( monitor ))
         {
-            unsigned int real_width  = (tmp.visrect.right - tmp.visrect.left) * fs_hack_user_to_real_w;
-            unsigned int real_height = (tmp.visrect.bottom - tmp.visrect.top) * fs_hack_user_to_real_h;
-            tmp_pixmap = XCreatePixmap( gdi_display, root_window, real_width,
-                                        real_height, physdev_dst->pict_format->depth );
+            double user_to_real_scale;
+            SIZE size;
+
+            user_to_real_scale = fs_hack_get_user_to_real_scale( monitor );
+            size.cx = (tmp.visrect.right - tmp.visrect.left) * user_to_real_scale;
+            size.cy = (tmp.visrect.bottom - tmp.visrect.top) * user_to_real_scale;
+            tmp_pixmap = XCreatePixmap( gdi_display, root_window, size.cx, size.cy, physdev_dst->pict_format->depth );
         }
         else
             tmp_pixmap = XCreatePixmap( gdi_display, root_window, tmp.visrect.right - tmp.visrect.left,
@@ -1893,6 +1908,7 @@ static DWORD CDECL xrenderdrv_PutImage( PHYSDEV dev, HRGN clip, BITMAPINFO *info
         if (rop != SRCCOPY)
         {
             BOOL restore_region = add_extra_clipping_region( physdev->x11dev, clip );
+            HMONITOR monitor;
 
             /* make coordinates relative to tmp pixmap */
             tmp = *dst;
@@ -1904,12 +1920,16 @@ static DWORD CDECL xrenderdrv_PutImage( PHYSDEV dev, HRGN clip, BITMAPINFO *info
             XSetSubwindowMode( gdi_display, gc, IncludeInferiors );
             XSetGraphicsExposures( gdi_display, gc, False );
 
-            if (fs_hack_mapping_required())
+            monitor = fs_hack_monitor_from_hwnd( WindowFromDC( dev->hdc ) );
+            if (fs_hack_mapping_required( monitor ))
             {
-                unsigned int real_width  = (tmp.visrect.right - tmp.visrect.left) * fs_hack_user_to_real_w;
-                unsigned int real_height = (tmp.visrect.bottom - tmp.visrect.top) * fs_hack_user_to_real_h;
-                tmp_pixmap = XCreatePixmap( gdi_display, root_window,
-                                            real_width, real_height,
+                double user_to_real_scale;
+                SIZE size;
+
+                user_to_real_scale = fs_hack_get_user_to_real_scale( monitor );
+                size.cx = (tmp.visrect.right - tmp.visrect.left) * user_to_real_scale;
+                size.cy = (tmp.visrect.bottom - tmp.visrect.top) * user_to_real_scale;
+                tmp_pixmap = XCreatePixmap( gdi_display, root_window, size.cx, size.cy,
                                             physdev->pict_format->depth );
             }
             else
@@ -1998,7 +2018,7 @@ static DWORD CDECL xrenderdrv_BlendImage( PHYSDEV dev, BITMAPINFO *info, const s
         EnterCriticalSection( &xrender_cs );
         mask_pict = get_mask_pict( func.SourceConstantAlpha * 257 );
 
-        xrender_blit( PictOpOver, src_pict, mask_pict, dst_pict,
+        xrender_blit( physdev, PictOpOver, src_pict, mask_pict, dst_pict,
                       src->x, src->y, src->width, src->height,
                       physdev->x11dev->dc_rect.left + dst->x,
                       physdev->x11dev->dc_rect.top + dst->y,
@@ -2087,7 +2107,7 @@ static BOOL CDECL xrenderdrv_AlphaBlend( PHYSDEV dst_dev, struct bitblt_coords *
     EnterCriticalSection( &xrender_cs );
     mask_pict = get_mask_pict( blendfn.SourceConstantAlpha * 257 );
 
-    xrender_blit( PictOpOver, src_pict, mask_pict, dst_pict,
+    xrender_blit( physdev_dst, PictOpOver, src_pict, mask_pict, dst_pict,
                   physdev_src->x11dev->dc_rect.left + src->x,
                   physdev_src->x11dev->dc_rect.top + src->y,
                   src->width, src->height,
@@ -2190,7 +2210,7 @@ static BOOL CDECL xrenderdrv_GradientFill( PHYSDEV dev, TRIVERTEX *vert_array, U
             dst_pict = get_xrender_picture( physdev, 0, NULL );
 
             src_pict = pXRenderCreateLinearGradient( gdi_display, &gradient, stops, colors, 2 );
-            xrender_blit( PictOpSrc, src_pict, 0, dst_pict,
+            xrender_blit( physdev, PictOpSrc, src_pict, 0, dst_pict,
                           0, 0, rc.right - rc.left, rc.bottom - rc.top,
                           physdev->x11dev->dc_rect.left + rc.left,
                           physdev->x11dev->dc_rect.top + rc.top,

dlls/winevulkan/make_vulkan
@@ -2576,7 +2576,7 @@ class VkGenerator(object):
         f.write("     * resolution; user_sz will contain the app's requested mode; and dst_blit\n")
         f.write("     * will contain the area to blit the user image to in real coordinates.\n")
         f.write("     * All parameters are optional. */\n")
-        f.write("    VkBool32 (*query_fs_hack)(VkExtent2D *real_sz, VkExtent2D *user_sz, VkRect2D *dst_blit, VkFilter *filter);\n")
+        f.write("    VkBool32 (*query_fs_hack)(VkSurfaceKHR surface, VkExtent2D *real_sz, VkExtent2D *user_sz, VkRect2D *dst_blit, VkFilter *filter);\n")
 
         f.write("};\n\n")
 

dlls/winevulkan/vulkan.c
@@ -1626,7 +1626,7 @@ VkResult WINAPI wine_vkGetPhysicalDeviceSurfaceCapabilitiesKHR(VkPhysicalDevice 
         adjust_max_image_count(phys_dev, capabilities);
 
     if (res == VK_SUCCESS && vk_funcs->query_fs_hack &&
-            vk_funcs->query_fs_hack(NULL, &user_res, NULL, NULL)){
+            vk_funcs->query_fs_hack(surface, NULL, &user_res, NULL, NULL)){
         capabilities->currentExtent = user_res;
         capabilities->minImageExtent = user_res;
         capabilities->maxImageExtent = user_res;
@@ -1649,7 +1649,7 @@ VkResult WINAPI wine_vkGetPhysicalDeviceSurfaceCapabilities2KHR(VkPhysicalDevice
         adjust_max_image_count(phys_dev, &capabilities->surfaceCapabilities);
 
     if (res == VK_SUCCESS && vk_funcs->query_fs_hack &&
-            vk_funcs->query_fs_hack(NULL, &user_res, NULL, NULL)){
+            vk_funcs->query_fs_hack(surface_info->surface, NULL, &user_res, NULL, NULL)){
         capabilities->surfaceCapabilities.currentExtent = user_res;
         capabilities->surfaceCapabilities.minImageExtent = user_res;
         capabilities->surfaceCapabilities.maxImageExtent = user_res;
@@ -2178,7 +2178,7 @@ VkResult WINAPI wine_vkCreateSwapchainKHR(VkDevice device, const VkSwapchainCrea
         our_createinfo.oldSwapchain = ((struct VkSwapchainKHR_T *)(UINT_PTR)our_createinfo.oldSwapchain)->swapchain;
 
     if(vk_funcs->query_fs_hack &&
-            vk_funcs->query_fs_hack(&object->real_extent, &user_sz, &object->blit_dst, &object->fs_hack_filter) &&
+            vk_funcs->query_fs_hack(pCreateInfo->surface, &object->real_extent, &user_sz, &object->blit_dst, &object->fs_hack_filter) &&
             our_createinfo.imageExtent.width == user_sz.width &&
             our_createinfo.imageExtent.height == user_sz.height)
     {

dlls/winex11.drv/Makefile.in
@@ -11,6 +11,7 @@ C_SRCS = \
 	desktop.c \
 	display.c \
 	event.c \
+	fs.c \
 	graphics.c \
 	ime.c \
 	init.c \

dlls/winex11.drv/desktop.c
@@ -310,6 +310,7 @@ void X11DRV_init_desktop( Window win, unsigned int width, unsigned int height )
     settings_handler.free_modes = X11DRV_desktop_free_modes;
     settings_handler.get_current_mode = X11DRV_desktop_get_current_mode;
     settings_handler.set_current_mode = X11DRV_desktop_set_current_mode;
+    settings_handler.convert_coordinates = NULL;
     X11DRV_Settings_SetHandler( &settings_handler );
 }
 
@@ -421,8 +422,7 @@ void X11DRV_resize_desktop( BOOL send_display_change )
 
     if (GetWindowThreadProcessId( hwnd, NULL ) != GetCurrentThreadId())
     {
-        POINT new_mode = fs_hack_current_mode();
-        SendMessageW( hwnd, WM_X11DRV_RESIZE_DESKTOP, MAKEWPARAM(new_mode.x, new_mode.y), (LPARAM)send_display_change );
+        SendMessageW( hwnd, WM_X11DRV_RESIZE_DESKTOP, 0, (LPARAM)send_display_change );
     }
     else
     {

dlls/winex11.drv/display.c
@@ -200,21 +200,33 @@ fail:
 
 POINT virtual_screen_to_root(INT x, INT y)
 {
-    RECT virtual = get_virtual_screen_rect();
+    RECT virtual = fs_hack_get_real_virtual_screen();
     POINT pt;
 
-    pt.x = x - virtual.left;
-    pt.y = y - virtual.top;
+    TRACE("from %d,%d\n", x, y);
+
+    pt.x = x;
+    pt.y = y;
+    fs_hack_point_user_to_real(&pt);
+    TRACE("to real %d,%d\n", pt.x, pt.y);
+
+    pt.x -= virtual.left;
+    pt.y -= virtual.top;
+    TRACE("to root %d,%d\n", pt.x, pt.y);
     return pt;
 }
 
 POINT root_to_virtual_screen(INT x, INT y)
 {
-    RECT virtual = get_virtual_screen_rect();
+    RECT virtual = fs_hack_get_real_virtual_screen();
     POINT pt;
 
+    TRACE("from root %d,%d\n", x, y);
     pt.x = x + virtual.left;
     pt.y = y + virtual.top;
+    TRACE("to real %d,%d\n", pt.x, pt.y);
+    fs_hack_point_real_to_user(&pt);
+    TRACE("to user %d,%d\n", pt.x, pt.y);
     return pt;
 }
 
@@ -329,6 +341,11 @@ void X11DRV_DisplayDevices_SetHandler(const struct x11drv_display_device_handler
     }
 }
 
+struct x11drv_display_device_handler X11DRV_DisplayDevices_GetHandler(void)
+{
+    return host_handler;
+}
+
 void X11DRV_DisplayDevices_RegisterEventHandlers(void)
 {
     struct x11drv_display_device_handler *handler = is_virtual_desktop() ? &desktop_handler : &host_handler;
@@ -341,29 +358,43 @@ static BOOL CALLBACK update_windows_on_display_change(HWND hwnd, LPARAM lparam)
 {
     struct x11drv_win_data *data;
     UINT mask = (UINT)lparam;
+    HMONITOR monitor;
 
     if (!(data = get_win_data(hwnd)))
         return TRUE;
 
-    if (fs_hack_mapping_required() &&
-            fs_hack_matches_current_mode(
+    monitor = fs_hack_monitor_from_hwnd( hwnd );
+    if (fs_hack_mapping_required( monitor ) &&
+            fs_hack_matches_current_mode( monitor,
                 data->whole_rect.right - data->whole_rect.left,
                 data->whole_rect.bottom - data->whole_rect.top)){
-        if (!data->fs_hack){
-            POINT p = fs_hack_real_mode();
-            POINT tl = virtual_screen_to_root(0, 0);
-            TRACE("Enabling fs hack, resizing window %p to (%u,%u)-(%u,%u)\n", hwnd, tl.x, tl.y, p.x, p.y);
+        if(!data->fs_hack){
+            RECT real_rect = fs_hack_real_mode( monitor );
+            MONITORINFO monitor_info;
+            UINT width, height;
+            POINT tl;
+
+            monitor_info.cbSize = sizeof(monitor_info);
+            GetMonitorInfoW( monitor, &monitor_info );
+            tl = virtual_screen_to_root( monitor_info.rcMonitor.left, monitor_info.rcMonitor.top );
+            width = real_rect.right - real_rect.left;
+            height = real_rect.bottom - real_rect.top;
+
+            TRACE("Enabling fs hack, resizing window %p to (%u,%u)-(%u,%u)\n", hwnd, tl.x, tl.y, width, height);
             data->fs_hack = TRUE;
             set_wm_hints( data );
-            XMoveResizeWindow(data->display, data->whole_window, tl.x, tl.y, p.x, p.y);
+            XMoveResizeWindow(data->display, data->whole_window, tl.x, tl.y, width, height);
             if(data->client_window)
-                XMoveResizeWindow(data->display, data->client_window, 0, 0, p.x, p.y);
+                XMoveResizeWindow(data->display, data->client_window, 0, 0, width, height);
             sync_gl_drawable(hwnd, FALSE);
             update_net_wm_states( data );
         }
     } else {
         /* update the full screen state */
         update_net_wm_states(data);
+
+        if (data->fs_hack)
+            mask |= CWX | CWY;
 
         if (mask && data->whole_window)
         {
@@ -374,10 +405,10 @@ static BOOL CALLBACK update_windows_on_display_change(HWND hwnd, LPARAM lparam)
             XReconfigureWMWindow(data->display, data->whole_window, data->vis.screen, mask, &changes);
         }
 
-        if(data->fs_hack &&
-            !fs_hack_matches_current_mode(
+        if(data->fs_hack && (!fs_hack_mapping_required(monitor) ||
+            !fs_hack_matches_current_mode(monitor,
                 data->whole_rect.right - data->whole_rect.left,
-                data->whole_rect.bottom - data->whole_rect.top)){
+                data->whole_rect.bottom - data->whole_rect.top))){
             TRACE("Disabling fs hack\n");
             data->fs_hack = FALSE;
             if(data->client_window){

dlls/winex11.drv/event.c
@@ -1171,11 +1171,14 @@ static BOOL X11DRV_ConfigureNotify( HWND hwnd, XEvent *xev )
     else pos = root_to_virtual_screen( x, y );
 
     if(data->fs_hack){
-        POINT p = fs_hack_current_mode();
-        rect.left = 0;
-        rect.top = 0;
-        rect.right = p.x;
-        rect.bottom = p.y;
+        MONITORINFO monitor_info;
+        HMONITOR monitor;
+
+        monitor = fs_hack_monitor_from_hwnd( hwnd );
+        monitor_info.cbSize = sizeof(monitor_info);
+        GetMonitorInfoW( monitor, &monitor_info );
+        rect = monitor_info.rcMonitor;
+        TRACE( "monitor %p rect: %s\n", monitor, wine_dbgstr_rect(&rect) );
     }else{
         X11DRV_X_to_window_rect( data, &rect, pos.x, pos.y, event->width, event->height );
         if (root_coords) MapWindowPoints( 0, parent, (POINT *)&rect, 2 );

commit f0ed8c8bdea2ff031b945262b037ba998b463410

winex11.drv: Pass a valid struct xrender_physdev pointer to xrender_put_image().
Fix Age of Empires II HD crashes when creating/entering multiplayer room with Proton 5.13-1.
The crash is a regression from fe51369b94c4766450fb45e1510e7ca8b435c601. Based on research
done by Giovanni Mascellani.

Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

dlls/winex11.drv/xrender.c
@@ -1941,7 +1941,7 @@ static DWORD CDECL xrenderdrv_PutImage( PHYSDEV dev, HRGN clip, BITMAPINFO *info
             }
 
             xrender_put_image( src_pixmap, src_pict, mask_pict, NULL, physdev->pict_format,
-                               NULL, tmp_pixmap, src, &tmp, use_repeat );
+                               physdev, tmp_pixmap, src, &tmp, use_repeat );
             execute_rop( physdev->x11dev, tmp_pixmap, gc, &dst->visrect, rop );
 
             XFreePixmap( gdi_display, tmp_pixmap );