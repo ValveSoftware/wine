d9ca86aa9490e0b5cf0ab3a7b56bf4ab69cee234
Rémi Bernon <rbernon@codeweavers.com>
2021-01-08 12:19:47 +0100

    user32: Cleanup thread shared mapping on thread detach.

--- dlls/user32/user_main.c
+++ dlls/user32/user_main.c
@@ -391,6 +391,13 @@ static void thread_detach(void)
         thread_info->desktop_shared_memory = NULL;
     }
 
+    if (thread_info->thread_shared_map)
+    {
+        CloseHandle( thread_info->thread_shared_map );
+        thread_info->thread_shared_map = NULL;
+        thread_info->thread_shared_memory = NULL;
+    }
+
     exiting_thread_id = 0;
 }
 
