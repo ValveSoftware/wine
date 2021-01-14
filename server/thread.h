2d2d0845b145dd4fac23dba261b1166664693058
Rémi Bernon <rbernon@codeweavers.com>
2021-01-14 15:22:05 +0100

    server: Create thread input shared mapping.

--- server/thread.h
+++ server/thread.h
@@ -97,6 +97,8 @@ struct thread
     WCHAR                 *desc;          /* thread description string */
     struct object         *queue_shared_mapping; /* thread queue shared memory mapping */
     volatile struct queue_shared_memory *queue_shared;  /* thread queue shared memory ptr */
+    struct object         *input_shared_mapping; /* thread input shared memory mapping */
+    volatile struct input_shared_memory *input_shared;  /* thread input shared memory ptr */
 };
 
 extern struct thread *current;
