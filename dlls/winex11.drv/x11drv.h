0869d02bc97225460495c986862205bba7b5437e
Andrew Eikum <aeikum@codeweavers.com>
2021-01-22 10:13:33 -0600

    winex11.drv: Determine fs hack status on destination monitor

--- dlls/winex11.drv/x11drv.h
+++ dlls/winex11.drv/x11drv.h
@@ -630,6 +630,7 @@ extern BOOL fs_hack_enabled(HMONITOR monitor) DECLSPEC_HIDDEN;
 extern BOOL fs_hack_mapping_required(HMONITOR monitor) DECLSPEC_HIDDEN;
 extern BOOL fs_hack_is_integer(void) DECLSPEC_HIDDEN;
 extern HMONITOR fs_hack_monitor_from_hwnd(HWND hwnd) DECLSPEC_HIDDEN;
+extern HMONITOR fs_hack_monitor_from_rect(const RECT *rect) DECLSPEC_HIDDEN;
 extern BOOL fs_hack_matches_current_mode(HMONITOR monitor, INT width, INT height) DECLSPEC_HIDDEN;
 extern RECT fs_hack_current_mode(HMONITOR monitor) DECLSPEC_HIDDEN;
 extern RECT fs_hack_real_mode(HMONITOR monitor) DECLSPEC_HIDDEN;
