13a7bdf0121b1f80bd87efda3310a8a5b205826b
Rémi Bernon <rbernon@codeweavers.com>
2021-01-26 14:32:33 +0100

    server: Notify desktop of cursor handle changes.

--- server/user.h
+++ server/user.h
@@ -66,7 +66,9 @@ struct desktop
     struct thread_input                   *foreground_input; /* thread input of foreground thread */
     unsigned int                           users;            /* processes and threads using this desktop */
     unsigned int                           cursor_clip_msg;  /* message to post for cursor clip changes */
+    unsigned int                           cursor_change_msg;/* message to post for cursor win / handle changes */
     user_handle_t                          cursor_win;       /* window that contains the cursor */
+    user_handle_t                          cursor_handle;    /* current cursor handle */
     struct object                         *shared_mapping;   /* desktop shared memory mapping */
     volatile struct desktop_shared_memory *shared;           /* desktop shared memory ptr */
     unsigned int                           last_press_alt:1; /* last key press was Alt (used to determine msg on Alt release) */
