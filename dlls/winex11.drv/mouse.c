c18e239c466ce64168cad04b75b70d8c42d263ad
Rémi Bernon <rbernon@codeweavers.com>
2021-02-01 15:34:05 +0100

    winex11.drv: Don't allow querying pointer while overlay is active.

--- dlls/winex11.drv/mouse.c
+++ dlls/winex11.drv/mouse.c
@@ -1553,6 +1553,8 @@ BOOL CDECL X11DRV_GetCursorPos(LPPOINT pos)
     unsigned int xstate;
     BOOL ret;
 
+    if (WaitForSingleObject(steam_overlay_event, 0) == WAIT_OBJECT_0) return FALSE;
+
     ret = XQueryPointer( display, root_window, &root, &child, &rootX, &rootY, &winX, &winY, &xstate );
     if (ret)
     {
