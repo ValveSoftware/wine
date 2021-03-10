114ccb5b70a313b159e23b2d9e39333cf3b0dc51
Derek Lesho <dlesho@codeweavers.com>
2021-03-10 13:09:51 -0500

    winegstreamer: Introduce H.264 decoder transform.
    Signed-off-by: Derek Lesho <dlesho@codeweavers.com>

--- dlls/winegstreamer/main.c
+++ dlls/winegstreamer/main.c
@@ -529,6 +529,9 @@ HRESULT WINAPI DllRegisterServer(void)
 
     init_gstreamer();
 
+    if (FAILED(hr = mfplat_DllRegisterServer()))
+        return hr;
+
     if (FAILED(hr = __wine_register_resources()))
         return hr;
 
