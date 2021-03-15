018c625215b3bdb0267219e3fcb2867ad78709db
Giovanni Mascellani <gmascellani@codeweavers.com>
2021-03-15 12:01:25 -0500

    winex11.drv: Flush X connection after ungrabbing the pointer.
    CW-Bug-Id: #18169

--- dlls/winex11.drv/mouse.c
+++ dlls/winex11.drv/mouse.c
@@ -497,7 +497,11 @@ void ungrab_clipping_window(void)
 
     TRACE( "no longer clipping\n" );
     XUnmapWindow( display, clip_window );
-    if (clipping_cursor) XUngrabPointer( display, CurrentTime );
+    if (clipping_cursor)
+    {
+        XUngrabPointer( display, CurrentTime );
+        XFlush( display );
+    }
     clipping_cursor = FALSE;
     SendNotifyMessageW( GetDesktopWindow(), WM_X11DRV_CLIP_CURSOR_NOTIFY, 0, 0 );
 }
