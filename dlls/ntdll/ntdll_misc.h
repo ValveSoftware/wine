fde9b2bafc1ceab25e305f7d8ffebbff65cc1d49
Rémi Bernon <rbernon@codeweavers.com>
2021-03-16 15:13:43 +0100

    ntdll: Split RtlAllocateHeap standard implementation.
    CW-Bug-Id: #16549

--- dlls/ntdll/ntdll_misc.h
+++ dlls/ntdll/ntdll_misc.h
@@ -110,6 +110,7 @@ static inline TEB64 *NtCurrentTeb64(void) { return NULL; }
 static inline TEB64 *NtCurrentTeb64(void) { return (TEB64 *)NtCurrentTeb()->GdiBatchCount; }
 #endif
 
+NTSTATUS HEAP_std_allocate( HANDLE heap, ULONG flags, SIZE_T size, void **out );
 NTSTATUS HEAP_std_free( HANDLE heap, ULONG flags, void *ptr );
 NTSTATUS HEAP_std_get_allocated_size( HANDLE heap, ULONG flags, const void *ptr, SIZE_T *out );
 
