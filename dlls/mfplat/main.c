b2a000e7157525361754264aa164af3104c90a09
Andrew Eikum <aeikum@codeweavers.com>
2021-05-04 12:38:39 -0500

    Revert "mfplat: Stub out MFCreateDXGIDeviceManager, to avoid the d3d path."
    This reverts commit 1b6dbd0611bca61c388172861403adf13ac02e1d.

--- dlls/mfplat/main.c
+++ dlls/mfplat/main.c
@@ -8917,8 +8917,6 @@ HRESULT WINAPI MFCreateDXGIDeviceManager(UINT *token, IMFDXGIDeviceManager **man
 
     TRACE("%p, %p.\n", token, manager);
 
-    return E_NOTIMPL;
-
     if (!token || !manager)
         return E_POINTER;
 
