ebededa44515e7a68c8254c329768e2b72eea886
Andrew Eikum <aeikum@codeweavers.com>
2021-05-04 12:38:41 -0500

    Revert "winegstreamer: Activate source pad in push mode if it isn't activated in pull mode."
    This reverts commit 382ca92acf285e23c24f513b411ec220baccf159.

--- dlls/winegstreamer/wg_parser.c
+++ dlls/winegstreamer/wg_parser.c
@@ -58,7 +58,7 @@ struct wg_parser
     pthread_mutex_t mutex;
 
     pthread_cond_t init_cond;
-    bool no_more_pads, has_duration, error, pull_mode;
+    bool no_more_pads, has_duration, error;
 
     pthread_cond_t read_cond, read_done_cond;
     struct
@@ -1383,12 +1383,9 @@ static gboolean src_activate_mode_cb(GstPad *pad, GstObject *parent, GstPadMode 
     GST_DEBUG("%s source pad for parser %p in %s mode.",
             activate ? "Activating" : "Deactivating", parser, gst_pad_mode_get_name(mode));
 
-    parser->pull_mode = false;
-
     switch (mode)
     {
         case GST_PAD_MODE_PULL:
-            parser->pull_mode = activate;
             return TRUE;
         case GST_PAD_MODE_PUSH:
             return activate_push(pad, activate);
@@ -1628,8 +1625,6 @@ static void CDECL wg_parser_disconnect(struct wg_parser *parser)
     pthread_mutex_unlock(&parser->mutex);
 
     gst_element_set_state(parser->container, GST_STATE_NULL);
-    if (!parser->pull_mode)
-        gst_pad_set_active(parser->my_src, 0);
     gst_pad_unlink(parser->my_src, parser->their_sink);
     gst_object_unref(parser->my_src);
     gst_object_unref(parser->their_sink);
@@ -1689,8 +1684,6 @@ static BOOL decodebin_parser_init_gst(struct wg_parser *parser)
     }
 
     gst_element_set_state(parser->container, GST_STATE_PAUSED);
-    if (!parser->pull_mode)
-        gst_pad_set_active(parser->my_src, 1);
     ret = gst_element_get_state(parser->container, NULL, NULL, -1);
     if (ret == GST_STATE_CHANGE_FAILURE)
     {
@@ -1742,8 +1735,6 @@ static BOOL avi_parser_init_gst(struct wg_parser *parser)
     }
 
     gst_element_set_state(parser->container, GST_STATE_PAUSED);
-    if (!parser->pull_mode)
-        gst_pad_set_active(parser->my_src, 1);
     ret = gst_element_get_state(parser->container, NULL, NULL, -1);
     if (ret == GST_STATE_CHANGE_FAILURE)
     {
@@ -1798,8 +1789,6 @@ static BOOL mpeg_audio_parser_init_gst(struct wg_parser *parser)
 
     gst_pad_set_active(stream->my_sink, 1);
     gst_element_set_state(parser->container, GST_STATE_PAUSED);
-    if (!parser->pull_mode)
-        gst_pad_set_active(parser->my_src, 1);
     ret = gst_element_get_state(parser->container, NULL, NULL, -1);
     if (ret == GST_STATE_CHANGE_FAILURE)
     {
@@ -1854,8 +1843,6 @@ static BOOL wave_parser_init_gst(struct wg_parser *parser)
 
     gst_pad_set_active(stream->my_sink, 1);
     gst_element_set_state(parser->container, GST_STATE_PAUSED);
-    if (!parser->pull_mode)
-        gst_pad_set_active(parser->my_src, 1);
     ret = gst_element_get_state(parser->container, NULL, NULL, -1);
     if (ret == GST_STATE_CHANGE_FAILURE)
     {
