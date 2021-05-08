5a7b5dd1d7b6709439017caaa624316388f811b2
Joshua Ashton <joshua@froggi.es>
2021-05-08 05:20:08 +0100

    amd_ags_x64: Implement AGS 6.0.1
    Signed-off-by: Joshua Ashton <joshua@froggi.es>

--- dlls/amd_ags_x64/amd_ags.h
+++ dlls/amd_ags_x64/amd_ags.h
@@ -151,7 +151,7 @@
 
 #define AMD_AGS_VERSION_MAJOR 6             ///< AGS major version
 #define AMD_AGS_VERSION_MINOR 0             ///< AGS minor version
-#define AMD_AGS_VERSION_PATCH 0             ///< AGS patch version
+#define AMD_AGS_VERSION_PATCH 1             ///< AGS patch version
 
 #ifdef __cplusplus
 extern "C" {
@@ -906,8 +906,8 @@ typedef struct AGSDX12ReturnedParams
             unsigned int        baseInstance : 1;                   ///< Supported in Radeon Software Version 20.2.1 onwards.
             unsigned int        getWaveSize : 1;                    ///< Supported in Radeon Software Version 20.5.1 onwards.
             unsigned int        floatConversion : 1;                ///< Supported in Radeon Software Version 20.5.1 onwards.
-            unsigned int        readLaneAt : 1;                     ///< Supported in Radeon Software Version 20.11.1 onwards.
-            unsigned int        rayHitToken : 1;                    ///< Supported in Radeon Software Version 20.11.1 onwards.
+            unsigned int        readLaneAt : 1;                     ///< Supported in Radeon Software Version 20.11.2 onwards.
+            unsigned int        rayHitToken : 1;                    ///< Supported in Radeon Software Version 20.11.2 onwards.
             unsigned int        padding : 20;                       ///< Reserved
         } ExtensionsSupported;
         ExtensionsSupported     extensionsSupported;                ///< List of supported extensions
@@ -1631,7 +1631,7 @@ AMD_AGS_API AGSReturnCode agsDriverExtensionsDX11_SetMaxAsyncCompileThreadCount(
 
 ///
 /// This method can be used to determine the total number of asynchronous shader compile jobs that are either
-/// queued for waiting for compilation or being compiled by the drivers asynchronous compilation threads.
+/// queued for waiting for compilation or being compiled by the driver's asynchronous compilation threads.
 /// This method can be called at any during the lifetime of the driver.
 ///
 /// \param [in] context                             Pointer to a context.
