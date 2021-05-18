6a8e72a1b2461aa99d9f1e0300e869e6743f7f2e
Andrew Eikum <aeikum@codeweavers.com>
2021-05-18 13:34:47 -0500

    HACK: proton: winedbg: Support dumping crash logs to a directory
    CW-Bug-Id: #18944

--- programs/winedbg/debugger.h
+++ programs/winedbg/debugger.h
@@ -283,6 +283,7 @@ extern	DWORD	                dbg_curr_tid;
 extern  dbg_ctx_t               dbg_context;
 extern  BOOL                    dbg_interactiveP;
 extern  HANDLE                  dbg_houtput;
+extern  HANDLE                  dbg_crash_report_file;
 
 struct dbg_internal_var
 {
