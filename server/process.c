98bd5df0ee5b718b8b4f62f54757fc6a4e5e8593
Paul Gofman <pgofman@codeweavers.com>
2021-05-21 15:10:07 +0300

    ntdll: Read process memory on the client side in NtReadVirtualMemory().
    CW-Bug-Id: 18957

--- server/process.c
+++ server/process.c
@@ -1682,6 +1682,8 @@ DECL_HANDLER(read_process_memory)
 
     if (!(process = get_process_from_handle( req->handle, PROCESS_VM_READ ))) return;
 
+    reply->unix_pid = process->unix_pid;
+
     if (len)
     {
         char *buffer = mem_alloc( len );
