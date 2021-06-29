eec9db7246c2435eb1687061441adfc012dcd166
Connor McAdams <cmcadams@codeweavers.com>
2021-06-29 13:00:49 -0400

    uiautomationcore: Add event message queue.
    Signed-off-by: Connor McAdams <cmcadams@codeweavers.com>
    CW-Bug-Id: #18351

--- dlls/uiautomationcore/uia_event.c
+++ dlls/uiautomationcore/uia_event.c
@@ -26,6 +26,59 @@ WINE_DEFAULT_DEBUG_CHANNEL(uiautomation);
 
 DWORD tls_index = TLS_OUT_OF_INDEXES;
 
+static HRESULT uia_evm_add_msaa_event(struct uia_evl *evl, HWND hwnd,
+        LONG obj_id, LONG child_id, LONG event);
+
+static EVENTID uia_msaa_event_to_uia_event_id(LONG obj_id, LONG event)
+{
+    switch (event)
+    {
+    case EVENT_OBJECT_ACCELERATORCHANGE: return UIA_AcceleratorKeyPropertyId;
+    case EVENT_OBJECT_CREATE: return UIA_StructureChangedEventId; /* StructureChangeType_ChildAdded */
+    case EVENT_OBJECT_DESTROY: return UIA_StructureChangedEventId; /* StructureChangeType_ChildRemoved */
+    case EVENT_OBJECT_FOCUS: return UIA_AutomationFocusChangedEventId;
+    case EVENT_OBJECT_HELPCHANGE: return UIA_AutomationPropertyChangedEventId; /* UIA_HelpTextPropertyId change */
+    case EVENT_OBJECT_LOCATIONCHANGE: return UIA_AutomationPropertyChangedEventId; /* UIA_BoundingRectanglePropertyId change */
+    case EVENT_OBJECT_NAMECHANGE: return UIA_AutomationPropertyChangedEventId; /* UIA_NamePropertyId change */
+    case EVENT_OBJECT_PARENTCHANGE: return UIA_StructureChangedEventId; /* unsure of StructureChangeType, needs tests. */
+    case EVENT_OBJECT_REORDER: return UIA_StructureChangedEventId; /* StructureChangeType_ChildrenReordered? */
+    case EVENT_OBJECT_SELECTION: return	UIA_SelectionItem_ElementSelectedEventId;
+    case EVENT_OBJECT_SELECTIONADD: return UIA_SelectionItem_ElementAddedToSelectionEventId;
+    case EVENT_OBJECT_SELECTIONREMOVE: return UIA_SelectionItem_ElementRemovedFromSelectionEventId;
+    case EVENT_OBJECT_HIDE: return UIA_StructureChangedEventId; /* StructureChangeType_ChildRemoved */
+    case EVENT_OBJECT_SHOW: return UIA_StructureChangedEventId; /* StructureChangeType_ChildAdded */
+    case EVENT_OBJECT_STATECHANGE: return UIA_AutomationPropertyChangedEventId; /* Various property-changed events. */
+    case EVENT_OBJECT_VALUECHANGE: return UIA_AutomationPropertyChangedEventId; /* UIA_RangeValueValuePropertyId or UIA_ValueValuePropertyId */
+    case EVENT_SYSTEM_ALERT: return UIA_SystemAlertEventId;
+    case EVENT_SYSTEM_DIALOGEND: return UIA_Window_WindowClosedEventId;
+    case EVENT_SYSTEM_DIALOGSTART: return UIA_Window_WindowOpenedEventId;
+    case EVENT_SYSTEM_FOREGROUND: return UIA_AutomationFocusChangedEventId;
+    case EVENT_SYSTEM_MENUEND: return UIA_MenuModeEndEventId;
+    case EVENT_SYSTEM_MENUPOPUPEND: return UIA_MenuClosedEventId;
+    case EVENT_SYSTEM_MENUPOPUPSTART: return UIA_MenuOpenedEventId;
+    case EVENT_SYSTEM_MENUSTART: return UIA_MenuModeStartEventId;
+    case EVENT_SYSTEM_MINIMIZEEND: return UIA_AutomationPropertyChangedEventId; /* UIA_WindowWindowVisualStatePropertyId change */
+    case EVENT_SYSTEM_MINIMIZESTART: return UIA_AutomationPropertyChangedEventId; /* UIA_WindowWindowVisualStatePropertyId change */
+    case EVENT_SYSTEM_MOVESIZEEND: return UIA_AutomationPropertyChangedEventId; /* UIA_BoundingRectanglePropertyId change */
+    case EVENT_SYSTEM_MOVESIZESTART: return UIA_AutomationPropertyChangedEventId; /* UIA_BoundingRectanglePropertyId change */
+    case EVENT_SYSTEM_SCROLLINGEND:
+    case EVENT_SYSTEM_SCROLLINGSTART:
+    case 0x8015: /* FIXME: EVENT_OBJECT_CONTENTSCROLLED. Do we not have this defined? */
+        if (obj_id == OBJID_VSCROLL)
+            return UIA_AutomationPropertyChangedEventId; /* UIA_ScrollVerticalScrollPercentPropertyId change */
+        else if (obj_id == OBJID_HSCROLL)
+            return UIA_AutomationPropertyChangedEventId; /* UIA_ScrollHorizontalScrollPercentPropertyId change */
+        FIXME("Scroll events only supported on OBJID_VSCROLL/OBJID_HSCROLL!\n");
+        break;
+
+    default:
+        FIXME("Unimplemented mapping for MSAA event %#x to UIA event!\n", event);
+        break;
+    }
+
+    return 0;
+}
+
 /*
  * Event hook callback for window creation events.
  */
@@ -43,7 +96,93 @@ void CALLBACK uia_evl_msaa_obj_focus_proc(HWINEVENTHOOK hWinEventHook, DWORD eve
         HWND hwnd, LONG idObject, LONG idChild, DWORD idEventThread,
         DWORD dwmsEventTime)
 {
-    return;
+    struct uia_evl *evl;
+    HRESULT hr;
+
+    if (idObject != OBJID_CLIENT && idObject != OBJID_WINDOW)
+        return;
+
+    evl = (struct uia_evl *)TlsGetValue(tls_index);
+    hr = uia_evm_add_msaa_event(evl, hwnd, idObject, idChild, event);
+    if (FAILED(hr))
+        FIXME("Failed to add event to event message queue!\n");
+}
+
+static void uia_evl_check_evh_evm_match(struct uia_evl *evl, struct uia_evm *evm)
+{
+    struct list *evh_list = &evl->uia_evh_list;
+    struct list *cursor, *cursor2;
+    struct uia_evh *evh;
+
+    EnterCriticalSection(&evl->ev_handler_cs);
+
+    LIST_FOR_EACH_SAFE(cursor, cursor2, evh_list)
+    {
+        evh = LIST_ENTRY(cursor, struct uia_evh, entry);
+
+        switch (evh->event_type)
+        {
+            case FOCUS_EVH:
+            {
+                IUIAutomationFocusChangedEventHandler *handler = evh->u.IUIAutomationFocusChangedEvh_iface;
+
+                if (evm->event != UIA_AutomationFocusChangedEventId)
+                    break;
+
+                IUIAutomationFocusChangedEventHandler_HandleFocusChangedEvent(handler, evm->elem);
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    LeaveCriticalSection(&evl->ev_handler_cs);
+}
+
+static void uia_evl_process_evm_queue(struct uia_evl *evl)
+{
+    struct list *evm_queue = &evl->uia_evm_queue;
+    struct list *cursor, *cursor2;
+    struct uia_evm *evm;
+    HRESULT hr;
+
+    if (list_empty(&evl->uia_evm_queue))
+        return;
+
+    LIST_FOR_EACH_SAFE(cursor, cursor2, evm_queue)
+    {
+        evm = LIST_ENTRY(cursor, struct uia_evm, entry);
+
+        if (evm->uia_evo == UIA_EVO_MSAA)
+        {
+            IAccessible *acc;
+            VARIANT child_id;
+
+            hr = AccessibleObjectFromEvent(evm->u.msaa_ev.hwnd, evm->u.msaa_ev.obj_id,
+                    evm->u.msaa_ev.child_id, &acc, &child_id);
+            if (FAILED(hr))
+                goto message_abort;
+
+            hr = create_uia_elem_from_msaa_acc(&evm->elem, acc, V_I4(&child_id));
+            if (FAILED(hr))
+                goto message_abort;
+        }
+        else
+        {
+            hr = create_uia_elem_from_raw_provider(&evm->elem, evm->u.uia_ev.elem_prov);
+            if (FAILED(hr))
+                goto message_abort;
+        }
+
+        uia_evl_check_evh_evm_match(evl, evm);
+        IUIAutomationElement_Release(evm->elem);
+
+message_abort:
+        list_remove(cursor);
+        heap_free(evm);
+    }
 }
 
 /*
@@ -78,6 +217,9 @@ static void uia_event_listener_thread_exit(struct uia_evl *evl)
 
     UnhookWinEvent(evl->object_focus_hook);
     UnhookWinEvent(evl->win_creation_hook);
+
+    DeleteCriticalSection(&evl->ev_handler_cs);
+    DeleteCriticalSection(&evl->evm_queue_cs);
 
     heap_free(evl);
     data->evl = NULL;
@@ -115,6 +257,8 @@ static DWORD WINAPI uia_event_listener_main(LPVOID lpParam)
             continue;
         }
 
+        uia_evl_process_evm_queue(evl);
+
         EnterCriticalSection(&evl->ev_handler_cs);
 
         if (list_empty(&evl->uia_evh_list))
@@ -144,6 +288,7 @@ static HRESULT start_uia_event_listener(struct uia_data *data)
 
     evl->data = data;
     list_init(&evl->uia_evh_list);
+    list_init(&evl->uia_evm_queue);
 
     /*
      * Create an event handler to signal when the event listener threads
@@ -152,6 +297,7 @@ static HRESULT start_uia_event_listener(struct uia_data *data)
     evl->pump_initialized = CreateEventW(NULL, 0, 0, NULL);
     evl->first_event = CreateEventW(NULL, 0, 0, NULL);
     InitializeCriticalSection(&evl->ev_handler_cs);
+    InitializeCriticalSection(&evl->evm_queue_cs);
 
     evl->h_thread = CreateThread(NULL, 0, uia_event_listener_main, evl, 0, &evl->tid);
 
@@ -299,6 +445,45 @@ HRESULT uia_evh_remove_all_event_handlers(struct uia_data *data)
 
     LeaveCriticalSection(&data->evl->ev_handler_cs);
     PostThreadMessageW(data->evl->tid, WM_NULL, 0, 0);
+
+    return S_OK;
+}
+
+/*
+ * uia_evm (Event Message) functions.
+ */
+
+/*
+ * Add an event message to the message queue.
+ */
+static HRESULT uia_evm_add_message_to_queue(struct uia_evl *evl, struct uia_evm *evm)
+{
+    EnterCriticalSection(&evl->evm_queue_cs);
+
+    list_add_tail(&evl->uia_evm_queue, &evm->entry);
+
+    LeaveCriticalSection(&evl->evm_queue_cs);
+    PostThreadMessageW(evl->tid, WM_NULL, 0, 0);
+
+    return S_OK;
+}
+
+static HRESULT uia_evm_add_msaa_event(struct uia_evl *evl, HWND hwnd,
+        LONG obj_id, LONG child_id, LONG event)
+{
+    struct uia_evm *evm = heap_alloc_zero(sizeof(*evm));
+
+    TRACE("evl %p, hwnd %p, obj_id %d, child_id %d, event_id %d\n", evl, hwnd, obj_id, child_id, event);
+    if (!evm)
+        return E_OUTOFMEMORY;
+
+    evm->uia_evo = UIA_EVO_MSAA;
+    evm->u.msaa_ev.hwnd = hwnd;
+    evm->u.msaa_ev.obj_id = obj_id;
+    evm->u.msaa_ev.child_id = child_id;
+    evm->event = uia_msaa_event_to_uia_event_id(obj_id, event);
+
+    uia_evm_add_message_to_queue(evl, evm);
 
     return S_OK;
 }
