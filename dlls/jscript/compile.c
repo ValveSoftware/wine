665f84415694728ef8242fa8a010fa8079474673
Paul Gofman <pgofman@codeweavers.com>
2021-07-30 15:42:40 +0300

    jscript: Store the head of statement list instead of the list structure.
    Signed-off-by: Paul Gofman <pgofman@codeweavers.com>
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit cfeb815ff455b26f8e627416f7d160baf1c57bbe)
    
    For The Sims 3 launcher.
    
    CW-Bug-Id: #19151

--- dlls/jscript/compile.c
+++ dlls/jscript/compile.c
@@ -2490,7 +2490,7 @@ static HRESULT init_code(compiler_ctx_t *compiler, const WCHAR *source, UINT64 s
     return S_OK;
 }
 
-static HRESULT compile_function(compiler_ctx_t *ctx, statement_list_t *source, function_expression_t *func_expr,
+static HRESULT compile_function(compiler_ctx_t *ctx, statement_t *source, function_expression_t *func_expr,
         BOOL from_eval, function_code_t *func)
 {
     function_expression_t *iter;
@@ -2547,7 +2547,7 @@ static HRESULT compile_function(compiler_ctx_t *ctx, statement_list_t *source, f
             return E_OUTOFMEMORY;
     }
 
-    hres = visit_block_statement(ctx, NULL, source ? source->head : NULL);
+    hres = visit_block_statement(ctx, NULL, source);
     if(FAILED(hres))
         return hres;
 
@@ -2588,7 +2588,7 @@ static HRESULT compile_function(compiler_ctx_t *ctx, statement_list_t *source, f
 
     ctx->current_function_expr = ctx->func_head;
     off = ctx->code_off;
-    hres = compile_block_statement(ctx, NULL, source ? source->head : NULL);
+    hres = compile_block_statement(ctx, NULL, source);
     if(FAILED(hres))
         return hres;
 
