735fbc8089415f2e11243ecb67f8c8ec2c963804
Derek Lesho <dlesho@codeweavers.com>
2021-10-01 13:56:55 +0200

    bcrypt: Add support for calculating secret ecc keys.
    For Rainbow 6: Siege.

--- dlls/bcrypt/gnutls.c
+++ dlls/bcrypt/gnutls.c
@@ -58,6 +58,10 @@
 #include <gmp.h>
 #endif
 
+#ifdef HAVE_GCRYPT_H
+#include <gcrypt.h>
+#endif
+
 
 WINE_DEFAULT_DEBUG_CHANNEL(bcrypt);
 WINE_DECLARE_DEBUG_CHANNEL(winediag);
@@ -170,6 +174,24 @@ MAKE_FUNCPTR(mpz_mod);
 MAKE_FUNCPTR(mpz_powm);
 MAKE_FUNCPTR(mpz_sub_ui);
 #endif
+
+#if defined(HAVE_GCRYPT_H) && defined(SONAME_LIBGCRYPT)
+static BOOL gcrypt_available;
+static void *libgcrypt_handle;
+
+MAKE_FUNCPTR(gcry_check_version);
+MAKE_FUNCPTR(gcry_sexp_build);
+MAKE_FUNCPTR(gcry_pk_encrypt);
+MAKE_FUNCPTR(gcry_mpi_new);
+MAKE_FUNCPTR(gcry_mpi_print);
+MAKE_FUNCPTR(gcry_sexp_release);
+MAKE_FUNCPTR(gcry_mpi_release);
+MAKE_FUNCPTR(gcry_strsource);
+MAKE_FUNCPTR(gcry_strerror);
+MAKE_FUNCPTR(gcry_sexp_find_token);
+MAKE_FUNCPTR(gcry_sexp_nth_mpi);
+#endif
+
 #undef MAKE_FUNCPTR
 
 static int compat_gnutls_cipher_tag(gnutls_cipher_hd_t handle, void *tag, size_t tag_size)
@@ -342,6 +364,36 @@ static NTSTATUS gnutls_process_attach( void *args )
 #undef LOAD_FUNCPTR_STR
 #endif
 
+#if defined(HAVE_GCRYPT_H) && defined(SONAME_LIBGCRYPT)
+#define LOAD_FUNCPTR(f) \
+    if (!(p##f = dlsym( libgcrypt_handle, #f ))) \
+    { \
+        WARN( "failed to load %s\n", #f ); \
+        gcrypt_available = FALSE; \
+    }
+
+    if ((libgcrypt_handle = dlopen( SONAME_LIBGCRYPT, RTLD_NOW )))
+    {
+        gcrypt_available = TRUE;
+
+        LOAD_FUNCPTR(gcry_check_version);
+        LOAD_FUNCPTR(gcry_sexp_build);
+        LOAD_FUNCPTR(gcry_pk_encrypt);
+        LOAD_FUNCPTR(gcry_mpi_new);
+        LOAD_FUNCPTR(gcry_mpi_print);
+        LOAD_FUNCPTR(gcry_sexp_release);
+        LOAD_FUNCPTR(gcry_mpi_release);
+        LOAD_FUNCPTR(gcry_strsource);
+        LOAD_FUNCPTR(gcry_strerror);
+        LOAD_FUNCPTR(gcry_sexp_find_token);
+        LOAD_FUNCPTR(gcry_sexp_nth_mpi);
+    }
+    else
+        WARN("failed to load gcrypt, no support for ECC secret agreement\n");
+
+#undef LOAD_FUNCPTR
+#endif
+
 #define LOAD_FUNCPTR_OPT(f) \
     if (!(p##f = dlsym( libgnutls_handle, #f ))) \
     { \
@@ -434,6 +486,11 @@ static NTSTATUS gnutls_process_detach( void *args )
 #if defined(HAVE_GMP_H) && defined(SONAME_LIBGMP)
     dlclose( libgmp_handle );
     libgmp_handle = NULL;
+#endif
+
+#if defined(HAVE_GCRYPT_H) && defined(SONAME_LIBGCRYPT)
+    dlclose( libgcrypt_handle );
+    libgcrypt_handle = NULL;
 #endif
 }
 
@@ -771,6 +828,10 @@ static NTSTATUS export_gnutls_pubkey_ecc( gnutls_privkey_t gnutls_key, enum alg_
         magic = BCRYPT_ECDH_PUBLIC_P256_MAGIC;
         size = 32;
         break;
+    case ALG_ID_ECDH_P384:
+        magic = BCRYPT_ECDH_PUBLIC_P384_MAGIC;
+        size = 48;
+        break;
     case ALG_ID_ECDSA_P256:
         magic = BCRYPT_ECDSA_PUBLIC_P256_MAGIC;
         size = 32;
@@ -786,7 +847,7 @@ static NTSTATUS export_gnutls_pubkey_ecc( gnutls_privkey_t gnutls_key, enum alg_
         return STATUS_INTERNAL_ERROR;
     }
 
-    if (curve != GNUTLS_ECC_CURVE_SECP256R1)
+    if (curve != GNUTLS_ECC_CURVE_SECP256R1 && curve != GNUTLS_ECC_CURVE_SECP384R1)
     {
         FIXME( "curve %u not supported\n", curve );
         free( x.data ); free( y.data );
@@ -1140,6 +1201,11 @@ static NTSTATUS key_asymmetric_generate( void *args )
         bitlen = GNUTLS_CURVE_TO_BITS( GNUTLS_ECC_CURVE_SECP256R1 );
         break;
 
+    case ALG_ID_ECDH_P384:
+        pk_alg = GNUTLS_PK_ECC;
+        bitlen = GNUTLS_CURVE_TO_BITS( GNUTLS_ECC_CURVE_SECP384R1 );
+        break;
+
     case ALG_ID_DH:
         return key_dh_generate( key );
 
@@ -1208,6 +1274,10 @@ static NTSTATUS key_export_ecc( void *args )
         magic = BCRYPT_ECDH_PRIVATE_P256_MAGIC;
         size = 32;
         break;
+    case ALG_ID_ECDH_P384:
+        magic = BCRYPT_ECDH_PRIVATE_P384_MAGIC;
+        size = 48;
+        break;
     case ALG_ID_ECDSA_P256:
         magic = BCRYPT_ECDSA_PRIVATE_P256_MAGIC;
         size = 32;
@@ -1224,7 +1294,7 @@ static NTSTATUS key_export_ecc( void *args )
         return STATUS_INTERNAL_ERROR;
     }
 
-    if (curve != GNUTLS_ECC_CURVE_SECP256R1)
+    if (curve != GNUTLS_ECC_CURVE_SECP256R1 && curve != GNUTLS_ECC_CURVE_SECP384R1)
     {
         FIXME( "curve %u not supported\n", curve );
         free( x.data ); free( y.data ); free( d.data );
@@ -2123,6 +2193,59 @@ static NTSTATUS key_asymmetric_decrypt( void *args )
     return status;
 }
 
+#if defined(HAVE_GCRYPT_H) && defined(SONAME_LIBGCRYPT)
+static NTSTATUS gcrypt_extract_result_into_secret(gcry_sexp_t result, struct secret *secret)
+{
+    NTSTATUS status = STATUS_SUCCESS;
+    gcry_mpi_t fullcoords = NULL;
+    gcry_sexp_t fragment = NULL;
+    UCHAR *tmp_buffer = NULL;
+    gcry_error_t err;
+    size_t size;
+
+    fragment = pgcry_sexp_find_token(result, "s", 0);
+    if (!fragment)
+    {
+        status = STATUS_NO_MEMORY;
+        goto done;
+    }
+
+    fullcoords = pgcry_sexp_nth_mpi(fragment, 1, GCRYMPI_FMT_USG);
+    if (!fullcoords)
+    {
+        status = STATUS_NO_MEMORY;
+        goto done;
+    }
+
+    if ((err = pgcry_mpi_print(GCRYMPI_FMT_USG, NULL, 0, &size, fullcoords)))
+    {
+        ERR("Error = %s/%s.\n", pgcry_strsource(err), pgcry_strerror(err));
+        status = STATUS_INTERNAL_ERROR;
+        goto done;
+    }
+
+    tmp_buffer = malloc(size);
+    if ((err = pgcry_mpi_print(GCRYMPI_FMT_STD, tmp_buffer, size, NULL, fullcoords)))
+    {
+        ERR("Error = %s/%s.\n", pgcry_strsource(err), pgcry_strerror(err));
+        status = STATUS_INTERNAL_ERROR;
+        goto done;
+    }
+
+    secret->data = malloc(size / 2);
+    memcpy(secret->data, tmp_buffer + size % 2, size / 2);
+    secret->data_len = size / 2;
+
+done:
+    free(tmp_buffer);
+
+    pgcry_mpi_release(fullcoords);
+    pgcry_sexp_release(fragment);
+
+    return status;
+}
+#endif
+
 static NTSTATUS key_secret_agreement( void *args )
 {
     struct key_secret_agreement_params *params = args;
@@ -2189,8 +2312,125 @@ static NTSTATUS key_secret_agreement( void *args )
 #endif
 
         case ALG_ID_ECDH_P256:
-            FIXME("ECDH is not supported.\n");
+        case ALG_ID_ECDH_P384:
+/* this is necessary since GNUTLS doesn't support ECDH public key encryption, maybe we can replace this when it does:
+   https://github.com/gnutls/gnutls/blob/cdc4fc288d87f91f974aa23b6e8595a53970ce00/lib/nettle/pk.c#L495 */
+#if defined(HAVE_GCRYPT_H) && defined(SONAME_LIBGCRYPT)
+        {
+            struct key_export_params key_export;
+            const char *pubkey_format;
+            DWORD key_size;
+            UCHAR *pubkey_raw;
+            gcry_sexp_t pubkey = NULL;
+            UCHAR *privkey_raw;
+            ULONG privkey_size;
+            gcry_sexp_t privkey = NULL;
+            gcry_sexp_t xchg_result = NULL;
+            gcry_error_t err;
+            NTSTATUS status = STATUS_SUCCESS;
+
+            if (!gcrypt_available)
+            {
+                WARN("ECC secret support not available.\n");
+                return STATUS_NOT_IMPLEMENTED;
+            }
+
+            if (priv_key->alg_id == ALG_ID_ECDH_P256)
+            {
+                pubkey_format = "NIST P-256";
+                key_size = 32;
+            }
+            else if (priv_key->alg_id == ALG_ID_ECDH_P384)
+            {
+                pubkey_format = "NIST P-384";
+                key_size = 48;
+            }
+            else return STATUS_NOT_IMPLEMENTED;
+
+            /* copy public key into temporary buffer so we can prepend 0x04 (to indicate it is uncompressed) */
+            pubkey_raw = malloc((key_size * 2) + 1);
+            pubkey_raw[0] = 0x04;
+            memcpy(pubkey_raw + 1, peer_key->u.a.pubkey + sizeof(BCRYPT_ECCKEY_BLOB), key_size * 2);
+
+            err = pgcry_sexp_build(&pubkey, NULL,
+                                "(key-data(public-key(ecdh(curve %s)(q %b))))",
+                                pubkey_format,
+                                (key_size * 2) + 1,
+                                pubkey_raw);
+
+            free(pubkey_raw);
+
+            if (err)
+            {
+                ERR("Failed to build gcrypt public key. err %s/%s\n", pgcry_strsource (err), pgcry_strerror (err));
+                return STATUS_INTERNAL_ERROR;
+            }
+
+            /* copy private key from gnutls to gcrypt */
+            privkey_size = sizeof(BCRYPT_ECCKEY_BLOB) + key_size * 3;
+            privkey_raw = malloc(privkey_size);
+            key_export.key = priv_key;
+            key_export.buf = privkey_raw;
+            key_export.len = privkey_size;
+            key_export.ret_len = &privkey_size;
+            status = key_export_ecc(&key_export);
+
+            if (status)
+            {
+                ERR("Failed to extra gnutls private key\n");
+                free(privkey_raw);
+                pgcry_sexp_release(pubkey);
+                return status;
+            }
+
+            err = pgcry_sexp_build(&privkey, NULL,
+                                "(data(flags raw)(value %b))",
+                                key_size,
+                                privkey_raw + sizeof(BCRYPT_ECCKEY_BLOB) + key_size * 2);
+
+            free(privkey_raw);
+
+            if (err)
+            {
+                ERR("Failed to build gcrypt private key. err %s/%s\n", pgcry_strsource (err), pgcry_strerror (err));
+                pgcry_sexp_release(pubkey);
+                return STATUS_INTERNAL_ERROR;
+            }
+
+            err = pgcry_pk_encrypt(&xchg_result, privkey, pubkey);
+
+            pgcry_sexp_release(privkey);
+            pgcry_sexp_release(pubkey);
+
+            if (err)
+            {
+                ERR("Failed to perform key exchange. err %s/%s\n", pgcry_strsource (err), pgcry_strerror (err));
+                return STATUS_INTERNAL_ERROR;
+            }
+
+            status = gcrypt_extract_result_into_secret(xchg_result, secret);
+
+            pgcry_sexp_release(xchg_result);
+
+            if (status)
+            {
+                ERR("Failed to extract secret key.\n");
+                return status;
+            }
+
+            if (secret->data_len != key_size)
+            {
+                ERR("got secret size %u, expected %u.\n", secret->data_len, key_size);
+                free(secret->data);
+                return STATUS_INTERNAL_ERROR;
+            }
+
             break;
+        }
+#else
+            WARN("Compiled without ECC secret support.\n");
+            return STATUS_NOT_IMPLEMENTED;
+#endif
 
         default:
             ERR( "unhandled algorithm %u\n", priv_key->alg_id );
