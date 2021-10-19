0633ca0ac7922ea9effc1986a67041cb6e429fb8
Matteo Bruni <mbruni@codeweavers.com>
2021-10-19 11:52:49 +0200

    ntdll: Call usleep() in NtDelayExecution() instead of NtYieldExecution().
    This implements the general fix from
    b1a79c6b9c3ada0c34b1411b60879962f1815e4d (in particular, making sure
    that Sleep(0) will not immediately resume execution of the thread if
    there are no other runnable threads) while preserving the existing
    behavior of NtYieldExecution() / SwitchToThread(). Thanks Rémi for the
    idea.
    
    Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>

--- dlls/ntdll/unix/sync.c
+++ dlls/ntdll/unix/sync.c
@@ -1666,7 +1666,7 @@ NTSTATUS WINAPI NtDelayExecution( BOOLEAN alertable, const LARGE_INTEGER *timeou
         }
 
         /* Note that we yield after establishing the desired timeout */
-        NtYieldExecution();
+        usleep(0);
         if (!when) return STATUS_SUCCESS;
 
         for (;;)
