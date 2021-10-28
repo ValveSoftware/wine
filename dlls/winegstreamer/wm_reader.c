4d5f5ad064eea30f764e9a24981355660f12d1cc
Zebediah Figura <zfigura@codeweavers.com>
2021-10-28 17:46:32 -0500

    winegstreamer: Use unlimited buffering for the WM reader objects.

--- dlls/winegstreamer/wm_reader.c
+++ dlls/winegstreamer/wm_reader.c
@@ -1455,7 +1455,7 @@ static HRESULT init_stream(struct wm_reader *reader, QWORD file_size)
     HRESULT hr;
     WORD i;
 
-    if (!(wg_parser = wg_parser_create(WG_PARSER_DECODEBIN, false)))
+    if (!(wg_parser = wg_parser_create(WG_PARSER_DECODEBIN, true)))
         return E_OUTOFMEMORY;
 
     reader->wg_parser = wg_parser;
