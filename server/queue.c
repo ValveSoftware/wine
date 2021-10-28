8fa1095bb13ea7f366ade8c4345bb3aeead259ac
Rémi Bernon <rbernon@codeweavers.com>
2021-10-28 09:11:02 +0200

    server: Clear the MOUSEEVENTF_(ABSOLUTE|VIRTUALDESK) flags.
    For rawinput messages, as user32 is currently only expecting relative
    motion.

--- server/queue.c
+++ server/queue.c
@@ -1926,7 +1926,7 @@ static int queue_mouse_message( struct desktop *desktop, user_handle_t win, cons
         msg_data = &raw_msg.data;
         msg_data->info                = input->mouse.info;
         msg_data->size                = sizeof(*msg_data);
-        msg_data->flags               = flags;
+        msg_data->flags               = flags & ~(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_VIRTUALDESK);
         msg_data->rawinput.type       = RIM_TYPEMOUSE;
         msg_data->rawinput.mouse.x    = x - desktop->cursor.x;
         msg_data->rawinput.mouse.y    = y - desktop->cursor.y;
