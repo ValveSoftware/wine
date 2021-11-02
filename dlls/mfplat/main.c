6abb873e1b7a444fd6102c7f82a7f47be03a990d
Anna Lasky <alasky@codeweavers.com>
2021-11-02 11:42:05 -0500

    fixup! mfplat: Stub out MFCreateDXGIDeviceManager, to avoid the d3d path.

--- dlls/mfplat/main.c
+++ dlls/mfplat/main.c
@@ -8912,7 +8912,7 @@ HRESULT WINAPI MFCreateDXGIDeviceManager(UINT *token, IMFDXGIDeviceManager **man
        returning it breaks other games, e.g. CONTRA: ROGUE CORPS and
        NieR Replicant. So we error out on all games except these last
        two. This should be removed once CW bug #19126 is solved. */
-    if (sgi && !(strcmp(sgi, "1020540") == 0 || strcmp(sgi, "1113560") == 0))
+    if (sgi && !(strcmp(sgi, "1020540") == 0 || strcmp(sgi, "1113560") == 0 || strcmp(sgi, "1449280") == 0))
     {
         FIXME("stubbing out\n");
         return E_NOTIMPL;
