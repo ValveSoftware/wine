d95960f63749969d25637af9141b3f6e0318ae62
Rémi Bernon <rbernon@codeweavers.com>
2021-11-23 11:17:18 +0100

    HACK: gamescope: winex11.drv: Ignore mouse events when Steam Keyboard is opened.
    CW-Bug-Id: #19673

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -446,6 +446,7 @@ static BOOL process_events( Display *display, Bool (*filter)(Display*, XEvent*,X
     BOOL queued = FALSE, overlay_enabled = FALSE;
     enum event_merge_action action = MERGE_DISCARD;
     ULONG_PTR overlay_filter = QS_KEY | QS_MOUSEBUTTON | QS_MOUSEMOVE;
+    ULONG_PTR keyboard_filter = QS_MOUSEBUTTON | QS_MOUSEMOVE;
 
     if (WaitForSingleObject(steam_overlay_event, 0) == WAIT_OBJECT_0)
         overlay_enabled = TRUE;
@@ -455,6 +456,7 @@ static BOOL process_events( Display *display, Bool (*filter)(Display*, XEvent*,X
     {
         count++;
         if (overlay_enabled && filter_event( display, &event, (char *)overlay_filter )) continue;
+        if (steam_keyboard_opened && filter_event( display, &event, (char *)keyboard_filter )) continue;
         if (XFilterEvent( &event, None ))
         {
             /*
@@ -1458,6 +1460,7 @@ static void handle_gamescope_focused_app( XPropertyEvent *event )
 
     unsigned long count, remaining, *property;
     int format, app_id, focused_app_id;
+    BOOL keyboard_opened;
     Atom type;
 
     if (!sgi && !(sgi = getenv( "SteamGameId" ))) return;
@@ -1474,7 +1477,12 @@ static void handle_gamescope_focused_app( XPropertyEvent *event )
         XFree( property );
     }
 
+    keyboard_opened = app_id != focused_app_id;
+    if (steam_keyboard_opened == keyboard_opened) return;
+
     TRACE( "Got app id %u, focused app %u\n", app_id, focused_app_id );
+    if ((steam_keyboard_opened = keyboard_opened)) TRACE( "Steam Keyboard is opened, filtering events.\n" );
+    else TRACE( "Steam Keyboard is closed, stopping events filter.\n" );
 }
 
 /***********************************************************************
