13017e7bc9bb1df0db55f01bacfc2c1226516a84
Rémi Bernon <rbernon@codeweavers.com>
2021-12-24 12:40:38 +0100

    user32: Add a filter member to device_notification_details.
    CW-Bug-Id: #19841
    CW-Bug-Id: #19843
    CW-Bug-Id: #19844
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 41ff8effa3b308109ea183b4135abda7387aef09)

--- dlls/sechost/service.c
+++ dlls/sechost/service.c
@@ -2004,6 +2004,11 @@ struct device_notification_details
 {
     DWORD (CALLBACK *cb)(HANDLE handle, DWORD flags, DEV_BROADCAST_HDR *header);
     HANDLE handle;
+    union
+    {
+        DEV_BROADCAST_HDR header;
+        DEV_BROADCAST_DEVICEINTERFACE_W iface;
+    } filter;
 };
 
 static HANDLE device_notify_thread;
