ValveSoftware/wine

commit d2cbafab08fc0d6de0faed9451309908287068eb

user32: Use input shared memory in GetKeyboardState.

dlls/user32/input.c
@@ -523,10 +523,21 @@ SHORT WINAPI DECLSPEC_HOTPATCH GetKeyState(INT vkey)
  */
 BOOL WINAPI DECLSPEC_HOTPATCH GetKeyboardState( LPBYTE state )
 {
-    BOOL ret;
+    volatile struct input_shared_memory *shared = get_input_shared_memory();
+    BOOL ret, skip = TRUE;
     UINT i;
 
     TRACE("(%p)\n", state);
+
+    if (!shared) skip = FALSE;
+    else SHARED_READ_BEGIN( &shared->seq )
+    {
+        if (!shared->created) skip = FALSE; /* server needs to create the queue */
+        else memcpy( state, (const void *)shared->keystate, 256 );
+    }
+    SHARED_READ_END( &shared->seq );
+
+    if (skip) return TRUE;
 
     memset( state, 0, 256 );
     SERVER_START_REQ( get_key_state )

commit 98eb74839c7dfffd749b23c458335ea2c1a59c42

user32: Mask off the unspecified bits in GetKeyboardState()
When using shared memory for the keyboard state (as added in d2cbafab08)
we no-longer mask off the unused bits, which causes the map to scroll
uncontrollably in some situations in the Age of Empires series.

This is a regression of bug:
https://bugs.winehq.org/show_bug.cgi?id=30814

An analysis of the bug can be found:
https://davidgow.net/hacks/aoescroll.html

Fixes: d2cbafab08 ("user32: Use input shared memory in GetKeyboardState.")
Signed-off-by: David Gow <david@ingeniumdigital.com>

dlls/user32/input.c
@@ -571,7 +571,11 @@ BOOL WINAPI DECLSPEC_HOTPATCH GetKeyboardState( LPBYTE state )
     }
     SHARED_READ_END( &shared->seq );
 
-    if (skip) return TRUE;
+    if (skip)
+    {
+        for (i = 0; i < 256; i++) state[i] &= 0x81;
+        return TRUE;
+    }
 
     memset( state, 0, 256 );
     SERVER_START_REQ( get_key_state )