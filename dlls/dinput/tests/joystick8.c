7e05af0a69c9d20ede68ca4863dd89b629f779ad
Rémi Bernon <rbernon@codeweavers.com>
2022-01-13 12:07:03 +0100

    dinput: Return DIERR_INPUTLOST when device is removed.
    This fixes hotplug with DS4 and other DInput-compatible controllers in
    Tekken 7.
    
    CW-Bug-Id: #19968
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit ea60e4678c7b8f39cc4245e42b8de74718dd146c)

--- dlls/dinput/tests/joystick8.c
+++ dlls/dinput/tests/joystick8.c
@@ -2822,20 +2822,15 @@ static void test_input_lost( DWORD version )
     pnp_driver_stop();
 
     hr = IDirectInputDevice8_GetDeviceState( device, sizeof(state), &state );
-    todo_wine
     ok( hr == DIERR_INPUTLOST, "GetDeviceState returned %#x\n", hr );
     hr = IDirectInputDevice8_GetDeviceState( device, sizeof(state), &state );
-    todo_wine
     ok( hr == DIERR_INPUTLOST, "GetDeviceState returned %#x\n", hr );
     hr = IDirectInputDevice8_GetDeviceData( device, size, objdata, &count, DIGDD_PEEK );
-    todo_wine
     ok( hr == DIERR_INPUTLOST, "GetDeviceData returned %#x\n", hr );
     hr = IDirectInputDevice8_Poll( device );
-    todo_wine
     ok( hr == DIERR_INPUTLOST, "Poll returned: %#x\n", hr );
 
     hr = IDirectInputDevice8_Acquire( device );
-    todo_wine
     ok( hr == DIERR_UNPLUGGED, "Acquire returned %#x\n", hr );
     hr = IDirectInputDevice8_GetDeviceState( device, sizeof(state), &state );
     ok( hr == DIERR_NOTACQUIRED, "GetDeviceState returned %#x\n", hr );
