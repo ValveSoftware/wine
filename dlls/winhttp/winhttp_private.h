ed84b3b1ac82a522b0f767bd692512b7d3218ba3
Paul Gofman <pgofman@codeweavers.com>
2022-01-18 20:32:12 +0300

    winhttp: Allow synchronous nonblocking send in send_socket_shutdown().
    CW-Bug-Id: #19976

--- dlls/winhttp/winhttp_private.h
+++ dlls/winhttp/winhttp_private.h
@@ -319,6 +319,8 @@ struct socket_shutdown
     char reason[123];
     DWORD len;
     BOOL send_callback;
+    WSAOVERLAPPED ovr;
+    BOOL complete_only;
 };
 
 struct object_header *addref_object( struct object_header * ) DECLSPEC_HIDDEN;
