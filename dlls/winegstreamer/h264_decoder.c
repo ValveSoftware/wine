21e6907b8bee224b2924e91d6e8e24900ad00379
Rémi Bernon <rbernon@codeweavers.com>
2022-01-18 13:34:15 +0100

    winegstreamer: Implement H264 decoder SetInputType.
    For: Call of Duty III, Mortal Kombat 11, Shadow Warrior 2,
    Yakuza 4 Remastered, Hard Reset Redux.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=45988
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47084
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=49715
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=52183
    CW-Bug-Id: #16839
    CW-Bug-Id: #18678
    CW-Bug-Id: #19362

--- dlls/winegstreamer/h264_decoder.c
+++ dlls/winegstreamer/h264_decoder.c
@@ -30,10 +30,16 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(mfplat);
 
+static const GUID *h264_decoder_input_types[] =
+{
+    &MFVideoFormat_H264,
+};
+
 struct h264_decoder
 {
     IMFTransform IMFTransform_iface;
     LONG refcount;
+    IMFMediaType *input_type;
 };
 
 static struct h264_decoder *impl_from_IMFTransform(IMFTransform *iface)
@@ -78,7 +84,11 @@ static ULONG WINAPI h264_decoder_Release(IMFTransform *iface)
     TRACE("iface %p decreasing refcount to %u.\n", decoder, refcount);
 
     if (!refcount)
+    {
+        if (decoder->input_type)
+            IMFMediaType_Release(decoder->input_type);
         free(decoder);
+    }
 
     return refcount;
 }
@@ -166,8 +176,31 @@ static HRESULT WINAPI h264_decoder_GetOutputAvailableType(IMFTransform *iface, D
 
 static HRESULT WINAPI h264_decoder_SetInputType(IMFTransform *iface, DWORD id, IMFMediaType *type, DWORD flags)
 {
-    FIXME("iface %p, id %u, type %p, flags %#x stub!\n", iface, id, type, flags);
-    return E_NOTIMPL;
+    struct h264_decoder *decoder = impl_from_IMFTransform(iface);
+    GUID major, subtype;
+    HRESULT hr;
+    ULONG i;
+
+    TRACE("iface %p, id %u, type %p, flags %#x.\n", iface, id, type, flags);
+
+    if (FAILED(hr = IMFMediaType_GetGUID(type, &MF_MT_MAJOR_TYPE, &major)) ||
+        FAILED(hr = IMFMediaType_GetGUID(type, &MF_MT_SUBTYPE, &subtype)))
+        return E_INVALIDARG;
+
+    if (!IsEqualGUID(&major, &MFMediaType_Video))
+        return MF_E_INVALIDMEDIATYPE;
+
+    for (i = 0; i < ARRAY_SIZE(h264_decoder_input_types); ++i)
+        if (IsEqualGUID(&subtype, h264_decoder_input_types[i]))
+            break;
+    if (i == ARRAY_SIZE(h264_decoder_input_types))
+        return MF_E_INVALIDMEDIATYPE;
+
+    if (decoder->input_type)
+        IMFMediaType_Release(decoder->input_type);
+    IMFMediaType_AddRef((decoder->input_type = type));
+
+    return S_OK;
 }
 
 static HRESULT WINAPI h264_decoder_SetOutputType(IMFTransform *iface, DWORD id, IMFMediaType *type, DWORD flags)
