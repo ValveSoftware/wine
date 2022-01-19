2f965c8afd33caf4fd0bf04cf6267352d2820830
Paul Gofman <gofmanp@gmail.com>
2022-01-19 18:16:22 +0300

    winevulkan: HACK: Report Windows driver version for AMD VAN GOGH.
    CW-Bug-Id: #20009

--- dlls/winevulkan/vulkan.c
+++ dlls/winevulkan/vulkan.c
@@ -1832,6 +1832,15 @@ static void release_display_device_init_mutex(HANDLE mutex)
     CloseHandle(mutex);
 }
 
+static void update_driver_version( VkPhysicalDeviceProperties *properties )
+{
+    if (properties->vendorID == 0x1002 && properties->deviceID == 0x163f)
+    {
+        /* AMD VANGOGH */
+        properties->driverVersion = VK_MAKE_VERSION(21, 20, 1);
+    }
+}
+
 void WINAPI wine_vkGetPhysicalDeviceProperties(VkPhysicalDevice physical_device,
         VkPhysicalDeviceProperties *properties)
 {
@@ -1850,6 +1859,7 @@ void WINAPI wine_vkGetPhysicalDeviceProperties(VkPhysicalDevice physical_device,
             }
         }
     }
+    update_driver_version(properties);
 }
 
 /* Wait until graphics driver is loaded by explorer */
@@ -1927,6 +1937,7 @@ void WINAPI wine_vkGetPhysicalDeviceProperties2(VkPhysicalDevice phys_dev,
             }
         }
     }
+    update_driver_version(&properties2->properties);
 }
 
 void WINAPI wine_vkGetPhysicalDeviceProperties2KHR(VkPhysicalDevice phys_dev,
@@ -1948,6 +1959,7 @@ void WINAPI wine_vkGetPhysicalDeviceProperties2KHR(VkPhysicalDevice phys_dev,
             }
         }
     }
+    update_driver_version(&properties2->properties);
 }
 
 void WINAPI wine_vkGetPhysicalDeviceExternalSemaphoreProperties(VkPhysicalDevice phys_dev,
