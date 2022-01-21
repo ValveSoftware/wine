f132c9755b5a5ddf44cfff0c2fa135d74630653d
Rémi Bernon <rbernon@codeweavers.com>
2022-01-21 14:35:59 +0100

    winegstreamer: Support dynamic wg_transform video format change.
    For: Call of Duty III, Mortal Kombat 11, Shadow Warrior 2,
    Yakuza 4 Remastered, Hard Reset Redux.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=45988
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47084
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=49715
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=52183
    CW-Bug-Id: #16839
    CW-Bug-Id: #18678
    CW-Bug-Id: #19362

--- dlls/winegstreamer/unix_private.h
+++ dlls/winegstreamer/unix_private.h
@@ -26,6 +26,8 @@
 extern bool init_gstreamer(void) DECLSPEC_HIDDEN;
 extern GstElement *create_element(const char *name, const char *plugin_set) DECLSPEC_HIDDEN;
 extern GstCaps *wg_format_to_caps(const struct wg_format *format) DECLSPEC_HIDDEN;
+extern void wg_format_from_caps(struct wg_format *format, const GstCaps *caps) DECLSPEC_HIDDEN;
+extern bool wg_format_compare(const struct wg_format *a, const struct wg_format *b) DECLSPEC_HIDDEN;
 
 extern NTSTATUS wg_transform_create(void *args) DECLSPEC_HIDDEN;
 extern NTSTATUS wg_transform_destroy(void *args) DECLSPEC_HIDDEN;
