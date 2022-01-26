f90fc97132ae7b1e0d50bce4ab6989594b993769
Paul Gofman <pgofman@codeweavers.com>
2022-01-26 15:25:21 +0100

    winhttp: Count queued websocket receive operations.
    Signed-off-by: Paul Gofman <pgofman@codeweavers.com>
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 2a812a5645c71d9d6b3503df7065d8a5434e2b6f)

--- dlls/winhttp/request.c
+++ dlls/winhttp/request.c
@@ -3304,6 +3304,12 @@ static void send_io_complete( struct object_header *hdr )
     assert( count >= 0 );
 }
 
+static void receive_io_complete( struct socket *socket )
+{
+    LONG count = InterlockedDecrement( &socket->hdr.pending_receives );
+    assert( count >= 0 );
+}
+
 static enum socket_opcode map_buffer_type( WINHTTP_WEB_SOCKET_BUFFER_TYPE type )
 {
     switch (type)
@@ -3674,6 +3680,7 @@ static void CALLBACK task_socket_receive( TP_CALLBACK_INSTANCE *instance, void *
 
     TRACE("running %p\n", work);
     ret = socket_receive( r->socket, r->buf, r->len, &count, &type );
+    receive_io_complete( r->socket );
 
     if (!ret)
     {
@@ -3727,8 +3734,10 @@ DWORD WINAPI WinHttpWebSocketReceive( HINTERNET hsocket, void *buf, DWORD len, D
         r->len    = len;
 
         addref_object( &socket->hdr );
+        InterlockedIncrement( &socket->hdr.pending_receives );
         if ((ret = queue_task( &socket->recv_q, task_socket_receive, r )))
         {
+            InterlockedDecrement( &socket->hdr.pending_receives );
             release_object( &socket->hdr );
             free( r );
         }
