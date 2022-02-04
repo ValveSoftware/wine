a516e891dfb86ac7e911cb7d5f3526e5e4f4f920
Rémi Bernon <rbernon@codeweavers.com>
2022-02-04 11:34:26 +0100

    HACK: proton: hidclass.sys: Do not send WM_INPUT messages when overlay is open.
    So that games listening to them, such as ICEY, do not have input
    bleeding through the overlay. Note that some other games, such as Hades,
    which are reading the HID devices directly, have the input bleeding
    through on Windows, so we do the same here.
    
    CW-Bug-Id: #20083

--- dlls/hidclass.sys/hid.h
+++ dlls/hidclass.sys/hid.h
@@ -84,6 +84,9 @@ typedef struct _BASE_DEVICE_EXTENSION
     WCHAR container_id[MAX_GUID_STRING_LEN];
     const GUID *class_guid;
 
+    HANDLE steam_overlay_event;
+    HANDLE steam_keyboard_event;
+
     BOOL is_fdo;
 } BASE_DEVICE_EXTENSION;
 
@@ -115,6 +118,9 @@ typedef struct _minidriver
 
     PDRIVER_ADD_DEVICE AddDevice;
     PDRIVER_DISPATCH PNPDispatch;
+
+    HANDLE steam_overlay_event;
+    HANDLE steam_keyboard_event;
 } minidriver;
 
 void call_minidriver( ULONG code, DEVICE_OBJECT *device, void *in_buff, ULONG in_size,
