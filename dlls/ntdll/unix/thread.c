a260f8bc13ce6c44cb2439aebb2cf10473838bcc
Paul Gofman <pgofman@codeweavers.com>
2022-02-10 16:17:41 +0300

    ntdll: Guard against syscall stack overrun.

--- dlls/ntdll/unix/thread.c
+++ dlls/ntdll/unix/thread.c
@@ -1252,7 +1252,7 @@ NTSTATUS init_thread_stack( TEB *teb, ULONG_PTR limit, SIZE_T reserve_size, SIZE
         if ((status = virtual_alloc_thread_stack( &stack, limit_4g, 0, chpev2_stack_size, chpev2_stack_size, FALSE )))
             return status;
 
-        cpu_area = stack.DeallocationStack;
+        cpu_area = (CHPE_V2_CPU_AREA_INFO *)((char*)stack.DeallocationStack + kernel_stack_guard_size);
         cpu_area->ContextAmd64 = (ARM64EC_NT_CONTEXT *)&cpu_area->EmulatorDataInline;
         cpu_area->EmulatorStackBase  = (ULONG_PTR)stack.StackBase;
         cpu_area->EmulatorStackLimit = (ULONG_PTR)stack.StackLimit + page_size;
