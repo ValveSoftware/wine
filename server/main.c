a45fc88c6310310164fae16798e1f0e9418dfe72
Rémi Bernon <rbernon@codeweavers.com>
2022-02-15 09:49:07 +0100

    Revert "server: Initialize local user SID with unique values."
    This reverts commit 7e3d44cb755cd11a48aabe448733acbe5409a955.

--- server/main.c
+++ server/main.c
@@ -34,7 +34,6 @@
 #include "thread.h"
 #include "request.h"
 #include "unicode.h"
-#include "security.h"
 #include "esync.h"
 #include "fsync.h"
 
@@ -244,7 +243,6 @@ int main( int argc, char *argv[] )
     if (debug_level) fprintf( stderr, "wineserver: starting (pid=%ld)\n", (long) getpid() );
     set_current_time();
     init_signals();
-    init_user_sid();
     init_directories( load_intl_file() );
     init_threading();
     init_registry();
