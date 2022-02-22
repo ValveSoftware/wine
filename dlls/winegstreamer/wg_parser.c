d3472421048c2e65d21266b5ef5dc7a3aef5475f
Rémi Bernon <rbernon@codeweavers.com>
2022-02-22 12:23:24 -0600

    winegstreamer: Append audioconvert and audioresample elements.
    So that we can decode WMA to something else than the default avdec_wmav2
    F32LE / non-interleaved format.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=51931
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=52391
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 0052ffbfa0e7672278b3d5fcdb767d604fa0f565)

--- dlls/winegstreamer/wg_parser.c
+++ dlls/winegstreamer/wg_parser.c
@@ -695,7 +695,7 @@ static gboolean sink_query_cb(GstPad *pad, GstObject *parent, GstQuery *query)
     }
 }
 
-static GstElement *create_element(const char *name, const char *plugin_set)
+GstElement *create_element(const char *name, const char *plugin_set)
 {
     GstElement *element;
 
