518d034ddc8aee9344ab7459384cd940c35fa80b
Joshua Ashton <joshua@froggi.es>
2022-02-23 21:06:16 +0000

    kernelbase: HACK: Force swiftshader + in-process-gpu for Rockstar Social Club
    'Fixes' it being one frame behind in all of its rendering.
    
    Using WineD3D etc, doesn't help here either, but this works for now.

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -587,6 +587,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"UplayWebCore.exe", L" --use-gl=swiftshader"},
         {L"Paradox Launcher.exe", L" --use-gl=swiftshader --in-process-gpu"},
         {L"UnrealCEFSubProcess.exe", L" --use-gl=swiftshader", "1233550"},
+        {L"SocialClubHelper.exe", L" --use-gl=swiftshader --in-process-gpu"},
     };
     unsigned int i;
     char sgi[64];
