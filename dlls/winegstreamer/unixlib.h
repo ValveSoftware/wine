ac93de905f27005c12a8520629188fef347a22e6
Rémi Bernon <rbernon@codeweavers.com>
2022-03-01 14:44:58 +0100

    Revert "winegstreamer: Implement MF_SD_LANGUAGE."
    This reverts commit 9aa63940259baea543e0d66026765f2cc55aa150.

--- dlls/winegstreamer/unixlib.h
+++ dlls/winegstreamer/unixlib.h
@@ -254,13 +254,6 @@ struct wg_parser_stream_get_duration_params
     UINT64 duration;
 };
 
-struct wg_parser_stream_get_language_params
-{
-    struct wg_parser_stream *stream;
-    char *buffer;
-    UINT32 size;
-};
-
 struct wg_parser_stream_seek_params
 {
     struct wg_parser_stream *stream;
@@ -297,7 +290,6 @@ enum unix_funcs
     unix_wg_parser_stream_notify_qos,
 
     unix_wg_parser_stream_get_duration,
-    unix_wg_parser_stream_get_language,
     unix_wg_parser_stream_seek,
 
     unix_wg_parser_stream_drain,
