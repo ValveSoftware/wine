9aa40fd61e9fb18504f31c4a2b1195b9848b10a4
Paul Gofman <pgofman@codeweavers.com>
2022-03-03 19:29:52 +0300

    ws2_32: Manager shorter length for IPV6_UNICAST_HOPS.
    CW-Bug-Id: #20236

--- dlls/ws2_32/tests/sock.c
+++ dlls/ws2_32/tests/sock.c
@@ -1193,6 +1193,7 @@ static void test_set_getsockopt(void)
         {AF_INET6, SOCK_DGRAM, IPPROTO_IPV6, IPV6_MULTICAST_LOOP, TRUE, {1, 1, 4}},
         {AF_INET6, SOCK_DGRAM, IPPROTO_IPV6, IPV6_PKTINFO, FALSE, {0, 0, 4}},
         {AF_INET6, SOCK_DGRAM, IPPROTO_IPV6, IPV6_RECVTCLASS, FALSE, {0, 0, 4}},
+        {AF_INET6, SOCK_DGRAM, IPPROTO_IPV6, IPV6_UNICAST_HOPS, TRUE, {1, 1, 4}},
     };
     SOCKET s, s2;
     int i, j, err, lasterr;
