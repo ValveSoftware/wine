8975693f6263f9c7bfa0ac381f11058606408bb0
Paul Gofman <pgofman@codeweavers.com>
2022-03-03 19:15:53 +0300

    ws2_32: Manager shorter length for IP_PKTINFO.
    CW-Bug-Id: #20236

--- dlls/ws2_32/socket.c
+++ dlls/ws2_32/socket.c
@@ -1754,6 +1754,12 @@ int WINAPI getsockopt( SOCKET s, int level, int optname, char *optval, int *optl
             return server_getsockopt( s, IOCTL_AFD_WINE_GET_IP_OPTIONS, optval, optlen );
 
         case IP_PKTINFO:
+            if (!optlen || *optlen < sizeof(DWORD) || !optval)
+            {
+                if (optlen) *optlen = 0;
+                SetLastError(WSAEFAULT);
+                return SOCKET_ERROR;
+            }
             return server_getsockopt( s, IOCTL_AFD_WINE_GET_IP_PKTINFO, optval, optlen );
 
         case IP_RECVTOS:
@@ -3042,6 +3048,11 @@ int WINAPI setsockopt( SOCKET s, int level, int optname, const char *optval, int
             return server_setsockopt( s, IOCTL_AFD_WINE_SET_IP_OPTIONS, optval, optlen );
 
         case IP_PKTINFO:
+            if (optlen < sizeof(DWORD) || !optval)
+            {
+                SetLastError(WSAEFAULT);
+                return SOCKET_ERROR;
+            }
             return server_setsockopt( s, IOCTL_AFD_WINE_SET_IP_PKTINFO, optval, optlen );
 
         case IP_RECVTOS:
