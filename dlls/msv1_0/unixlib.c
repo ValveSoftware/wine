f98d479c37add3c461844aab019b146bc4a25831
Andrew Eikum <aeikum@codeweavers.com>
2022-03-21 14:02:30 -0500

    msv1_0: Downgrade missing ntlm_auth message
    This appears in almost every log and is almost never actually a problem.

--- dlls/msv1_0/unixlib.c
+++ dlls/msv1_0/unixlib.c
@@ -43,7 +43,6 @@
 extern char **environ;
 
 WINE_DEFAULT_DEBUG_CHANNEL(ntlm);
-WINE_DECLARE_DEBUG_CHANNEL(winediag);
 
 #define INITIAL_BUFFER_SIZE 200
 
@@ -233,7 +232,7 @@ static NTSTATUS ntlm_check_version( void *args )
         status = STATUS_SUCCESS;
     }
 
-    if (status) ERR_(winediag)( "ntlm_auth was not found. Make sure that ntlm_auth >= 3.0.25 is in your path. "
+    if (status) WARN( "ntlm_auth was not found. Make sure that ntlm_auth >= 3.0.25 is in your path. "
                                 "Usually, you can find it in the winbind package of your distribution.\n" );
     ntlm_cleanup( &ctx );
     return status;
