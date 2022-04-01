cf08808609a9010a489309ef0a00a92d4c54a400
Paul Gofman <pgofman@codeweavers.com>
2022-04-01 17:39:50 +0300

    msvcrt: HACK: Introduce asm wrapper for _isatty on x64.
    CW-Bug-Id: #20419

--- dlls/msvcrt/file.c
+++ dlls/msvcrt/file.c
@@ -44,6 +44,7 @@
 #include "mtdll.h"
 #include "wine/asm.h"
 #include "wine/debug.h"
+#include "wine/asm.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(msvcrt);
 
@@ -818,12 +819,28 @@ static int msvcrt_flush_buffer(FILE* file)
 /*********************************************************************
  *		_isatty (MSVCRT.@)
  */
+#ifdef __x86_64__
+int CDECL MSVCRT__isatty(int fd)
+{
+    TRACE(":fd (%d)\n",fd);
+
+    return get_ioinfo_nolock(fd)->wxflag & WX_TTY;
+}
+__ASM_GLOBAL_FUNC( _isatty,
+        "sub $0x30,%rsp\n\t"
+        "lea MSVCRT___pioinfo(%rip),%rdx\n\t"
+        "nop;nop;nop;nop;nop;nop;nop;nop;nop\n\t"
+        "add $0x30,%rsp\n\t"
+        "jmp " __ASM_NAME( "MSVCRT__isatty" ) )
+#else
 int CDECL _isatty(int fd)
 {
     TRACE(":fd (%d)\n",fd);
 
     return get_ioinfo_nolock(fd)->wxflag & WX_TTY;
 }
+#endif
+
 
 /* INTERNAL: Allocate stdio file buffer */
 static BOOL msvcrt_alloc_buffer(FILE* file)
