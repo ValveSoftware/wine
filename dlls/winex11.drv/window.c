eb0584865363172c989e4ba8ed346330aa798976
Paul Gofman <gofmanp@gmail.com>
2022-04-12 22:37:30 +0300

    Revert "winex11.drv: fshack: Don't set 'pending_fullscreen' if window already has NET_WM_STATE_FULLSCREEN."
    Temporarily reverted before some other changes to winex11 go in, to
    simplify testing.
    
    This reverts commit 44f00c9fdbc1a35bc83300a71ddc5a82042e5d42.

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -1246,8 +1246,7 @@ void update_net_wm_states( struct x11drv_win_data *data )
 
             if(i == NET_WM_STATE_FULLSCREEN)
             {
-                data->pending_fullscreen = (new_state & (1 << i))
-                                            && !(data->net_wm_state & (1 << NET_WM_STATE_FULLSCREEN));
+                data->pending_fullscreen = (new_state & (1 << i)) != 0;
                 TRACE("set pending_fullscreen to: %u\n", data->pending_fullscreen);
             }
 
