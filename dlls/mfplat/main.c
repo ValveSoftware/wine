fd2688b6402fdbb86232c79d19b1bbd39b3edfec
Andrew Eikum <aeikum@codeweavers.com>
2022-05-09 10:56:29 -0500

    mfplat: Allow NULL length pointer in IMFAttributes::GetAllocatedString.
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit ca10760d2e7ea4f8718741592fd639f589247e53)
    
    For "The Turing Test" crash.
    
    CW-Bug-Id: #20402

--- dlls/mfplat/main.c
+++ dlls/mfplat/main.c
@@ -2679,7 +2679,8 @@ HRESULT attributes_GetAllocatedString(struct attributes *attributes, REFGUID key
     if (SUCCEEDED(hr))
     {
         *value = attrval.pwszVal;
-        *length = lstrlenW(*value);
+        if (length)
+            *length = lstrlenW(*value);
     }
 
     return hr;
