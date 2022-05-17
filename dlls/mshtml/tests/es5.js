c358c30839f0e1dea4f5a97b1a307d5dcc185e12
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2022-05-17 21:00:30 +0300

    mshtml: Add window.msCrypto stub.
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dlls/mshtml/tests/es5.js
+++ dlls/mshtml/tests/es5.js
@@ -2973,6 +2973,17 @@ sync_test("console", function() {
     ok(except, "console.timeLog: expected exception");
 });
 
+sync_test("Crypto", function() {
+    var crypto = window.msCrypto, r;
+    ok(Object.prototype.hasOwnProperty.call(Object.getPrototypeOf(window), "msCrypto"), "msCrypto not a property of window's prototype.");
+    r = Object.getPrototypeOf(crypto);
+    ok(r === window.Crypto.prototype, "getPrototypeOf(crypto) = " + r);
+
+    ok("subtle" in crypto, "subtle not in crypto");
+    ok("getRandomValues" in crypto, "getRandomValues not in crypto");
+    ok(!("randomUUID" in crypto), "randomUUID is in crypto");
+});
+
 sync_test("DOMParser", function() {
     var p, r = DOMParser.length, mimeType;
     ok(r === 0, "length = " + r);
