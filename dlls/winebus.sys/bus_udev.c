d8ff6b2d789a13b6fc233ec8b9c494d080c5b18c
Rémi Bernon <rbernon@codeweavers.com>
2022-05-17 15:24:01 +0200

    winebus.sys: Fix the HAVE_SYS_INOTIFY_H checks.
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 9aa1d0a1a79647a8b575f6a68473392e16a5efcb)

--- dlls/winebus.sys/bus_udev.c
+++ dlls/winebus.sys/bus_udev.c
@@ -2114,7 +2114,7 @@ NTSTATUS udev_bus_init(void *args)
         options.disable_udevd = TRUE;
     }
 
-#if HAVE_SYS_INOTIFY_H
+#ifdef HAVE_SYS_INOTIFY_H
     if (options.disable_udevd) monitor_fd = create_inotify();
     if (monitor_fd < 0) options.disable_udevd = FALSE;
 #else
@@ -2139,7 +2139,7 @@ NTSTATUS udev_bus_init(void *args)
     poll_count = 2;
 
     if (!options.disable_udevd) build_initial_deviceset_udevd();
-#if HAVE_SYS_INOTIFY_H
+#ifdef HAVE_SYS_INOTIFY_H
     else build_initial_deviceset_direct();
 #endif
 
@@ -2182,7 +2182,7 @@ NTSTATUS udev_bus_wait(void *args)
         if (pfd[0].revents)
         {
             if (udev_monitor) process_monitor_event(udev_monitor);
-#if HAVE_SYS_INOTIFY_H
+#ifdef HAVE_SYS_INOTIFY_H
             else process_inotify_event(pfd[0].fd);
 #endif
         }
