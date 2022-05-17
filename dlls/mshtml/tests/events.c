32c65ba681f21a81e60793bf8eac649b8cfcca5e
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2022-05-17 21:00:29 +0300

    mshtml: Allow null or undefined listeners in addEventListener/removeEventListener.
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dlls/mshtml/tests/events.c
+++ dlls/mshtml/tests/events.c
@@ -1889,6 +1889,7 @@ static void test_onclick(IHTMLDocument2 *doc)
     VariantClear(&v);
 
     if(document_mode >= 9) {
+        add_event_listener((IUnknown*)div, L"click", NULL, VARIANT_FALSE);
         add_event_listener((IUnknown*)div, L"click", (IDispatch*)&div_onclick_capture_obj, VARIANT_TRUE);
         add_event_listener((IUnknown*)div, L"click", (IDispatch*)&div_onclick_bubble_obj, VARIANT_FALSE);
     }
@@ -2043,6 +2044,7 @@ static void test_onclick(IHTMLDocument2 *doc)
     doc_detach_event(doc, L"onclick", (IDispatch*)&doc_onclick_attached_obj);
 
     if(document_mode >= 9) {
+        remove_event_listener((IUnknown*)div, L"click", NULL, VARIANT_FALSE);
         remove_event_listener((IUnknown*)div, L"click", (IDispatch*)&div_onclick_capture_obj, VARIANT_TRUE);
         remove_event_listener((IUnknown*)div, L"click", (IDispatch*)&div_onclick_bubble_obj, VARIANT_FALSE);
     }
