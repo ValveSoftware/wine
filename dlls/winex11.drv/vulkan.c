a471a5863cfab22dfb97e455260e7b1dfd49608d
Paul Gofman <pgofman@codeweavers.com>
2022-05-20 18:45:15 -0500

    winex11.drv: HACK: Add an option to support GDI blits from unmapped Vulkan windows.
    And enable it for Disgaea 5 Complete.
    
    CW-Bug-Id: #19945

--- dlls/winex11.drv/vulkan.c
+++ dlls/winex11.drv/vulkan.c
@@ -72,6 +72,7 @@ struct wine_vk_surface
     LONG swapchain_count; /* surface can have one active an many retired swapchains */
     HWND hwnd;
     DWORD hwnd_thread_id;
+    BOOL gdi_blit_source; /* HACK: gdi blits from the window should work with Vulkan rendered contents. */
 };
 
 typedef struct VkXlibSurfaceCreateInfoKHR
@@ -315,7 +316,7 @@ void sync_vk_surface(HWND hwnd, BOOL known_child)
     {
         if (surface->hwnd != hwnd) continue;
         if (surface_with_swapchain_count > 1) wine_vk_surface_set_offscreen(surface, TRUE);
-        else wine_vk_surface_set_offscreen(surface, known_child);
+        else wine_vk_surface_set_offscreen(surface, known_child || surface->gdi_blit_source);
     }
     LeaveCriticalSection(&context_section);
 }
@@ -330,7 +331,7 @@ Window wine_vk_active_surface(HWND hwnd)
     LIST_FOR_EACH_ENTRY(surface, &surface_list, struct wine_vk_surface, entry)
     {
         if (surface->hwnd != hwnd) continue;
-        if (!surface->swapchain_count) continue;
+        if (!surface->swapchain_count || surface->gdi_blit_source) continue;
         active = surface;
         surface_with_swapchain_count++;
     }
@@ -345,6 +346,25 @@ Window wine_vk_active_surface(HWND hwnd)
     LeaveCriticalSection(&context_section);
 
     return window;
+}
+
+BOOL wine_vk_direct_window_draw( HWND hwnd )
+{
+    struct wine_vk_surface *surface;
+    BOOL ret = FALSE;
+
+    EnterCriticalSection(&context_section);
+    LIST_FOR_EACH_ENTRY(surface, &surface_list, struct wine_vk_surface, entry)
+    {
+        if (surface->hwnd != hwnd) continue;
+        if (surface->gdi_blit_source)
+        {
+            ret = TRUE;
+            break;
+        }
+    }
+    LeaveCriticalSection(&context_section);
+    return ret;
 }
 
 void vulkan_thread_detach(void)
@@ -396,6 +416,17 @@ static VkResult X11DRV_create_vk_instance_with_callback(const VkInstanceCreateIn
     return res;
 }
 
+static void set_dc_drawable( HDC hdc, Drawable drawable, const RECT *rect )
+{
+    struct x11drv_escape_set_drawable escape;
+
+    escape.code = X11DRV_SET_DRAWABLE;
+    escape.mode = IncludeInferiors;
+    escape.drawable = drawable;
+    escape.dc_rect = *rect;
+    ExtEscape( hdc, X11DRV_ESCAPE, sizeof(escape), (LPSTR)&escape, 0, NULL );
+}
+
 static VkResult X11DRV_vkAcquireNextImageKHR(VkDevice device,
         VkSwapchainKHR swapchain, uint64_t timeout, VkSemaphore semaphore,
         VkFence fence, uint32_t *image_index)
@@ -403,10 +434,12 @@ static VkResult X11DRV_vkAcquireNextImageKHR(VkDevice device,
     static int once;
     struct x11drv_escape_present_drawable escape;
     struct wine_vk_surface *surface = NULL;
+    DWORD dc_flags = DCX_USESTYLE;
     VkResult result;
     VkFence orig_fence;
     BOOL wait_fence = FALSE;
     HDC hdc = 0;
+    RECT rect;
 
     EnterCriticalSection(&context_section);
     if (!XFindContext(gdi_display, (XID)swapchain, vulkan_swapchain_context, (char **)&surface))
@@ -432,17 +465,29 @@ static VkResult X11DRV_vkAcquireNextImageKHR(VkDevice device,
     result = pvkAcquireNextImageKHR(device, swapchain, timeout, semaphore, fence, image_index);
 
     if ((result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR) && surface && surface->offscreen)
-        hdc = GetDCEx(surface->hwnd, 0, DCX_USESTYLE | DCX_CACHE);
+    {
+        if (!surface->gdi_blit_source)
+            dc_flags |= DCX_CACHE;
+        hdc = GetDCEx(surface->hwnd, 0, dc_flags);
+    }
 
     if (hdc)
     {
         if (wait_fence) pvkWaitForFences(device, 1, &fence, 0, timeout);
-        escape.code = X11DRV_PRESENT_DRAWABLE;
-        escape.drawable = surface->window;
-        escape.flush = TRUE;
-        ExtEscape(hdc, X11DRV_ESCAPE, sizeof(escape), (char *)&escape, 0, NULL);
-        if (surface->present_mode == VK_PRESENT_MODE_MAILBOX_KHR)
-            if (once++) FIXME("Application requires child window rendering with mailbox present mode, expect possible tearing!\n");
+        if (surface->gdi_blit_source)
+        {
+            GetClientRect( surface->hwnd, &rect );
+            set_dc_drawable( hdc, surface->window, &rect );
+        }
+        else
+        {
+            escape.code = X11DRV_PRESENT_DRAWABLE;
+            escape.drawable = surface->window;
+            escape.flush = TRUE;
+            ExtEscape(hdc, X11DRV_ESCAPE, sizeof(escape), (char *)&escape, 0, NULL);
+            if (surface->present_mode == VK_PRESENT_MODE_MAILBOX_KHR)
+                if (once++) FIXME("Application requires child window rendering with mailbox present mode, expect possible tearing!\n");
+        }
         ReleaseDC(surface->hwnd, hdc);
     }
 
@@ -548,9 +593,24 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
         goto err;
     }
 
-    if (create_info->hwnd && (GetWindow( create_info->hwnd, GW_CHILD ) || GetAncestor( create_info->hwnd, GA_PARENT ) != GetDesktopWindow()))
+    if (vulkan_gdi_blit_source_hack)
     {
-        x11_surface->known_child = TRUE;
+        RECT rect;
+
+        GetWindowRect( create_info->hwnd, &rect );
+        if (!is_window_rect_mapped( &rect ))
+        {
+            FIXME("HACK: setting gdi_blit_source for hwnd %p, surface %p.\n", x11_surface->hwnd, x11_surface);
+            x11_surface->gdi_blit_source = TRUE;
+            XReparentWindow( gdi_display, x11_surface->window, get_dummy_parent(), 0, 0 );
+        }
+    }
+
+    x11_surface->known_child = create_info->hwnd && (GetWindow( create_info->hwnd, GW_CHILD )
+                               || GetAncestor( create_info->hwnd, GA_PARENT ) != GetDesktopWindow());
+
+    if (x11_surface->known_child || x11_surface->gdi_blit_source)
+    {
         TRACE("hwnd %p creating offscreen child window surface\n", x11_surface->hwnd);
         if (!wine_vk_surface_set_offscreen(x11_surface, TRUE))
         {
@@ -578,6 +638,13 @@ static VkResult X11DRV_vkCreateWin32SurfaceKHR(VkInstance instance,
 
     *surface = (uintptr_t)x11_surface;
 
+    if (x11_surface->gdi_blit_source)
+    {
+        /* Make sure window gets surface destroyed. */
+        SetWindowPos( x11_surface->hwnd, 0, 0, 0, 0, 0,
+                      SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOSIZE | SWP_NOMOVE |
+                      SWP_NOREDRAW | SWP_DEFERERASE | SWP_NOSENDCHANGING | SWP_STATECHANGED );
+    }
     TRACE("Created surface=0x%s\n", wine_dbgstr_longlong(*surface));
     return VK_SUCCESS;
 
