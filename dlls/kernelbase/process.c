56201707eb9b43981f0168d8bfff8334f2a2c81e
Paul Gofman <pgofman@codeweavers.com>
2022-05-22 16:51:09 -0500

    Revert "kernelbase: HACK: Force CEF swiftshader for Succubus x Saint."
    This reverts commit 8429553f6747507b2a7fcbd99820154c6d3a4a4d.

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -598,7 +598,6 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"SocialClubHelper.exe", L" --use-gl=swiftshader --in-process-gpu"},
         {L"Montaro\\nw.exe", L" --use-gl=swiftshader"},
         {L"EOSOverlayRenderer-Win64-Shipping.exe", L" --use-gl=swiftshader --in-process-gpu"},
-        {L"Succubus x Saint\\Game.exe", L" --use-gl=swiftshader"},
     };
     unsigned int i;
     char sgi[64];
