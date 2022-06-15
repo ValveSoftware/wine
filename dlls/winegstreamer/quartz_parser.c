e15dc87b05ff3a8a49e564a9a009d04f8c04d9a3
Rémi Bernon <rbernon@codeweavers.com>
2022-06-15 19:12:54 +0200

    winegstreamer: Keep default decodebin queue limits for buffers and time.
    Only make size unlimited, which fixes problems with Persona 4 while
    keeping the memory usage lower.
    
    CW-Bug-Id: #18799
    CW-Bug-Id: #18994
    CW-Bug-Id: #20182
    CW-Bug-Id: #20363
    CW-Bug-Id: #20905
    CW-Bug-Id: #20980

--- dlls/winegstreamer/quartz_parser.c
+++ dlls/winegstreamer/quartz_parser.c
@@ -1233,7 +1233,7 @@ HRESULT decodebin_parser_create(IUnknown *outer, IUnknown **out)
     if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
-    if (!(object->wg_parser = wg_parser_create(WG_PARSER_DECODEBIN, true)))
+    if (!(object->wg_parser = wg_parser_create(WG_PARSER_DECODEBIN)))
     {
         free(object);
         return E_OUTOFMEMORY;
@@ -1753,7 +1753,7 @@ HRESULT wave_parser_create(IUnknown *outer, IUnknown **out)
     if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
-    if (!(object->wg_parser = wg_parser_create(WG_PARSER_WAVPARSE, false)))
+    if (!(object->wg_parser = wg_parser_create(WG_PARSER_WAVPARSE)))
     {
         free(object);
         return E_OUTOFMEMORY;
@@ -1839,7 +1839,7 @@ HRESULT avi_splitter_create(IUnknown *outer, IUnknown **out)
     if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
-    if (!(object->wg_parser = wg_parser_create(WG_PARSER_AVIDEMUX, false)))
+    if (!(object->wg_parser = wg_parser_create(WG_PARSER_AVIDEMUX)))
     {
         free(object);
         return E_OUTOFMEMORY;
@@ -1946,7 +1946,7 @@ HRESULT mpeg_splitter_create(IUnknown *outer, IUnknown **out)
     if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
-    if (!(object->wg_parser = wg_parser_create(WG_PARSER_MPEGAUDIOPARSE, false)))
+    if (!(object->wg_parser = wg_parser_create(WG_PARSER_MPEGAUDIOPARSE)))
     {
         free(object);
         return E_OUTOFMEMORY;
