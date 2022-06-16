4888f498b319cb837c487498c91427a77709b953
Paul Gofman <pgofman@codeweavers.com>
2022-06-16 13:50:10 -0500

    ntdll: Validate context xstate at once in NtGetContextThread().
    CW-Bug-Id: #20821

--- dlls/ntdll/unix/signal_i386.c
+++ dlls/ntdll/unix/signal_i386.c
@@ -993,6 +993,8 @@ NTSTATUS WINAPI NtGetContextThread( HANDLE handle, CONTEXT *context )
     BOOL use_cached_debug_regs = FALSE;
     NTSTATUS ret;
 
+    if (!validate_context_xstate( context )) return STATUS_INVALID_PARAMETER;
+
     if (self && needed_flags & CONTEXT_DEBUG_REGISTERS)
     {
         /* debug registers require a server call if hw breakpoints are enabled */
@@ -1091,8 +1093,6 @@ NTSTATUS WINAPI NtGetContextThread( HANDLE handle, CONTEXT *context )
             CONTEXT_EX *context_ex = (CONTEXT_EX *)(context + 1);
             XSTATE *xstate = (XSTATE *)((char *)context_ex + context_ex->XState.Offset);
             unsigned int mask;
-
-            if (!validate_context_xstate( context )) return STATUS_INVALID_PARAMETER;
 
             mask = (xstate_compaction_enabled ? xstate->CompactionMask : xstate->Mask) & XSTATE_MASK_GSSE;
             xstate->Mask = frame->xstate.Mask & mask;
