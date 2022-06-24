f720fc817430cf40dd1f73a494abc9acc3362028
Rémi Bernon <rbernon@codeweavers.com>
2022-06-24 19:34:55 +0200

    Revert "winegstreamer: Add MFVideoFormat_ABGR32 media type to media source video stream descriptor."
    This reverts commit 748ebe9a85cf0cc2481d4b1eb560720b0d248fb6.

--- dlls/winegstreamer/media_source.c
+++ dlls/winegstreamer/media_source.c
@@ -853,7 +853,7 @@ static HRESULT new_media_stream(struct media_source *source,
 static HRESULT media_stream_init_desc(struct media_stream *stream)
 {
     IMFMediaTypeHandler *type_handler = NULL;
-    IMFMediaType *stream_types[9];
+    IMFMediaType *stream_types[8];
     struct wg_format format;
     DWORD type_count = 0;
     unsigned int i;
@@ -874,7 +874,6 @@ static HRESULT media_stream_init_desc(struct media_stream *stream)
             &MFVideoFormat_I420,
             &MFVideoFormat_ARGB32,
             &MFVideoFormat_RGB32,
-            &MFVideoFormat_ABGR32,
         };
 
         IMFMediaType *base_type = mf_media_type_from_wg_format(&format);
