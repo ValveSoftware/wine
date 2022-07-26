1554f4c427433c377d96cf6ba3a484b6390c39df
Arkadiusz Hiler <ahiler@codeweavers.com>
2022-07-26 11:22:26 +0300

    winebus.sys: Enable hidraw for Thrustmaster HOTAS.
    CW-Bug-Id: #20694

--- dlls/winebus.sys/unix_private.h
+++ dlls/winebus.sys/unix_private.h
@@ -285,6 +285,6 @@ BOOL is_xbox_gamepad(WORD vid, WORD pid) DECLSPEC_HIDDEN;
 BOOL is_dualshock4_gamepad(WORD vid, WORD pid) DECLSPEC_HIDDEN;
 BOOL is_dualsense_gamepad(WORD vid, WORD pid) DECLSPEC_HIDDEN;
 BOOL is_logitech_g920(WORD vid, WORD pid) DECLSPEC_HIDDEN;
-BOOL is_thrustmaster_rudder_pedals(WORD vid, WORD pid) DECLSPEC_HIDDEN;
+BOOL is_thrustmaster_hotas(WORD vid, WORD pid) DECLSPEC_HIDDEN;
 
 #endif /* __WINEBUS_UNIX_PRIVATE_H */
