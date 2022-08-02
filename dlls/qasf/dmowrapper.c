9205a8a921efd167780fd989ddcff838264e4346
Rémi Bernon <rbernon@codeweavers.com>
2022-08-02 11:26:09 +0200

    qasf: Keep calling DMO ProcessOutput until error is returned.
    CW-Bug-Id: #18799
    CW-Bug-Id: #18994
    CW-Bug-Id: #20182
    CW-Bug-Id: #20905

--- dlls/qasf/dmowrapper.c
+++ dlls/qasf/dmowrapper.c
@@ -206,7 +206,6 @@ static HRESULT process_output(struct dmo_wrapper *filter, IMediaObject *dmo)
 {
     DMO_OUTPUT_DATA_BUFFER *buffers = filter->buffers;
     DWORD status, i;
-    BOOL more_data;
     HRESULT hr;
 
     for (i = 0; i < filter->source_count; ++i)
@@ -228,8 +227,6 @@ static HRESULT process_output(struct dmo_wrapper *filter, IMediaObject *dmo)
 
     do
     {
-        more_data = FALSE;
-
         hr = IMediaObject_ProcessOutput(dmo, DMO_PROCESS_OUTPUT_DISCARD_WHEN_NO_BUFFER,
                 filter->source_count, buffers, &status);
         if (hr != S_OK)
@@ -241,9 +238,6 @@ static HRESULT process_output(struct dmo_wrapper *filter, IMediaObject *dmo)
 
             if (!buffers[i].pBuffer)
                 continue;
-
-            if (buffers[i].dwStatus & DMO_OUTPUT_DATA_BUFFERF_INCOMPLETE)
-                more_data = TRUE;
 
             if (buffers[i].dwStatus & DMO_OUTPUT_DATA_BUFFERF_TIME)
             {
@@ -270,7 +264,7 @@ static HRESULT process_output(struct dmo_wrapper *filter, IMediaObject *dmo)
             }
 
         }
-    } while (more_data);
+    } while (1);
 
 out:
     for (i = 0; i < filter->source_count; ++i)
