5b6386435be0e487e9d59efdd65eea72ba6a629f
Paul Gofman <pgofman@codeweavers.com>
2022-08-03 11:56:39 -0500

    ntdll: HACK: Add an option to report ntdll from RtlPcToFileHeader() for PC in Unix lib.
    CW-Bug-Id: #21065
    
    (to be dropped once we have no blocking calls to Unix native libs going
    wthout wine_syscall_dispatcher).

--- dlls/ntdll/unixlib.h
+++ dlls/ntdll/unixlib.h
@@ -45,6 +45,7 @@ struct unix_funcs
     void          (CDECL *steamclient_setup_trampolines)( HMODULE src_mod, HMODULE tgt_mod );
     void          (CDECL *set_unix_env)( const char *var, const char *val );
     void          (CDECL *write_crash_log)( const char *log_type, const char *log_msg );
+    BOOL          (CDECL *is_pc_in_native_so)( void *pc );
 };
 
 #endif /* __NTDLL_UNIXLIB_H */
