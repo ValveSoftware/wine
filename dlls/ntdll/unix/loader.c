bb6411ca50017c19ee7781a10886eb3c24b0a185
Paul Gofman <pgofman@codeweavers.com>
2022-09-09 17:37:39 -0500

    ntdll: Enable WINE_FSYNC_SIMULATE_SCHED_QUANTUM for GTA5.
    CW-Bug-Id: #21194

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2276,6 +2276,7 @@ static void hacks_init(void)
     {
         fsync_simulate_sched_quantum = !!strstr(main_argv[1], upc_exe);
         fsync_simulate_sched_quantum = fsync_simulate_sched_quantum || !!strstr(main_argv[1], "PlanetZoo.exe");
+        fsync_simulate_sched_quantum = fsync_simulate_sched_quantum || !!strstr(main_argv[1], "GTA5.exe");
     }
     if (fsync_simulate_sched_quantum)
         ERR("HACK: Simulating sched quantum in fsync.\n");
