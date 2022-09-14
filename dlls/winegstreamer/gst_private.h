af4773ca581c642a60c413440fa5defc81859540
Rémi Bernon <rbernon@codeweavers.com>
2022-09-14 22:11:17 +0200

    winegstreamer: Create wg_sample from IMFSample within wg_transform_push_mf.
    (cherry picked from commit 71da6a7152bcdfcc8151c18b08c18e2978238326)

--- dlls/winegstreamer/gst_private.h
+++ dlls/winegstreamer/gst_private.h
@@ -136,7 +136,7 @@ void wg_sample_release(struct wg_sample *wg_sample);
 
 HRESULT wg_transform_push_dmo(struct wg_transform *transform, struct wg_sample *sample,
         struct wg_sample_queue *queue, REFERENCE_TIME pts, REFERENCE_TIME duration);
-HRESULT wg_transform_push_mf(struct wg_transform *transform, struct wg_sample *sample,
+HRESULT wg_transform_push_mf(struct wg_transform *transform, IMFSample *sample,
         struct wg_sample_queue *queue);
 HRESULT wg_transform_push_quartz(struct wg_transform *transform, struct wg_sample *sample,
         struct wg_sample_queue *queue);
