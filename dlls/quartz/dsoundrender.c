6392b69d9ad909a5fa07ae56255a6ee3cd79313b
Rémi Bernon <rbernon@codeweavers.com>
2022-10-13 10:48:51 +0200

    quartz: Handle DirectSound Renderer Filter underruns.
    CW-Bug-Id: #21192

--- dlls/quartz/dsoundrender.c
+++ dlls/quartz/dsoundrender.c
@@ -90,7 +90,7 @@ static HRESULT dsound_render_write_data(struct dsound_render *filter, const void
 {
     WAVEFORMATEX *wfx = (WAVEFORMATEX *)filter->sink.pin.mt.pbFormat;
     unsigned char silence = wfx->wBitsPerSample == 8 ? 128  : 0;
-    DWORD play_pos, write_end, size1, size2;
+    DWORD write_end, size1, size2;
     void *data1, *data2;
     HRESULT hr;
 
@@ -105,9 +105,18 @@ static HRESULT dsound_render_write_data(struct dsound_render *filter, const void
         filter->write_pos = 0;
     else
     {
+        DWORD play_pos, write_pos;
+
         if (FAILED(hr = IDirectSoundBuffer_GetCurrentPosition(filter->dsbuffer,
-                &play_pos, NULL)))
+                &play_pos, &write_pos)))
             return hr;
+
+        if (filter->write_pos - play_pos <= write_pos - play_pos)
+        {
+            WARN("Buffer underrun detected, filter %p, dsound play pos %#lx, write pos %#lx, filter write pos %#lx!\n",
+                    filter, play_pos, write_pos, filter->write_pos);
+            filter->write_pos = write_pos;
+        }
 
         write_end = (filter->write_pos + size) % filter->buffer_size;
         if (write_end - filter->write_pos >= play_pos - filter->write_pos)
