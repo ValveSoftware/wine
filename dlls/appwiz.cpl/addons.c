dd63d5256622bf45dac685c63b29d13c5fe246a6
Esme Povirk <esme@codeweavers.com>
2022-10-22 16:02:38 -0500

    mscoree: Update Wine Mono to 7.4.0.
    (cherry picked from commit 4493b10d17ecff23f483ed486127a123e90b3cc2)

--- dlls/appwiz.cpl/addons.c
+++ dlls/appwiz.cpl/addons.c
@@ -58,10 +58,10 @@ WINE_DEFAULT_DEBUG_CHANNEL(appwizcpl);
 #define GECKO_SHA "???"
 #endif
 
-#define MONO_VERSION "7.3.1"
+#define MONO_VERSION "7.4.0"
 #if defined(__i386__) || defined(__x86_64__)
 #define MONO_ARCH "x86"
-#define MONO_SHA "fe30a28e8d5816de8216f6358c93f400445e9c94c811c2d1c67dbe59c17ff055"
+#define MONO_SHA "6413ff328ebbf7ec7689c648feb3546d8102ded865079d1fbf0331b14b3ab0ec"
 #else
 #define MONO_ARCH ""
 #define MONO_SHA "???"
