be43ee353aac1b2d7bd08652b5e3cdc26e6af53a
Rémi Bernon <rbernon@codeweavers.com>
2022-12-14 21:57:01 +0100

    ntdll: HACK: Set LIBGL_ALWAYS_SOFTWARE for Witcher 3 (Launcher).

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2350,6 +2350,11 @@ static void hacks_init(void)
         ERR("HACK: setting LIBGL_ALWAYS_SOFTWARE.\n");
         setenv("LIBGL_ALWAYS_SOFTWARE", "1", 0);
     }
+    if (sgi && !strcmp(sgi, "292030"))
+    {
+        ERR("HACK: setting LIBGL_ALWAYS_SOFTWARE.\n");
+        setenv("LIBGL_ALWAYS_SOFTWARE", "1", 0);
+    }
 }
 
 /***********************************************************************
