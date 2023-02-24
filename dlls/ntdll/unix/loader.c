c5798e587d5a6f9fc33e7f04843fa923ab085628
Tim Clem <tclem@codeweavers.com>
2023-02-24 14:09:30 -0800

    ntdll: HACK: Also simulate async file read and I/O cancellation for Assassin's Creed Valhalla.

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2306,7 +2306,10 @@ static void hacks_init(void)
         env_str = getenv("WINE_SIMULATE_ASYNC_READ");
         if (env_str)
             ac_odyssey = !!atoi(env_str);
-        else if (main_argc > 1 && (strstr(main_argv[1], "ACOdyssey.exe") || strstr(main_argv[1], "ImmortalsFenyxRising.exe")))
+        else if (main_argc > 1 && (strstr(main_argv[1], "ACOdyssey.exe")
+                                || strstr(main_argv[1], "ACValhalla.exe")
+                                || strstr(main_argv[1], "ACValhalla_plus.exe")
+                                || strstr(main_argv[1], "ImmortalsFenyxRising.exe")))
             ac_odyssey = TRUE;
 
         if (ac_odyssey)
