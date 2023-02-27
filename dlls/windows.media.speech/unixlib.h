e50af9728181a0bddee7ae33e9cc27649fad5deb
Bernhard Kölbl <besentv@gmail.com>
2023-02-27 13:22:00 +0100

    windows.media.speech: Implement recognition in the unixlib.
    CW-Bug-Id: #20134

--- dlls/windows.media.speech/unixlib.h
+++ dlls/windows.media.speech/unixlib.h
@@ -44,12 +44,36 @@ struct speech_release_recognizer_params
     speech_recognizer_handle handle;
 };
 
-enum unix_funcs
+enum speech_recognition_status
+{
+    RECOGNITION_STATUS_CONTINUING,
+    RECOGNITION_STATUS_RESULT_AVAILABLE,
+    RECOGNITION_STATUS_EXCEPTION,
+};
+
+struct speech_recognize_audio_params
+{
+    speech_recognizer_handle handle;
+    const BYTE *samples;
+    UINT32 samples_size;
+    enum speech_recognition_status status;
+};
+
+struct speech_get_recognition_result_params
+{
+    speech_recognizer_handle handle;
+    char *result_buf;
+    UINT32 result_buf_size;
+};
+
+enum vosk_funcs
 {
     unix_process_attach,
     unix_process_detach,
     unix_speech_create_recognizer,
     unix_speech_release_recognizer,
+    unix_speech_recognize_audio,
+    unix_speech_get_recognition_result,
 };
 
 #endif
