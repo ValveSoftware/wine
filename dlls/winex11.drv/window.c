3d7e8431345da8b000bfd78c258e5b46c3c39757
Alex Henrie <alexhenrie24@gmail.com>
2023-02-28 09:49:36 -0700

    winex11: Avoid calling RtlInitUnicodeString on a static constant.
    (cherry picked from commit 354e999c5a8f284af3fe833f9fb45bace151a44e)

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -2511,7 +2511,7 @@ HWND create_foreign_window( Display *display, Window xwin )
     unsigned int nchildren;
     XWindowAttributes attr;
     UINT style = WS_CLIPCHILDREN;
-    UNICODE_STRING class_name;
+    UNICODE_STRING class_name = RTL_CONSTANT_STRING( classW );
 
     if (!class_registered)
     {
@@ -2522,7 +2522,6 @@ HWND create_foreign_window( Display *display, Window xwin )
         class.cbSize        = sizeof(class);
         class.lpfnWndProc   = client_foreign_window_proc;
         class.lpszClassName = classW;
-        RtlInitUnicodeString( &class_name, classW );
         if (!NtUserRegisterClassExWOW( &class, &class_name, &version, NULL, 0, 0, NULL ) &&
             RtlGetLastWin32Error() != ERROR_CLASS_ALREADY_EXISTS)
         {
