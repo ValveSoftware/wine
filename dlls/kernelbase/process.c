82e842a7bacb68133ff70282e792a6d04a09add0
Paul Gofman <pgofman@codeweavers.com>
2023-03-07 09:01:39 -0600

    kernelbase: HACK: Force Angle GL instead of Swiftshader for Paradox Launcher.
    CW-Bug-Id: #21997

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -597,7 +597,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
     }
     options[] =
     {
-        {L"Paradox Launcher.exe", L" --use-gl=swiftshader --in-process-gpu"},
+        {L"Paradox Launcher.exe", L" --use-angle=gl"},
         {L"Montaro\\nw.exe", L" --use-gl=swiftshader"},
         {L"OlympiaRising.exe", L" --use-gl=swiftshader"},
         {L"nw.exe.exe", L" --use-angle=d3d9"},
