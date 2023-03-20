2a98737c09869470ec28a8217020671b19b93c9f
Paul Gofman <pgofman@codeweavers.com>
2023-03-20 17:48:40 -0600

    kernelbase: HACK: Add --use-angle=gl command line option for nw.exe.exe.
    CW-Bug-Id: #22042

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -596,6 +596,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"Montaro\\nw.exe", L" --use-gl=swiftshader"},
         {L"EOSOverlayRenderer-Win64-Shipping.exe", L" --use-gl=swiftshader --in-process-gpu"},
         {L"EpicOnlineServicesUIHelper", L" --use-gl=desktop"},
+        {L"nw.exe.exe", L" --use-angle=gl"},
     };
     unsigned int i;
 
