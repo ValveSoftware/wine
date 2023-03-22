221ed0a5069e02e10174445400281660acd0a9f9
Ziqing Hui <zhui@codeweavers.com>
2023-03-22 15:41:00 +0800

    kernelbase: HACK: Force desktop GL for PaladinLias.
    CW-Bug-Id: #20816

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -606,6 +606,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"\\EpicOnlineServicesUIHelper", L" --use-gl=desktop"},
         {L"OlympiaRising.exe", L" --use-gl=swiftshader"},
         {L"nw.exe.exe", L" --use-angle=d3d9"},
+        {L"PaladinLias\\Game.exe", L" --use-gl=desktop"},
     };
     unsigned int i;
 
