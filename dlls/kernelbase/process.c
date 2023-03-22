f5ab6e8a089750f04616df6194fcad1d0d6c49c3
Ziqing Hui <zhui@codeweavers.com>
2023-03-22 15:41:00 +0800

    kernelbase: HACK: Force desktop GL for PaladinLias.
    CW-Bug-Id: #20816

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -602,6 +602,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"nw.exe.exe", L" --use-angle=d3d9"},
         {L"DC Universe Online\\LaunchPad.exe", L" --use-gl=swiftshader"},
         {L"PlanetSide 2\\LaunchPad.exe", L" --use-gl=swiftshader"},
+        {L"PaladinLias\\Game.exe", L" --use-gl=desktop"},
     };
     unsigned int i;
 
