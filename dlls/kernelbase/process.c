bde19bba3ed6c80e488f2bba64ae8e5bdf2a7bec
Paul Gofman <pgofman@codeweavers.com>
2023-03-28 19:11:14 -0600

    fixup! Fall Guys EOS overlay in process GPU.
    CW-Bug-Id: #22078

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -594,7 +594,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"UplayWebCore.exe", L" --use-angle=gl"},
         {L"Paradox Launcher.exe", L" --use-angle=gl"},
         {L"Montaro\\nw.exe", L" --use-gl=swiftshader"},
-        {L"EOSOverlayRenderer-Win64-Shipping.exe", L" --use-gl=swiftshader --in-process-gpu"},
+        {L"\\EOSOverlayRenderer-Win64-Shipping.exe", L" --use-gl=swiftshader --in-process-gpu"},
         {L"\\EpicOnlineServicesUIHelper", L" --use-gl=desktop"},
         {L"nw.exe.exe", L" --use-angle=d3d9"},
     };
