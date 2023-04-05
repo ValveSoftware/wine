db6eb96944c4143e5dc08701fe3d68d2eb4ead32
Paul Gofman <pgofman@codeweavers.com>
2023-04-05 11:31:10 -0600

    kernelbase: HACK: Add --use-gl=swiftshader for DC Universe Online/LaunchPad.exe.
    CW-Bug-Id: #22083

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -607,6 +607,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"OlympiaRising.exe", L" --use-gl=swiftshader"},
         {L"nw.exe.exe", L" --use-angle=d3d9"},
         {L"PaladinLias\\Game.exe", L" --use-gl=desktop"},
+        {L"DC Universe Online\\LaunchPad.exe", L" --use-gl=swiftshader"},
     };
     unsigned int i;
 
