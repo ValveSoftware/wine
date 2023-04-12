8de9f2bda79a41213e5ad9cfd2bf301e25bffdd0
Rémi Bernon <rbernon@codeweavers.com>
2023-04-12 10:14:50 +0200

    HACK: winex11: Create a dedicated thread for input events.
    CW-Bug-Id: #21879

--- dlls/winex11.drv/desktop.c
+++ dlls/winex11.drv/desktop.c
@@ -376,8 +376,9 @@ NTSTATUS x11drv_create_desktop( void *arg )
     if (!wcsicmp( name, rootW )) return FALSE;
 
     /* Create window */
-    win_attr.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask | EnterWindowMask |
-                          PointerMotionMask | ButtonPressMask | ButtonReleaseMask | FocusChangeMask;
+    win_attr.event_mask = ExposureMask | FocusChangeMask | EnterWindowMask |
+                          PointerMotionMask | ButtonPressMask | ButtonReleaseMask;
+    if (!input_thread_hack) win_attr.event_mask |= KeyPressMask | KeyReleaseMask;
     win_attr.cursor = XCreateFontCursor( display, XC_top_left_arrow );
 
     if (default_visual.visual != DefaultVisual( display, DefaultScreen(display) ))
