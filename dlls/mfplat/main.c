84b9e8d01a0ed620f9e227f0bd7b1df987c29387
Rémi Bernon <rbernon@codeweavers.com>
2023-04-27 17:49:08 +0200

    mfplat: Use the io queue for bytestream requests.
    CW-Bug-Id: #21953

--- dlls/mfplat/main.c
+++ dlls/mfplat/main.c
@@ -3826,7 +3826,7 @@ static HRESULT bytestream_create_io_request(struct bytestream *stream, enum asyn
             &stream->write_callback, NULL, &request)))
         goto failed;
 
-    RtwqPutWorkItem(MFASYNC_CALLBACK_QUEUE_STANDARD, 0, request);
+    RtwqPutWorkItem(MFASYNC_CALLBACK_QUEUE_IO, 0, request);
     IRtwqAsyncResult_Release(request);
 
 failed:
