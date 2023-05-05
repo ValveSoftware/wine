0b66ed2321ba383c3d50728a73660d985226401a
Rémi Bernon <rbernon@codeweavers.com>
2023-05-05 09:15:23 +0200

    winex11: Use ime_comp_buf != NULL instead of ximInComposeMode.
    (cherry picked from commit e70b1b722aebf22f52b90f9f00e9ea37935de752)

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -49,8 +49,6 @@
 WINE_DEFAULT_DEBUG_CHANNEL(event);
 WINE_DECLARE_DEBUG_CHANNEL(xdnd);
 
-extern BOOL ximInComposeMode;
-
 #define DndNotDnd       -1    /* OffiX drag&drop */
 #define DndUnknown      0
 #define DndRawData      1
@@ -887,7 +885,7 @@ static void focus_out( Display *display , HWND hwnd )
     XIC xic;
     struct x11drv_win_data *data;
 
-    if (ximInComposeMode) return;
+    if (xim_in_compose_mode()) return;
 
     data = get_win_data(hwnd);
     if(data){
