d182fd4b93ddd025b51f5a6e93acff7f6259e90e
Rémi Bernon <rbernon@codeweavers.com>
2023-05-16 15:29:37 +0200

    winex11: Make client_foreign_window_proc hidden.
    (cherry picked from commit 2b0677341704affce04710a633ba9fb5b3a134d1)

--- dlls/winex11.drv/x11drv.h
+++ dlls/winex11.drv/x11drv.h
@@ -459,7 +459,7 @@ extern int xrender_error_base DECLSPEC_HIDDEN;
 extern int xfixes_event_base DECLSPEC_HIDDEN;
 extern char *process_name DECLSPEC_HIDDEN;
 extern Display *clipboard_display DECLSPEC_HIDDEN;
-extern WNDPROC client_foreign_window_proc;
+extern WNDPROC client_foreign_window_proc DECLSPEC_HIDDEN;
 extern HANDLE steam_overlay_event DECLSPEC_HIDDEN;
 extern HANDLE steam_keyboard_event DECLSPEC_HIDDEN;
 
