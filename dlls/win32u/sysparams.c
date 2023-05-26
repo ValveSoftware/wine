c3b67e70a88a95f229ec19ace8c21563e5f15fe1
Rémi Bernon <rbernon@codeweavers.com>
2023-05-26 19:50:57 +0200

    win32u: Reset cursor clipping rectangle on display mode change.
    (cherry picked from commit 18cb55583a50aabf60467316dd23b7c2763ea271)
    CW-Bug-Id: #21879

--- dlls/win32u/sysparams.c
+++ dlls/win32u/sysparams.c
@@ -2732,6 +2732,7 @@ static LONG apply_display_settings( const WCHAR *devname, const DEVMODEW *devmod
         if (!adapter_get_current_settings( adapter, &current_mode )) WARN( "Failed to get primary adapter current display settings.\n" );
         adapter_release( adapter );
 
+        NtUserClipCursor( NULL );
         send_notify_message( NtUserGetDesktopWindow(), WM_DISPLAYCHANGE, current_mode.dmBitsPerPel,
                              MAKELPARAM( current_mode.dmPelsWidth, current_mode.dmPelsHeight ), FALSE );
         send_message_timeout( HWND_BROADCAST, WM_DISPLAYCHANGE, current_mode.dmBitsPerPel,
