3a3e00833ddfd5d2da4a12730733d88a9b99ec1d
Rémi Bernon <rbernon@codeweavers.com>
2023-05-29 14:12:25 +0200

    winex11: Use the user driver interface to create host desktops.
    (cherry picked from commit 4fcbbf8c9a15c9f18259d5c57b61fa8974121964)
    CW-Bug-Id: #21879

--- dlls/winex11.drv/x11drv_main.c
+++ dlls/winex11.drv/x11drv_main.c
@@ -1485,7 +1485,6 @@ NTSTATUS x11drv_client_call( enum client_callback func, UINT arg )
 
 const unixlib_entry_t __wine_unix_call_funcs[] =
 {
-    x11drv_create_desktop,
     x11drv_init,
     x11drv_systray_clear,
     x11drv_systray_dock,
@@ -1574,7 +1573,6 @@ static NTSTATUS x11drv_wow64_tablet_info( void *arg )
 
 const unixlib_entry_t __wine_unix_call_wow64_funcs[] =
 {
-    x11drv_create_desktop,
     x11drv_wow64_init,
     x11drv_wow64_systray_clear,
     x11drv_wow64_systray_dock,
