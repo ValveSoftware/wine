d966268e5ccddad46cf983cc1eb1fed9b126627f
Rémi Bernon <rbernon@codeweavers.com>
2023-05-31 22:59:39 +0200

    Revert "winegstreamer: Implement MFT_MESSAGE_COMMAND_FLUSH for h264 decoder."
    This reverts commit 6fc8c46aab46ad5e3ccc1019a44288f3143279e9.
    
    CW-Bug-Id: #21804
    CW-Bug-Id: #21813
    CW-Bug-Id: #22299

--- dlls/winegstreamer/resampler.c
+++ dlls/winegstreamer/resampler.c
@@ -513,7 +513,7 @@ static HRESULT WINAPI transform_ProcessMessage(IMFTransform *iface, MFT_MESSAGE_
         return MF_E_TRANSFORM_TYPE_NOT_SET;
 
     if (message == MFT_MESSAGE_COMMAND_DRAIN)
-        return wg_transform_drain(impl->wg_transform, FALSE);
+        return wg_transform_drain(impl->wg_transform);
 
     FIXME("Ignoring message %#x.\n", message);
 
