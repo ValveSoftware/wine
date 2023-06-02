633f6e6fc2f5584ea768f98d9c9738ad1acb1d50
Rémi Bernon <rbernon@codeweavers.com>
2023-06-02 11:19:22 +0200

    Revert "winex11.drv: Restore pointer grab on FocusIn events."
    This reverts commit 48294dc6b2b35cac26badd08f07306b416e0aba0.
    CW-Bug-Id: #21879

--- dlls/winex11.drv/mouse.c
+++ dlls/winex11.drv/mouse.c
@@ -129,6 +129,9 @@ XContext cursor_context = 0;
 static HWND cursor_window;
 static HCURSOR last_cursor;
 static DWORD last_cursor_change;
+static RECT last_clip_rect;
+static HWND last_clip_foreground_window;
+static BOOL last_clip_refused;
 static RECT clip_rect;
 static Cursor create_cursor( HANDLE handle );
 
@@ -459,7 +462,14 @@ static BOOL grab_clipping_window( const RECT *clip )
     if (keyboard_grabbed)
     {
         WARN( "refusing to clip to %s\n", wine_dbgstr_rect(clip) );
+        last_clip_refused = TRUE;
+        last_clip_foreground_window = NtUserGetForegroundWindow();
+        last_clip_rect = *clip;
         return FALSE;
+    }
+    else
+    {
+        last_clip_refused = FALSE;
     }
 
     /* enable XInput2 unless we are already clipping */
@@ -541,6 +551,20 @@ void reset_clipping_window(void)
 {
     ungrab_clipping_window();
     NtUserClipCursor( NULL );  /* make sure the clip rectangle is reset too */
+}
+
+/***********************************************************************
+ *      retry_grab_clipping_window
+ *
+ * Restore the current clip rectangle or retry the last one if it has
+ * been refused because of an active keyboard grab.
+ */
+void retry_grab_clipping_window(void)
+{
+    if (clipping_cursor)
+        NtUserClipCursor( &clip_rect );
+    else if (last_clip_refused && NtUserGetForegroundWindow() == last_clip_foreground_window)
+        NtUserClipCursor( &last_clip_rect );
 }
 
 /***********************************************************************
