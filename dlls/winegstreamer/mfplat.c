56e8f8e68b1d47cda114b06242f5b54fac86d809
Zebediah Figura <zfigura@codeweavers.com>
2023-06-02 16:37:42 +0200

    winegstreamer: Separate a mf_video_format_to_wg() helper.
    (cherry picked from commit 2803a220f684869ccd66e78778133b2586282da9)

--- dlls/winegstreamer/mfplat.c
+++ dlls/winegstreamer/mfplat.c
@@ -682,11 +682,23 @@ static void mf_media_type_to_wg_format_audio_mpeg4(IMFMediaType *type, const GUI
     format->u.audio_mpeg4.codec_data_len = codec_data_size;
 }
 
+static enum wg_video_format mf_video_format_to_wg(const GUID *subtype)
+{
+    unsigned int i;
+
+    for (i = 0; i < ARRAY_SIZE(video_formats); ++i)
+    {
+        if (IsEqualGUID(subtype, video_formats[i].subtype))
+            return video_formats[i].format;
+    }
+    FIXME("Unrecognized video subtype %s.\n", debugstr_guid(subtype));
+    return WG_VIDEO_FORMAT_UNKNOWN;
+}
+
 static void mf_media_type_to_wg_format_video(IMFMediaType *type, const GUID *subtype, struct wg_format *format)
 {
     UINT64 frame_rate, frame_size;
     MFVideoArea aperture;
-    unsigned int i;
     UINT32 size;
 
     if (FAILED(IMFMediaType_GetUINT64(type, &MF_MT_FRAME_SIZE, &frame_size)))
@@ -716,15 +728,7 @@ static void mf_media_type_to_wg_format_video(IMFMediaType *type, const GUID *sub
         format->u.video.fps_d = (UINT32)frame_rate;
     }
 
-    for (i = 0; i < ARRAY_SIZE(video_formats); ++i)
-    {
-        if (IsEqualGUID(subtype, video_formats[i].subtype))
-        {
-            format->u.video.format = video_formats[i].format;
-            return;
-        }
-    }
-    FIXME("Unrecognized video subtype %s.\n", debugstr_guid(subtype));
+    format->u.video.format = mf_video_format_to_wg(subtype);
 }
 
 static void mf_media_type_to_wg_format_audio_wma(IMFMediaType *type, const GUID *subtype, struct wg_format *format)
