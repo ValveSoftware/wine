93ffa6d39ac7f8da5cda49cc400f4ddc39de7e95
Zhiyi Zhang <zzhang@codeweavers.com>
2023-06-09 15:14:59 +0800

    esync: Type-check HANDLE in esync_reset_event().
    CW-Bug-Id: #22326

--- dlls/ntdll/unix/esync.c
+++ dlls/ntdll/unix/esync.c
@@ -571,6 +571,9 @@ NTSTATUS esync_reset_event( HANDLE handle )
     if ((ret = get_object( handle, &obj ))) return ret;
     event = obj->shm;
 
+    if (obj->type != ESYNC_MANUAL_EVENT && obj->type != ESYNC_AUTO_EVENT)
+        return STATUS_OBJECT_TYPE_MISMATCH;
+
     if (obj->type == ESYNC_MANUAL_EVENT)
     {
         /* Acquire the spinlock. */
