29882c8edadef76d3bcc3b61244c53b5f9e37f9e
Rémi Bernon <rbernon@codeweavers.com>
2023-07-07 20:53:07 +0200

    winegstreamer: Free the media source work queue outside of the CS.
    Possibly fixing some rare deadlock with MSFS.

--- dlls/winegstreamer/media_source_old.c
+++ dlls/winegstreamer/media_source_old.c
@@ -1262,6 +1262,7 @@ static ULONG WINAPI media_source_Release(IMFMediaSource *iface)
     if (!ref)
     {
         IMFMediaSource_Shutdown(iface);
+        MFUnlockWorkQueue(source->async_commands_queue);
         IMFPresentationDescriptor_Release(source->pres_desc);
         IMFMediaEventQueue_Release(source->event_queue);
         IMFByteStream_Release(source->byte_stream);
@@ -1464,8 +1465,6 @@ static HRESULT WINAPI media_source_Shutdown(IMFMediaSource *iface)
         IMFMediaStream_Release(&stream->IMFMediaStream_iface);
     }
     free(source->streams);
-
-    MFUnlockWorkQueue(source->async_commands_queue);
 
     LeaveCriticalSection(&source->cs);
 
