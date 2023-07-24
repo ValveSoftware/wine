6ea8d3bf3120756afd5e79312073dd0fbd4af052
Rémi Bernon <rbernon@codeweavers.com>
2023-07-24 14:25:38 +0200

    winegstreamer/new_media_source: Select the wg_source streams on media source start.
    CW-Bug-Id: #21953

--- dlls/winegstreamer/unixlib.h
+++ dlls/winegstreamer/unixlib.h
@@ -399,6 +399,13 @@ struct wg_source_get_stream_tag_params
     char *buffer;
 };
 
+struct wg_source_set_stream_flags_params
+{
+    wg_source_t source;
+    UINT32 index;
+    UINT32 select;
+};
+
 struct wg_transform_attrs
 {
     UINT32 output_plane_align;
@@ -517,6 +524,7 @@ enum unix_funcs
     unix_wg_source_push_data,
     unix_wg_source_get_stream_format,
     unix_wg_source_get_stream_tag,
+    unix_wg_source_set_stream_flags,
 
     unix_wg_transform_create,
     unix_wg_transform_destroy,
