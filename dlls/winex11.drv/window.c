9b8c0afed5d06942e661e21d9f6888b920dcfd09
Paul Gofman <pgofman@codeweavers.com>
2023-08-17 19:28:17 -0600

    winex11.drv: fshack: Sync GL drawable outside of win data lock in X11DRV_WindowPosChanged().
    CW-Bug-Id: #22608

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -3256,7 +3256,7 @@ void X11DRV_WindowPosChanged( HWND hwnd, HWND insert_after, UINT swp_flags,
 
     set_hwnd_style_props( data->display, data->whole_window, data->hwnd );
 
-    if (data->fs_hack) sync_gl_drawable( hwnd, FALSE );
+    if (data->fs_hack) needs_resize = TRUE;
 
     /* check if we are currently processing an event relevant to this window */
     event_type = 0;
