0a4a996f75bd31e9366a8a5975e71501d5b794bf
Rémi Bernon <rbernon@codeweavers.com>
2023-08-21 19:17:48 +0200

    HACK: winegstreamer/wg_transform: Don't add unnecessary and slow? videoflip for some games.
    CW-Bug-Id: #22581

--- dlls/winegstreamer/wg_transform.c
+++ dlls/winegstreamer/wg_transform.c
@@ -404,6 +404,19 @@ NTSTATUS wg_transform_create(void *args)
             break;
 
         case WG_MAJOR_TYPE_VIDEO:
+        {
+            const char *sgi;
+            if ((sgi = getenv("SteamGameId")) && (!strcmp(sgi, "2009100")))
+            {
+                if (!(element = create_element("videoconvert", "base"))
+                        || !append_element(transform->container, element, &first, &last))
+                    goto out;
+                gst_util_set_object_arg(G_OBJECT(element), "n-threads", "0");
+                /* HACK: skip slow?? videoflip for some games */
+                break;
+            }
+        }
+
             if (!(element = create_element("videoconvert", "base"))
                     || !append_element(transform->container, element, &first, &last))
                 goto out;
