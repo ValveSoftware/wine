7940050635c1de9636194b59845e07ee7be49842
Paul Gofman <pgofman@codeweavers.com>
2023-10-12 21:38:46 -0600

    ntdll: HACK: Avoid unitialized Dr7 in RtlRaiseException on x64.
    CW-Bug-Id: #22848

--- dlls/ntdll/signal_x86_64.c
+++ dlls/ntdll/signal_x86_64.c
@@ -1553,6 +1553,8 @@ __ASM_GLOBAL_FUNC( RtlRaiseException,
                    "movq 0x4f8(%rsp),%rax\n\t"  /* return address */
                    "movq %rax,0xf8(%rdx)\n\t"   /* context->Rip */
                    "movq %rax,0x10(%rcx)\n\t"   /* rec->ExceptionAddress */
+                   "xor %rax,%rax\n\t"
+                   "movq %rax,0x70(%rdx)\n\t"   /* Context->Dr7 */
                    "movl $1,%r8d\n\t"
                    "movq %gs:(0x30),%rax\n\t"   /* Teb */
                    "movq 0x60(%rax),%rax\n\t"   /* Peb */
