2c528630d294191a6a441ac0e77dacef30ead2c3
Paul Gofman <pgofman@codeweavers.com>
2023-10-24 12:17:28 -0600

    amd_ags_x64,atiadllxx: Bump AMD driver version.

--- dlls/atiadlxx/atiadlxx_main.c
+++ dlls/atiadlxx/atiadlxx_main.c
@@ -172,15 +172,15 @@ typedef struct ADLDisplayMap
 } ADLDisplayMap, *LPADLDisplayMap;
 
 static const ADLVersionsInfo version = {
-    "23.10.23.02-230720a-394204C-AMD-Software-Adrenalin-Edition",
+    "23.19.02-230831a-396538C-AMD-Software-Adrenalin-Edition",
     "",
     "http://support.amd.com/drivers/xml/driver_09_us.xml",
 };
 
 static const ADLVersionsInfoX2 version2 = {
-    "23.10.23.02-230720a-394204C-AMD-Software-Adrenalin-Edition",
+    "23.19.02-230831a-396538C-AMD-Software-Adrenalin-Edition",
     "",
-    "23.8.1",
+    "23.10.2",
     "http://support.amd.com/drivers/xml/driver_09_us.xml",
 };
 
