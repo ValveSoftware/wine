ValveSoftware/wine

commit 01dad2c92a56e141225a992ba09599c444e4a586

fshack: winex11: Implement _NET_FULLSCREEN_MONITOR support.
CW-Bug-Id: #22105

dlls/winex11.drv/window.c
@@ -1180,6 +1180,8 @@ void update_user_time( Time time )
  * windows spanning multiple monitors */
 static void update_net_wm_fullscreen_monitors( struct x11drv_win_data *data )
 {
+    RECT window_rect = data->whole_rect, monitor_rect;
+    HMONITOR hmonitor;
     long monitors[4];
     XEvent xev;
 
@@ -1192,8 +1194,27 @@ static void update_net_wm_fullscreen_monitors( struct x11drv_win_data *data )
     if (!X11DRV_DisplayDevices_SupportEventHandlers())
         return;
 
-    if (!xinerama_get_fullscreen_monitors( &data->whole_rect, monitors ))
+    if (!(hmonitor = fs_hack_monitor_from_hwnd( data->hwnd )))
+    {
+        ERR( "Failed to find monitor for %p at %s\n", data->hwnd, wine_dbgstr_rect(&data->whole_rect) );
         return;
+    }
+
+    monitor_rect = fs_hack_current_mode( hmonitor );
+    intersect_rect( &window_rect, &monitor_rect, &window_rect );
+    if (!EqualRect( &window_rect, &data->whole_rect ))
+    {
+        ERR( "hwnd %p at %s is outside of monitor %p at %s, ignoring\n", data->hwnd,
+             wine_dbgstr_rect(&data->whole_rect), hmonitor, wine_dbgstr_rect(&monitor_rect) );
+        return;
+    }
+
+    monitor_rect = fs_hack_real_mode( hmonitor );
+    if (!xinerama_get_fullscreen_monitors( &monitor_rect, monitors ))
+    {
+        ERR( "Failed to find xinerama monitors for %p at %s\n", hmonitor, wine_dbgstr_rect(&monitor_rect) );
+        return;
+    }
 
     if (!data->mapped)
     {

dlls/winex11.drv/fs.c
@@ -913,9 +913,13 @@ void fs_hack_init(void)
 {
     struct x11drv_display_device_handler device_handler;
     struct x11drv_settings_handler settings_handler;
+    RECT rect;
 
     real_device_handler = X11DRV_DisplayDevices_GetHandler();
     real_settings_handler = X11DRV_Settings_GetHandler();
+
+    rect = get_host_primary_monitor_rect();
+    xinerama_init( rect.right - rect.left, rect.bottom - rect.top );
 
     settings_handler.name = "Fullscreen Hack";
     settings_handler.priority = 500;
@@ -934,7 +938,7 @@ void fs_hack_init(void)
     device_handler.free_gpus = real_device_handler.free_gpus;
     device_handler.free_adapters = real_device_handler.free_adapters;
     device_handler.free_monitors = real_device_handler.free_monitors;
-    device_handler.register_event_handlers = NULL;
+    device_handler.register_event_handlers = real_device_handler.register_event_handlers;
     X11DRV_DisplayDevices_SetHandler( &device_handler );
 
     initialized = TRUE;

dlls/winex11.drv/xinerama.c
@@ -126,7 +126,7 @@ static inline int query_screens(void)
 /* Get xinerama monitor indices required for _NET_WM_FULLSCREEN_MONITORS */
 BOOL xinerama_get_fullscreen_monitors( const RECT *rect, long *indices )
 {
-    RECT window_rect, intersected_rect, monitor_rect;
+    RECT window_rect, intersected_rect, monitor_rect, virtual;
     BOOL ret = FALSE;
     POINT offset;
     INT i;
@@ -140,11 +140,9 @@ BOOL xinerama_get_fullscreen_monitors( const RECT *rect, long *indices )
     }
 
     /* Convert window rectangle to root coordinates */
-    offset = virtual_screen_to_root( rect->left, rect->top );
-    window_rect.left = offset.x;
-    window_rect.top = offset.y;
-    window_rect.right = window_rect.left + rect->right - rect->left;
-    window_rect.bottom = window_rect.top + rect->bottom - rect->top;
+    window_rect = *rect;
+    virtual = fs_hack_get_real_virtual_screen();
+    OffsetRect( &window_rect, -virtual.left, -virtual.top );
 
     /* Compare to xinerama monitor rectangles in root coordinates */
     offset.x = INT_MAX;

commit 793404ff4dcc709f5597bd6d3bc0aaae4c8a4a51

fixup! fshack: winex11: Implement _NET_FULLSCREEN_MONITOR support.
Obviously, xinerama_get_fullscreen_monitors() won't be able to find multiple monitors with a single
monitor rectangle from fs_hack_real_mode().

Fix a regression from 01dad2c92a56e141225a992ba09599c444e4a586. Restore Project Cars 3 triple-monitor
support.

CW-Bug-Id: #22609

dlls/winex11.drv/window.c
@@ -1180,8 +1180,6 @@ void update_user_time( Time time )
  * windows spanning multiple monitors */
 static void update_net_wm_fullscreen_monitors( struct x11drv_win_data *data )
 {
-    RECT window_rect = data->whole_rect, monitor_rect;
-    HMONITOR hmonitor;
     long monitors[4];
     XEvent xev;
 
@@ -1194,25 +1192,9 @@ static void update_net_wm_fullscreen_monitors( struct x11drv_win_data *data )
     if (!X11DRV_DisplayDevices_SupportEventHandlers())
         return;
 
-    if (!(hmonitor = fs_hack_monitor_from_hwnd( data->hwnd )))
+    if (!xinerama_get_fullscreen_monitors( &data->whole_rect, monitors ))
     {
-        ERR( "Failed to find monitor for %p at %s\n", data->hwnd, wine_dbgstr_rect(&data->whole_rect) );
-        return;
-    }
-
-    monitor_rect = fs_hack_current_mode( hmonitor );
-    intersect_rect( &window_rect, &monitor_rect, &window_rect );
-    if (!EqualRect( &window_rect, &data->whole_rect ))
-    {
-        ERR( "hwnd %p at %s is outside of monitor %p at %s, ignoring\n", data->hwnd,
-             wine_dbgstr_rect(&data->whole_rect), hmonitor, wine_dbgstr_rect(&monitor_rect) );
-        return;
-    }
-
-    monitor_rect = fs_hack_real_mode( hmonitor );
-    if (!xinerama_get_fullscreen_monitors( &monitor_rect, monitors ))
-    {
-        ERR( "Failed to find xinerama monitors for %p at %s\n", hmonitor, wine_dbgstr_rect(&monitor_rect) );
+        ERR( "Failed to find xinerama monitors at %s\n", wine_dbgstr_rect(&data->whole_rect) );
         return;
     }
 