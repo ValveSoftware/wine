eed4612c7226aef76ab21d3fc58f3f7178147786
Rémi Bernon <rbernon@codeweavers.com>
2023-11-19 16:53:47 +0100

    winegstreamer/new_media_source: Use wg_source order to build media source streams.
    And map them to wg_parser streams according to their format.
    
    CW-Bug-Id: #21953

--- dlls/winegstreamer/unixlib.h
+++ dlls/winegstreamer/unixlib.h
@@ -383,6 +383,13 @@ struct wg_source_push_data_params
     UINT32 size;
 };
 
+struct wg_source_get_stream_format_params
+{
+    wg_source_t source;
+    UINT32 index;
+    struct wg_format format;
+};
+
 struct wg_transform_attrs
 {
     UINT32 output_plane_align;
@@ -499,6 +506,7 @@ enum unix_funcs
     unix_wg_source_get_duration,
     unix_wg_source_get_position,
     unix_wg_source_push_data,
+    unix_wg_source_get_stream_format,
 
     unix_wg_transform_create,
     unix_wg_transform_destroy,
