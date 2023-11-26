ab5a23197a57882f115c8585a8196f2857937d6a
Rémi Bernon <rbernon@codeweavers.com>
2023-11-26 18:59:54 +0100

    loader: Expose the standard debugging symbols for GDB.
    CW-Bug-Id: #22176

--- loader/main.c
+++ loader/main.c
@@ -76,7 +76,8 @@ static void init_reserved_areas(void)
 
 #else
 
-/* the preloader will set this variable */
+/* the preloader will set these variables */
+__attribute((visibility("default"))) struct r_debug *wine_r_debug = NULL;
 const __attribute((visibility("default"))) struct wine_preload_info *wine_main_preload_info = NULL;
 
 static void init_reserved_areas(void)
