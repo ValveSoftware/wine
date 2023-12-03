0cbf019a7c0f6d573341257405d6ecb0529eaf8d
Rémi Bernon <rbernon@codeweavers.com>
2023-12-03 12:18:27 +0100

    winex11.drv: Do not move window with _NET_WM_STATE_FULLSCREEN set.
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -1581,6 +1581,7 @@ static HWND sync_window_position( struct x11drv_win_data *data,
 {
     DWORD style = NtUserGetWindowLongW( data->hwnd, GWL_STYLE );
     DWORD ex_style = NtUserGetWindowLongW( data->hwnd, GWL_EXSTYLE );
+    RECT original_rect = {0};
     HWND prev_window = NULL;
     XWindowChanges changes;
     unsigned int mask = 0;
@@ -1640,10 +1641,21 @@ static HWND sync_window_position( struct x11drv_win_data *data,
 
     set_size_hints( data, style );
     set_mwm_hints( data, style, ex_style );
+    /* KWin doesn't allow moving a window with _NET_WM_STATE_FULLSCREEN set. So we need to remove
+     * _NET_WM_STATE_FULLSCREEN before moving the window and restore it later */
+    if (wm_is_kde( data->display ) && NtUserIsWindowRectFullScreen( &data->whole_rect ))
+    {
+        original_rect = data->whole_rect;
+        SetRectEmpty( &data->whole_rect );
+    }
     update_net_wm_states( data );
     data->configure_serial = NextRequest( data->display );
     XReconfigureWMWindow( data->display, data->whole_window, data->vis.screen, mask, &changes );
-
+    if (!IsRectEmpty( &original_rect ))
+    {
+        data->whole_rect = original_rect;
+        update_net_wm_states( data );
+    }
 #ifdef HAVE_LIBXSHAPE
     if (IsRectEmpty( old_window_rect ) != IsRectEmpty( &data->window_rect ))
         sync_window_region( data, (HRGN)1 );
