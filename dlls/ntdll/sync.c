14f7b22769d6cc6f2ded0dad7fde3755459bc407
Paul Gofman <pgofman@codeweavers.com>
2023-12-18 17:49:20 -0600

    ntdll: Set DebugInfo to NULL for absent DebugInfo in RtlDeleteCriticalSection().
    CW-Bug-Id: #23142

--- dlls/ntdll/sync.c
+++ dlls/ntdll/sync.c
@@ -288,7 +288,11 @@ NTSTATUS WINAPI RtlDeleteCriticalSection( RTL_CRITICAL_SECTION *crit )
             crit->DebugInfo = NULL;
         }
     }
-    else NtClose( crit->LockSemaphore );
+    else
+    {
+        NtClose( crit->LockSemaphore );
+        crit->DebugInfo = NULL;
+    }
     crit->LockSemaphore = 0;
     return STATUS_SUCCESS;
 }
