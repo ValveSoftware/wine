f2cc01b3180a9b22a710dcc940b23cda15645d65
Rémi Bernon <rbernon@codeweavers.com>
2024-01-08 14:35:42 +0100

    windows.gaming.input: Forward get_NonRoamableId to Wine provider.
    CW-Bug-Id: #23185

--- dlls/windows.gaming.input/controller.c
+++ dlls/windows.gaming.input/controller.c
@@ -364,8 +364,8 @@ static HRESULT WINAPI raw_controller_2_get_SimpleHapticsControllers( IRawGameCon
 
 static HRESULT WINAPI raw_controller_2_get_NonRoamableId( IRawGameController2 *iface, HSTRING* value )
 {
-    FIXME( "iface %p, value %p stub!\n", iface, value );
-    return E_NOTIMPL;
+    struct controller *impl = impl_from_IRawGameController2( iface );
+    return IWineGameControllerProvider_get_NonRoamableId( impl->wine_provider, value );
 }
 
 static HRESULT WINAPI raw_controller_2_get_DisplayName( IRawGameController2 *iface, HSTRING* value )
