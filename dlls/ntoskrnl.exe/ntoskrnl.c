f1ee2e8d36240aba21dde3dad62d19cefd788133
Rémi Bernon <rbernon@codeweavers.com>
2024-03-21 10:08:51 +0100

    ntoskrnl.exe: Open symbolic link with DELETE before making them temporary.
    CW-Bug-Id: #24206

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -1803,7 +1803,7 @@ NTSTATUS WINAPI IoDeleteSymbolicLink( UNICODE_STRING *name )
     attr.SecurityDescriptor       = NULL;
     attr.SecurityQualityOfService = NULL;
 
-    if (!(status = NtOpenSymbolicLinkObject( &handle, 0, &attr )))
+    if (!(status = NtOpenSymbolicLinkObject( &handle, DELETE, &attr )))
     {
         NtMakeTemporaryObject( handle );
         NtClose( handle );
