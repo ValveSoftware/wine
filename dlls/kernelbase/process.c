44fd0130497cc3b774901d858f138478cf0ed541
Paul Gofman <pgofman@codeweavers.com>
2024-04-01 21:15:03 -0600

    kernelbase: HACK: Force in-process-gpu for Summer Islands.
    CW-Bug-Id: #23635

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -598,6 +598,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
     }
     options[] =
     {
+        {L"SummerIslands.exe", L" --in-process-gpu"},
         {L"Paradox Launcher.exe", L" --use-angle=gl"},
         {L"Montaro\\nw.exe", L" --use-gl=swiftshader"},
         {L"Aisling and the Tavern of Elves\\nw.exe", L" --use-gl=swiftshader"},
