475c10217cd70f93e40316acc1d731b8ce032143
Rémi Bernon <rbernon@codeweavers.com>
2024-05-07 18:28:07 +0200

    winegstreamer: Translate MFVIDEOFORMAT / WAVEFORMATEX directly to GstCaps in wg_transform.
    (cherry picked from commit 0906f6957a4a7f48f915fe841a6be7ce68c86c94)
    
    CW-Bug-Id: #20833

--- dlls/winegstreamer/unix_private.h
+++ dlls/winegstreamer/unix_private.h
@@ -74,7 +74,7 @@ extern NTSTATUS wg_source_set_stream_flags(void *args);
 extern NTSTATUS wg_transform_create(void *args);
 extern NTSTATUS wg_transform_destroy(void *args);
 extern NTSTATUS wg_transform_get_output_type(void *args);
-extern NTSTATUS wg_transform_set_output_format(void *args);
+extern NTSTATUS wg_transform_set_output_type(void *args);
 extern NTSTATUS wg_transform_push_data(void *args);
 extern NTSTATUS wg_transform_read_data(void *args);
 extern NTSTATUS wg_transform_get_status(void *args);
@@ -84,6 +84,7 @@ extern NTSTATUS wg_transform_notify_qos(void *args);
 
 /* wg_media_type.c */
 
+extern GstCaps *caps_from_media_type(const struct wg_media_type *media_type);
 extern NTSTATUS caps_to_media_type(GstCaps *caps, struct wg_media_type *media_type,
         UINT32 video_plane_align);
 
