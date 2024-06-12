1577f4b43d84bcd80edd76e2c946740919018618
Brendan McGrath <bmcgrath@codeweavers.com>
2024-06-13 07:37:01 +1000

    ntdll: Treat XDG_SESSION_TYPE as special env variable.
    This can be used to incorrectly determine the availability of X graphics.
    
    (cherry picked from commit 05c0a50454f278fef0f142e11d5491890a979893)
    CW-Bug-Id: #23899

--- dlls/ntdll/unix/env.c
+++ dlls/ntdll/unix/env.c
@@ -344,7 +344,8 @@ static BOOL is_special_env_var( const char *var )
             STARTS_WITH( var, "TMP=" ) ||
             STARTS_WITH( var, "QT_" ) ||
             STARTS_WITH( var, "SDL_AUDIODRIVER=" ) ||
-            STARTS_WITH( var, "VK_" ));
+            STARTS_WITH( var, "VK_" ) ||
+            STARTS_WITH( var, "XDG_SESSION_TYPE=" ));
 }
 
 /* check if an environment variable changes dynamically in every new process */
