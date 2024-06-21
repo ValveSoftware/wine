8a5acd83b65cab7cf002368cb0bcac95af636747
Paul Gofman <pgofman@codeweavers.com>
2024-06-21 16:42:44 -0600

    ntdll: Validate length in get_working_set_ex().
    (cherry picked from commit 81caa1d72b8cd93219a9303ad04237672f57d988)
    
    CW-Bug-Id: #23947

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -5680,6 +5680,8 @@ static NTSTATUS get_working_set_ex( HANDLE process, LPCVOID addr,
         return STATUS_INVALID_INFO_CLASS;
     }
 
+    if (len < sizeof(*info)) return STATUS_INFO_LENGTH_MISMATCH;
+
 #if defined(HAVE_LIBPROCSTAT)
     {
         struct procstat *pstat;
@@ -5776,7 +5778,7 @@ static NTSTATUS get_working_set_ex( HANDLE process, LPCVOID addr,
 #endif
 
     if (res_len)
-        *res_len = (UINT_PTR)p - (UINT_PTR)info;
+        *res_len = len;
     return STATUS_SUCCESS;
 }
 
