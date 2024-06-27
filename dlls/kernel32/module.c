42eb3f14ef9f0fb6be44eab27f6ead02e03b89e0
Paul Gofman <pgofman@codeweavers.com>
2024-06-27 17:03:21 -0600

    Revert "kernel32: Get rid of an extra wrapper in GetProcAddress()."
    This reverts commit 05b2b519e9676cf0c080c0811dd7a91f0f8e4273.

--- dlls/kernel32/module.c
+++ dlls/kernel32/module.c
@@ -305,8 +305,9 @@ FARPROC get_proc_address( HMODULE hModule, LPCSTR function )
  * these registers if the function takes floating point parameters.
  * This wrapper saves xmm0 - 3 to the stack.
  */
-__ASM_GLOBAL_FUNC( GetProcAddress,
-                   ".byte 0x48,0x8d,0xa4,0x24,0x00,0x00,0x00,0x00\n\t" /* hotpatch prolog */
+extern FARPROC get_proc_address_wrapper( HMODULE module, LPCSTR function );
+
+__ASM_GLOBAL_FUNC( get_proc_address_wrapper,
                    "pushq %rbp\n\t"
                    __ASM_SEH(".seh_pushreg %rbp\n\t")
                    __ASM_CFI(".cfi_adjust_cfa_offset 8\n\t")
@@ -334,9 +335,14 @@ __ASM_GLOBAL_FUNC( GetProcAddress,
                    "ret" )
 #else /* __x86_64__ */
 
-FARPROC WINAPI DECLSPEC_HOTPATCH GetProcAddress( HMODULE hModule, LPCSTR function )
+static inline FARPROC get_proc_address_wrapper( HMODULE module, LPCSTR function )
 {
-    return get_proc_address( hModule, function );
+    return get_proc_address( module, function );
 }
 
 #endif /* __x86_64__ */
+
+FARPROC WINAPI GetProcAddress( HMODULE hModule, LPCSTR function )
+{
+    return get_proc_address_wrapper( hModule, function );
+}
