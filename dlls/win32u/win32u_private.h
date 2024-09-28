8ee1b711f93e1bfcd7a5deb5866783e03d4b9659
Paul Gofman <pgofman@codeweavers.com>
2024-09-27 18:17:34 -0600

    win32u: Correctly fill new foreground window TID in WM_ACTIVATEAPP.
    (cherry picked from commit b84fbae1c7c409a1fe5b0784f65baea32bef148b)
    
    CW-Bug-Id: #24294

--- dlls/win32u/win32u_private.h
+++ dlls/win32u/win32u_private.h
@@ -103,7 +103,7 @@ extern BOOL set_capture_window( HWND hwnd, UINT gui_flags, HWND *prev_ret );
 extern BOOL set_caret_blink_time( unsigned int time );
 extern BOOL set_caret_pos( int x, int y );
 extern BOOL set_foreground_window( HWND hwnd, BOOL mouse );
-extern BOOL set_active_window( HWND hwnd, HWND *prev, BOOL mouse, BOOL focus );
+extern BOOL set_active_window( HWND hwnd, HWND *prev, BOOL mouse, BOOL focus, DWORD new_active_thread_id );
 extern void toggle_caret( HWND hwnd );
 extern void update_mouse_tracking_info( HWND hwnd );
 extern BOOL get_clip_cursor( RECT *rect );
