92a11eb7662107d169e28289fbd60d41d9617988
Brendan McGrath <bmcgrath@codeweavers.com>
2024-12-02 18:40:52 +1100

    winegstreamer: Avoid pre-condition assertion failures.
    Both gst_audio_info_from_caps and gst_video_info_from_caps perform
    the following pre-condition assertion:
    g_return_val_if_fail (gst_caps_is_fixed (caps), FALSE);
    
    The documentation for this assertion states:
    Any failure of such a pre-condition assertion is considered a
    programming error on the part of the caller of the public API,
    and the program is considered to be in an undefined state afterwards.

--- dlls/winegstreamer/wg_format.c
+++ dlls/winegstreamer/wg_format.c
@@ -587,15 +587,19 @@ void wg_format_from_caps(struct wg_format *format, const GstCaps *caps)
     {
         GstAudioInfo info;
 
-        if (gst_audio_info_from_caps(&info, caps))
+        if (gst_caps_is_fixed(caps) && gst_audio_info_from_caps(&info, caps))
             wg_format_from_audio_info(format, &info);
+        else
+            GST_WARNING("Unable to get audio info from caps");
     }
     else if (!strcmp(name, "video/x-raw"))
     {
         GstVideoInfo info;
 
-        if (gst_video_info_from_caps(&info, caps))
+        if (gst_caps_is_fixed(caps) && gst_video_info_from_caps(&info, caps))
             wg_format_from_video_info(format, &info);
+        else
+            GST_WARNING("Unable to get video info from caps");
     }
     else if (!strcmp(name, "audio/mpeg") && gst_structure_get_boolean(structure, "parsed", &parsed) && parsed)
     {
