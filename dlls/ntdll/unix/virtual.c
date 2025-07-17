cb0202c19595d87e6502af733ca04475bba2520c
Paul Gofman <pgofman@codeweavers.com>
2025-07-17 11:17:48 -0600

    Revert "ntdll: HACK: Limit address space for Wuthering Waves."
    This reverts commit b5f0e4e3ec423b0c62016bda406623d96a4ed50f.
    
    CW-Bug-Id: #25606

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -2495,7 +2495,6 @@ static void fixup_effective_user_space_limit( const void **effective_user_space_
         cached = sgi &&
             (
                 !strcmp( sgi, "3092660" )
-                || !strcmp( sgi, "3513350" )
             );
     }
     if (cached)
