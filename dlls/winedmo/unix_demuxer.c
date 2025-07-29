3f6486cf1464c6b4c65bd38f50db2d613ad70fbf
Conor McCarthy <cmccarthy@codeweavers.com>
2025-07-29 17:46:05 +1000

    Revert "winedmo: Error out if avformat_open_input() fails."
    This reverts commit 3676ba1f0f1bbf0a9b595cc3c75fc7ab99843483.
    
    Some games, for example Resident Evil Village, depend upon execution
    continuing after this failure.

--- dlls/winedmo/unix_demuxer.c
+++ dlls/winedmo/unix_demuxer.c
@@ -247,8 +247,9 @@ NTSTATUS demuxer_create( void *arg )
     if (!(demuxer->ctx = avformat_alloc_context())) goto failed;
     if (!(demuxer->ctx->pb = avio_alloc_context( NULL, 0, 0, params->context, unix_read_callback, NULL, unix_seek_callback ))) goto failed;
 
-    ret = avformat_open_input( &demuxer->ctx, NULL, NULL, NULL );
-    if (ret < 0 || (ret = mediaconv_demuxer_open( &demuxer->ctx, params->context )) < 0)
+    if ((ret = avformat_open_input( &demuxer->ctx, NULL, NULL, NULL )) < 0)
+        WARN( "Failed to open input, error %s.\n", debugstr_averr(ret) );
+    if ((ret = mediaconv_demuxer_open( &demuxer->ctx, params->context ) < 0))
     {
         ERR( "Failed to open input, error %s.\n", debugstr_averr(ret) );
         goto failed;
