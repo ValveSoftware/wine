b8a85e6ac878f7e0559ffa212da7cee44bad2ba5
CharlieDuzStuf <99685295+charlieduzstuf@users.noreply.github.com>
2025-11-02 11:00:25 +0000

    Better ATL Support

---
+++ include/atl_runtime.h
@@ -0,0 +1,14 @@
+#ifndef GOLIATH_ATL_RUNTIME_H
+#define GOLIATH_ATL_RUNTIME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int atl_runtime_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GOLIATH_ATL_RUNTIME_H */
---
+++ libs/atl_runtime/atl_runtime.c
@@ -0,0 +1,7 @@
+#include "atl_runtime.h"
+
+int atl_runtime_init(void)
+{
+    /* Minimal stub initialization. Real implementation will bridge Android APIs. */
+    return 0;
+}
