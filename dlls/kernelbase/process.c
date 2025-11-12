6e4d3b537cff1d611ff209e7e2adfd4fa948b57d
Paul Gofman <pgofman@codeweavers.com>
2025-11-12 10:56:53 -0600

    Revert "kernelbase: HACK: Force CEF swiftshader for EpicOnlineServicesUIHelper."
    This reverts commit 40e67541749bc98c54cef4b42b3dbf541db206a7.
    
    CW-Bug-Id: #26205

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -608,7 +608,6 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
         {L"Montaro\\nw.exe", L" --use-gl=swiftshader"},
         {L"Aisling and the Tavern of Elves\\nw.exe", L" --use-gl=swiftshader"},
         {L"Snares of Ruin 2\\SoR2.exe", L" --use-gl=swiftshader"},
-        {L"\\EpicOnlineServicesUIHelper", L" --use-gl=desktop"},
         {L"OlympiaRising.exe", L" --use-gl=swiftshader"},
         {L"nw.exe.exe", L" --use-angle=d3d9"},
         {L"DC Universe Online\\LaunchPad.exe", L" --use-gl=swiftshader"},
