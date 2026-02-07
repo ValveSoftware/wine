e887228e7cbc4b463b1a1010a666531537bd046a
Rémi Bernon <rbernon@codeweavers.com>
2026-02-07 17:23:20 +0100

    winex11: Register clipboard format with NtUserRegisterWindowMessage.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59257
    (cherry picked from commit 0ed7e47d01975be9102b2a2c46e7e52ba889ddf7)

--- dlls/winex11.drv/clipboard.c
+++ dlls/winex11.drv/clipboard.c
@@ -288,9 +288,9 @@ static struct clipboard_format *find_x11_format( Atom atom )
 
 static ATOM register_clipboard_format( const WCHAR *name )
 {
-    ATOM atom;
-    if (NtAddAtom( name, lstrlenW( name ) * sizeof(WCHAR), &atom )) return 0;
-    return atom;
+    UNICODE_STRING name_str;
+    RtlInitUnicodeString( &name_str, name );
+    return NtUserRegisterWindowMessage( &name_str );
 }
 
 
