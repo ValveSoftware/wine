786c1373fee0f096dcbc4ad92c9508871ff31c33
Rémi Bernon <rbernon@codeweavers.com>
2026-02-17 11:05:50 +0100

    win32u: Don't offset client rect when using D3D present rect.
    The window rects are all in absolute screen position and the client rect
    shouldn't be made relative to the window position.
    
    CW-Bug-Id: #26545

--- dlls/win32u/window.c
+++ dlls/win32u/window.c
@@ -2297,7 +2297,6 @@ static BOOL apply_window_pos( HWND hwnd, HWND insert_after, UINT swp_flags, stru
         {
             MONITORINFO monitor_info = monitor_info_from_rect( new_rects->window, dpi );
             struct window_rects rects = { monitor_info.rcMonitor, monitor_info.rcMonitor, monitor_info.rcMonitor };
-            OffsetRect( &rects.client, -rects.client.left, -rects.client.top );
             swp_flags |= WINE_SWP_FULLSCREEN;
             swp_flags &= ~WINE_SWP_RESIZABLE;
             monitor_rects = map_window_rects_virt_to_raw( rects, dpi );
