e6b27a02ea97182e2b8d9ac196083b051eeedc7a
Conor McCarthy <cmccarthy@codeweavers.com>
2026-03-03 00:11:20 +1000

    mf/session: Support asynchronous transforms.
    (cherry picked from commit 622651f7eef97f1afb35dd31002f1c6f9e9cfe63)
    
    CW-Bug-Id: #26148

--- dlls/mf/tests/mf.c
+++ dlls/mf/tests/mf.c
@@ -7866,7 +7866,6 @@ static void test_transform_check_unlocked_(int line, struct test_transform *tran
         return;
     hr = IMFAttributes_GetUINT32(transform->attributes, &MF_TRANSFORM_ASYNC_UNLOCK, &unlock);
     ok_(__FILE__, line)(hr == S_OK, "Unexpected hr %#lx.\n", hr);
-    todo_wine
     ok_(__FILE__, line)(unlock, "Transform is locked.\n");
 }
 
@@ -9077,7 +9076,6 @@ static void test_async_transform(void)
     for (i = 0, ret = WAIT_OBJECT_0; i < 10 && ret == WAIT_OBJECT_0; ++i)
     {
         ret = WaitForSingleObject(grabber_callback->ready_event, 1000);
-        todo_wine
         ok(ret == WAIT_OBJECT_0, "WaitForSingleObject returned %lu\n", ret);
     }
 
@@ -9098,7 +9096,6 @@ static void test_async_transform(void)
     hr = IMFMediaSession_Shutdown(session);
     ok(hr == S_OK, "Unexpected hr %#lx.\n", hr);
 
-    todo_wine
     ok(transform->is_shut_down, "Transform was not shut down.\n");
 
     IMFTransform_Release(mft);
