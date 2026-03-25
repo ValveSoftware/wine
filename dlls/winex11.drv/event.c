ea1fac8828b305720d3b2eb3d4aab20134e03d17
Rémi Bernon <rbernon@codeweavers.com>
2026-03-25 13:35:52 +0100

    winex11: Update queue input time on WM_TAKE_FOCUS.
    CW-Bug-Id: #26817

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -797,6 +797,13 @@ static void handle_wm_protocols( HWND hwnd, XClientMessageEvent *event )
                 NtUserIsWindowEnabled( hwnd ), NtUserIsWindowVisible( hwnd ), NtUserGetWindowLongW( hwnd, GWL_STYLE ),
                 get_focus(), get_active_window(), last_focus );
 
+        if (event_time) SERVER_START_REQ( set_user_input_time )
+        {
+            req->set = 1;
+            wine_server_call( req );
+        }
+        SERVER_END_REQ;
+
         /* Steam sometimes calls XSetInputFocus with CurrentTime when it gets focused out, and the game gets
          * focused in, this effectively sometimes steals focus away from us. Although there's no guarantee to
          * win the race as it entirely depends on the request timings, using CurrentTime makes it more likely.
