e4e4d79bec13381fd70e076e406118d6263abe4b
Paul Gofman <pgofman@codeweavers.com>
2026-03-31 17:10:05 -0600

    win32u, winevulkan: Truncate deviceID to 16 bit.
    CW-Bug-Id: #26830

--- dlls/winevulkan/vulkan.c
+++ dlls/winevulkan/vulkan.c
@@ -741,7 +741,7 @@ static void parse_openvr_extensions(struct conversion_context *ctx, const char *
     char name[64];
 
     phys_dev->obj.instance->p_vkGetPhysicalDeviceProperties(phys_dev->obj.host.physical_device, &prop);
-    sprintf( name, "VK_WINE_OPENVR_DEVICE_EXTS_PCIID_%04x_%04x", prop.vendorID, prop.deviceID );
+    sprintf( name, "VK_WINE_OPENVR_DEVICE_EXTS_PCIID_%04x_%04x", prop.vendorID, (uint16_t)prop.deviceID );
     parse_vr_extensions(ctx, extra_extensions, extra_count, getenv(name));
 }
 
@@ -4767,6 +4767,7 @@ void wine_vkGetPhysicalDeviceProperties(VkPhysicalDevice client_physical_device,
     TRACE("%p, %p\n", phys_dev, properties);
 
     phys_dev->obj.instance->p_vkGetPhysicalDeviceProperties(phys_dev->obj.host.physical_device, properties);
+    properties->deviceID = properties->deviceID & 0xffff;
     fixup_device_id(&properties->vendorID, &properties->deviceID);
 }
 
@@ -4778,6 +4779,7 @@ void wine_vkGetPhysicalDeviceProperties2(VkPhysicalDevice client_physical_device
     TRACE("%p, %p\n", phys_dev, properties);
 
     phys_dev->obj.instance->p_vkGetPhysicalDeviceProperties2(phys_dev->obj.host.physical_device, properties);
+    properties->properties.deviceID = properties->properties.deviceID & 0xffff;
     fixup_device_id(&properties->properties.vendorID, &properties->properties.deviceID);
 }
 
@@ -4789,6 +4791,7 @@ void wine_vkGetPhysicalDeviceProperties2KHR(VkPhysicalDevice client_physical_dev
     TRACE("%p, %p\n", phys_dev, properties);
 
     phys_dev->obj.instance->p_vkGetPhysicalDeviceProperties2KHR(phys_dev->obj.host.physical_device, properties);
+    properties->properties.deviceID = properties->properties.deviceID & 0xffff;
     fixup_device_id(&properties->properties.vendorID, &properties->properties.deviceID);
 }
 
