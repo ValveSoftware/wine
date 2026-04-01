f865c499901ec78506c5e5dd16b7ffcf2da94863
Paul Gofman <pgofman@codeweavers.com>
2026-04-01 12:07:21 -0600

    ntdll: HACK: Enable WINE_SIMULATE_WRITECOPY for Puppeteer : Control.
    CW-Bug-Id: #26858

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2344,6 +2344,7 @@ static void hacks_init(void)
                                        || !strcmp(sgi, "2329630") /* Lovey-Dovey Lockdown */
                                        || !strcmp(sgi, "2209020") /* Gemstones */
                                        || !strcmp(sgi, "223750") /* DCS World */
+                                       || !strcmp(sgi, "2495160") /* Puppeteer : Control */
                                        || !strcmp(sgi, "2361360"); /* Hentai Maid Memories */
 
     if (sgi) wine_allocs_2g_limit = !strcmp(sgi, "359870");
