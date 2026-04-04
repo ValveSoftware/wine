ValveSoftware/wine

commit 55e5a1ccea37d1bd889ab269d55b499bf131ddf9

wm_reader: Fix order of streams in reinit_stream.
CW-Bug-Id: #23483

dlls/winegstreamer/wm_reader.c
@@ -1619,7 +1619,7 @@ static HRESULT reinit_stream(struct wm_reader *reader, bool read_compressed)
         struct wm_stream *stream = &reader->streams[i];
         struct wg_format format;
 
-        stream->wg_stream = wg_parser_get_stream(reader->wg_parser, i);
+        stream->wg_stream = wg_parser_get_stream(reader->wg_parser, reader->stream_count - i - 1);
         stream->reader = reader;
         wg_parser_stream_get_current_format(stream->wg_stream, &format);
         if (stream->selection == WMT_ON)

commit 0c4f72e0263e9f849a45c9ae191306cf13310134

winegstreamer: Don't get stream ID wires crossed.
Fixes: 55e5a1ccea37d1bd889ab269d55b499bf131ddf9

dlls/winegstreamer/wm_reader.c
@@ -1853,6 +1853,12 @@ static HRESULT wm_reader_read_stream_sample(struct wm_reader *reader, struct wg_
 
     if (!(stream = wm_reader_get_stream_by_stream_number(reader, buffer->stream + 1)))
         return E_INVALIDARG;
+    if (stream->read_compressed)
+    {
+        buffer->stream = reader->stream_count - buffer->stream - 1;
+        if (!(stream = wm_reader_get_stream_by_stream_number(reader, buffer->stream + 1)))
+            return E_INVALIDARG;
+    }
 
     TRACE("Got buffer for '%s' stream %p.\n", get_major_type_string(stream->format.major_type), stream);
 