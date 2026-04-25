e5a66d0774d6537b0585a275ab2ba915ce653585
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-04-25 19:07:09 +0530

    added nominmax to lsteamclient

--- lsteamclient/steamclient_private.h
+++ lsteamclient/steamclient_private.h
@@ -7,8 +7,10 @@
 #include <stdint.h>
 
 /* Pull in libc/libc++ declarations of wcsncpy/strncpy/etc before
- * <winbase.h> turns them into poison macros. */
+ * <winbase.h> turns them into poison macros, and disable Win32's
+ * min/max macros so std::max/std::min in C++ sources don't expand. */
 #ifdef __cplusplus
+#define NOMINMAX
 #include <array>
 #endif /* __cplusplus */
 
--- lsteamclient/unix_private.h
+++ lsteamclient/unix_private.h
@@ -5,8 +5,10 @@
 #include <stddef.h>
 
 /* Pull in libc/libc++ declarations of wcsncpy/strncpy/etc before
- * <winbase.h> turns them into poison macros. */
+ * <winbase.h> turns them into poison macros, and disable Win32's
+ * min/max macros so std::max/std::min in C++ sources don't expand. */
 #ifdef __cplusplus
+#define NOMINMAX
 #include <array>
 #endif /* __cplusplus */
 
