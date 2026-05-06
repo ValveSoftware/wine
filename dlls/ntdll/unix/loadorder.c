75c9e12a7caf4f5a3c48950ac0c809f121046a47
Anna Lasky <alasky@codeweavers.com>
2026-05-06 12:21:23 -0500

    Revert "ntdll: HACK: Disable old EAC bridge for Crossout."
    This reverts commit c6398fa5fbfe17d0c407da83ff62c32cfc538a63.
    
    This is no longer needed because the game replaced EAC with BattlEye.
    
    CW-Bug-Id: #16533

--- dlls/ntdll/unix/loadorder.c
+++ dlls/ntdll/unix/loadorder.c
@@ -364,19 +364,11 @@ static enum loadorder get_load_order_value( HANDLE std_key, HANDLE app_key, WCHA
 void set_load_order_app_name( const WCHAR *app_name )
 {
     static const WCHAR eac_launcherW[] = {'P','R','O','T','O','N','_','E','A','C','_','L','A','U','N','C','H','E','R','_','P','R','O','C','E','S','S',0};
-    static const WCHAR crossoutW[] = {'C','r','o','s','s','o','u','t','.','e','x','e',0};
     const WCHAR *p;
 
     if ((p = wcsrchr( app_name, '\\' ))) app_name = p + 1;
     app_key = open_app_key( app_name );
     main_exe_loaded = TRUE;
-
-    if (!wcscmp( app_name, crossoutW ))
-    {
-        ERR( "Disabling EAC bridge.\n" );
-        eac_launcher_process = TRUE;
-        return;
-    }
 
     p = NtCurrentTeb()->Peb->ProcessParameters->Environment;
     while(*p)
