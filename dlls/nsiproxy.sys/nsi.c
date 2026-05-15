68ee0af612ec78c82b5d54e57857bc182264f34a
Paul Gofman <pgofman@codeweavers.com>
2026-05-15 15:41:24 -0600

    nsiproxy.sys: Poll all icmp sockets in a single thread.
    CW-Bug-Id: #20391

--- dlls/nsiproxy.sys/nsi.c
+++ dlls/nsiproxy.sys/nsi.c
@@ -344,9 +344,8 @@ static NTSTATUS unix_nsi_get_notification( void *args )
 
 const unixlib_entry_t __wine_unix_call_funcs[] =
 {
-    icmp_cancel_listen,
-    icmp_close,
     icmp_get_reply,
+    icmp_listen,
     icmp_send_echo,
     unix_nsi_enumerate_all_ex,
     unix_nsi_get_all_parameters_ex,
