3bb8904cee076f08ac4c410f3f3232aa5b102f4a
Arkadiusz Hiler <ahiler@codeweavers.com>
2026-07-01 00:50:57 +0300

    Revert "ntdll: Add an interface to map the FEX stats shm."
    This reverts commit e1b3211935f478b0ac834d647de83f9a4bdac362.
    
    No longer needed with FEX's unixlib.

--- include/winternl.h
+++ include/winternl.h
@@ -2467,7 +2467,6 @@ typedef enum _MEMORY_INFORMATION_CLASS {
     MemoryWineUnixFuncs = 1000,
     MemoryWineUnixWow64Funcs,
 #endif
-    MemoryFexStatsShm = 2000,
 } MEMORY_INFORMATION_CLASS;
 
 typedef struct _MEMORY_SECTION_NAME
@@ -2533,12 +2532,6 @@ typedef struct _MEMORY_IMAGE_INFORMATION
         };
     };
 } MEMORY_IMAGE_INFORMATION, *PMEMORY_IMAGE_INFORMATION;
-
-typedef struct _MEMORY_FEX_STATS_SHM_INFORMATION
-{
-    void *shm_base;
-    SIZE_T map_size;
-} MEMORY_FEX_STATS_SHM_INFORMATION, *PMEMORY_FEX_STATS_SHM_INFORMATION;
 
 typedef enum _MUTANT_INFORMATION_CLASS
 {
