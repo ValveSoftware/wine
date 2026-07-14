6cee7d0f4e6c44bbb4d1f020757098c17bb2488f
Paul Gofman <pgofman@codeweavers.com>
2026-07-13 18:18:05 -0600

    Revert "fixup! ntdll: HACK: Force en-US locale for Tom Clancy’s The Division 2 EAC launcher."
    This reverts commit 3be3f2ebbcf48096737ace8463d61d7fb267e022.

--- dlls/ntdll/unix/env.c
+++ dlls/ntdll/unix/env.c
@@ -747,7 +747,7 @@ static void init_locale(void)
     if (main_argc > 1 && (
             strstr(main_argv[1], "start_protected_game.exe")
             || strstr(main_argv[1], "RocketLeague_EAC.exe")
-            || ((p = getenv( "SteamGameId" )) && !strcmp( p, "2221490" ) && !strcmp( main_argv[1], "-public_key_store_address" ))
+            || strstr(main_argv[1], "EACLaunch.exe")
        ))
     {
         FIXME( "HACK setting EN locale.\n" );
