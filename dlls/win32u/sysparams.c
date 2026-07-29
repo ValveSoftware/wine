4bb052271e7c0cfcf699085f7f5f8a0f3649feb8
Henri Verbeet <hverbeet@codeweavers.com>
2026-07-29 17:41:31 +0200

    fixup! HACK: fshack: win32u: Emulate modesetting by default.
    (cherry picked from commit b385d4d289dbd1b423a16e758984cff88756ad9e)

--- dlls/win32u/sysparams.c
+++ dlls/win32u/sysparams.c
@@ -6201,7 +6201,7 @@ void sysparams_init(void)
     if (!get_config_key( hkey, appkey, "EmulateModelist", buffer, sizeof(buffer) ))
         emulate_modelist = !IS_OPTION_TRUE( buffer[0] );
     if (!get_config_key( hkey, appkey, "EmulateModeset", buffer, sizeof(buffer) ))
-        emulate_modeset = !IS_OPTION_TRUE( buffer[0] );
+        emulate_modeset = IS_OPTION_TRUE( buffer[0] );
 
     {
         const char *s;
