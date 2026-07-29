5e4c89642791fc898d26cd15b44bb7ae782d79ea
Paul Gofman <pgofman@codeweavers.com>
2026-07-29 17:31:43 -0600

    kernelbase: Query debug port instead of BeingDebugged PEB flag in UnhandledExceptionFilter().
    CW-Bug-Id: #27458

--- dlls/kernelbase/debug.c
+++ dlls/kernelbase/debug.c
@@ -754,6 +754,7 @@ static BOOL check_resource_write( void *addr )
 LONG WINAPI UnhandledExceptionFilter( EXCEPTION_POINTERS *epointers )
 {
     const EXCEPTION_RECORD *rec = epointers->ExceptionRecord;
+    ULONG_PTR debug_port;
     BOOL nested;
 
     if (rec->ExceptionCode == EXCEPTION_ACCESS_VIOLATION && rec->NumberParameters >= 2)
@@ -767,7 +768,8 @@ LONG WINAPI UnhandledExceptionFilter( EXCEPTION_POINTERS *epointers )
         }
     }
 
-    if (!NtCurrentTeb()->Peb->BeingDebugged)
+    NtQueryInformationProcess( GetCurrentProcess(), ProcessDebugPort, &debug_port, sizeof(debug_port), NULL );
+    if (!debug_port)
     {
         if (rec->ExceptionCode == CONTROL_C_EXIT)
         {
