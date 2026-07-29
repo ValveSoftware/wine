73ed82326f033e6ee8d1263210a7ca7b484d960e
Paul Gofman <pgofman@codeweavers.com>
2026-07-29 17:40:55 -0600

    Revert "kernelbase: Call top filter even if BeingDebugged PEB flag is set."
    This reverts commit ba12b41ef10bf1efb6916b23174126edd5677fbd.
    
    CW-Bug-Id: #27458

--- dlls/kernelbase/debug.c
+++ dlls/kernelbase/debug.c
@@ -767,13 +767,6 @@ LONG WINAPI UnhandledExceptionFilter( EXCEPTION_POINTERS *epointers )
         }
     }
 
-    nested = rec->ExceptionFlags & EXCEPTION_NESTED_CALL;
-    if (top_filter && !nested)
-    {
-        LONG ret = top_filter( epointers );
-        if (ret != EXCEPTION_CONTINUE_SEARCH) return ret;
-    }
-
     if (!NtCurrentTeb()->Peb->BeingDebugged)
     {
         if (rec->ExceptionCode == CONTROL_C_EXIT)
@@ -782,10 +775,18 @@ LONG WINAPI UnhandledExceptionFilter( EXCEPTION_POINTERS *epointers )
             TerminateProcess( GetCurrentProcess(), 1 );
         }
 
-        if (!(GetErrorMode() & SEM_NOGPFAULTERRORBOX) && start_debugger_atomic( epointers ))
-            return EXCEPTION_CONTINUE_SEARCH;
+        nested = rec->ExceptionFlags & EXCEPTION_NESTED_CALL;
+        if (top_filter && !nested)
+        {
+            LONG ret = top_filter( epointers );
+            if (ret != EXCEPTION_CONTINUE_SEARCH) return ret;
+        }
+
+        if ((GetErrorMode() & SEM_NOGPFAULTERRORBOX) ||
+            !start_debugger_atomic( epointers ) || !NtCurrentTeb()->Peb->BeingDebugged)
+            return nested ? EXCEPTION_CONTINUE_SEARCH : EXCEPTION_EXECUTE_HANDLER;
     }
-    return nested ? EXCEPTION_CONTINUE_SEARCH : EXCEPTION_EXECUTE_HANDLER;
+    return EXCEPTION_CONTINUE_SEARCH;
 }
 
 
