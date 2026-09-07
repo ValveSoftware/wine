81b7818af42f7d03b9f622f8c8336127bf679f18
Arkadiusz Hiler <ahiler@codeweavers.com>
2026-09-07 20:48:29 +0300

    fixup! HACK: xinput1_3: Don't rescan controller in get_state for Far Cry 4.

--- dlls/xinput1_3/main.c
+++ dlls/xinput1_3/main.c
@@ -861,7 +861,7 @@ static DWORD xinput_get_state(DWORD index, XINPUT_STATE *state)
     ret = get_current_state(index, state) ? ERROR_SUCCESS : ERROR_DEVICE_NOT_CONNECTED;
     if (ret == ERROR_SUCCESS) goto done;
     sgi = getenv("SteamGameId");
-    if (!strcmp(sgi, "298110")) goto done;
+    if (sgi && !strcmp(sgi, "298110")) goto done;
 
     EnterCriticalSection(&xinput_cs);
     update_controller_list();
