a517ca918a52e14fb3f9043c08669b6fa7709e2b
Zhiyi Zhang <zzhang@codeweavers.com>
2026-09-14 10:26:23 +0800

    win32u: Allow 8-bit DDBs with 8-bit display DCs in NtGdiGetDIBitsInternal().
    The original check was meant for display DCs, so check whether the technology of a DC is
    DT_RASDISPLAY as well.
    
    CW-Bug-Id: #27702
    (cherry picked from commit 750be973eaa191b015669b6a972e01c66952bbde)

--- dlls/user32/tests/monitor.c
+++ dlls/user32/tests/monitor.c
@@ -2870,7 +2870,6 @@ static void test_display_dc(void)
                 count = GetDIBits(hdc2, hbitmap, 0, 0, NULL, bmi, DIB_RGB_COLORS);
                 if (ddb_bpps[j] == 1 || (bpps[i] == 8 && ddb_bpps[j] == 8))
                 {
-                    todo_wine_if(bpps[i] == 8 && ddb_bpps[j] == 8)
                     ok(count == 1, "GetDIBits failed.\n");
                     ok(*(unsigned int *)bmi->bmiColors == 0, "Got unexpected bmiColors %#x\n",
                        *(unsigned int *)bmi->bmiColors);
@@ -2910,7 +2909,6 @@ static void test_display_dc(void)
                 bmi->bmiHeader.biBitCount = ddb_bpps[j];
                 count = GetDIBits(hdc2, hbitmap, 0, 1, bits_buffer, bmi, DIB_RGB_COLORS);
                 if (ddb_bpps[j] == 1 || (bpps[i] == 8 && ddb_bpps[j] == 8) || ddb_bpps[j] == 32)
-                    todo_wine_if(bpps[i] == 8 && ddb_bpps[j] == 8)
                     ok(count == 1, "GetDIBits failed.\n");
                 else
                     ok(count == 0, "GetDIBits succeeded.\n");
@@ -2926,7 +2924,6 @@ static void test_display_dc(void)
                 bmi->bmiHeader.biBitCount = ddb_bpps[j];
                 count = GetDIBits(mem_dc, hbitmap, 0, 1, bits_buffer, bmi, DIB_RGB_COLORS);
                 if (ddb_bpps[j] == 1 || (bpps[i] == 8 && ddb_bpps[j] == 8) || ddb_bpps[j] == 32)
-                    todo_wine_if(bpps[i] == 8 && ddb_bpps[j] == 8)
                     ok(count == 1, "GetDIBits failed.\n");
                 else
                     ok(count == 0, "GetDIBits succeeded.\n");
